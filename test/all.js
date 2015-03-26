8a3121f3d5df092127d4dc10d042dcb807ec8ea3
Manfred Touron <m@42.am>
2015-03-26 20:27:59 +0100

    Added tests

---
+++ test/all.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var chai = require('chai'),
+    debug = require('debug')('tests'),
+    program = require('..'),
+    stdout = require('test-console').stdout,
+    util = require('util');
+
+
+// Initialize chai.should()
+chai.should();
+
+
+var inspect = function(name, obj) {
+  debug(name, util.inspect(obj, {showHidden: false, depth: null}));
+};
+
+
+var run = function(command) {
+  var args = [];
+  args = args.concat.apply(['node', 'onlinelabs'], command);
+  var inspect = stdout.inspect();
+  program.parse(args);
+  inspect.restore();
+  return inspect.output.join('');
+};
+
+
+suite("[program]", function() {
+  test('info', function() {
+    var output = run(['info']);
+    (output).should.contain('User: ' + process.env['USER']);
+  });
+  test('version', function() {
+    var output = run(['version']);
+    (output).should.contain('Client version: ' + require('../package.json').version);
+    (output).should.contain('Client API version: ' + require('onlinelabs/package.json').version);
+  });
+});
