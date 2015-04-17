97e98719ccade3e190eea875fed81342caa7cf2b
Manfred Touron <m@42.am>
2015-04-17 17:57:02 +0200

    Added timeout=600 for start --sync

--- examples/create-image-from-s3.sh
+++ examples/create-image-from-s3.sh
@@ -31,7 +31,7 @@ echo "[+] Server created: ${SERVER}"
 
 
 echo "[+] Booting..."
-scw start --sync "${SERVER}" >/dev/null
+scw start --sync --timeout=600 "${SERVER}" >/dev/null
 IP=$(scw inspect "${SERVER}" -f .server.public_ip.address)
 scw exec "${SERVER}" 'uname -a'
 echo "[+] SSH is ready (${IP})"
