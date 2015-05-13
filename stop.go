f9879d7644574f0b48ff9651a84dfd6bcd1bbf13
s. rannou <mxs@sbrk.org>
2015-05-13 19:02:06 +0200

    Added command 'scw stop'. #16

---
+++ stop.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var cmdStop = &Command{
+	Exec:        runStop,
+	UsageLine:   "stop [OPTIONS] SERVER [SERVER...]",
+	Description: "Stop a running server.",
+	Help:        "Stop a running server.",
+}
+
+func runStop(cmd *Command, args []string) {
+	if len(args) == 0 {
+		fmt.Fprintf(os.Stderr, "usage: scw %s\n", cmd.UsageLine)
+		os.Exit(1)
+	}
+	has_error := false
+	for _, needle := range args {
+		server := cmd.GetServer(needle)
+		err := cmd.API.PostServerAction(server, "poweroff")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to stop server %s: %s\n", server, err)
+			has_error = true
+		} else {
+			fmt.Fprintf(os.Stdout, "%s\n", server)
+		}
+	}
+	if has_error {
+		os.Exit(1)
+	}
+}
