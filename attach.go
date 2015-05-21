82023d33bad504b833cdebae61f6506ef27c7696
Manfred Touron <m@42.am>
2015-05-21 13:37:42 +0200

    Added fallback for scw attach

--- attach.go
+++ attach.go
@@ -15,6 +15,8 @@ var cmdAttach = &Command{
 	Help:        "Attach to a running server serial console.",
 }
 
+const termjsBin string = "termjs-cli"
+
 func runAttach(cmd *Command, args []string) {
 	if len(args) < 1 {
 		log.Fatalf("usage: scw %s", cmd.UsageLine)
@@ -24,14 +26,25 @@ func runAttach(cmd *Command, args []string) {
 
 	termjsUrl := fmt.Sprintf("https://tty.cloud.online.net?server_id=%s&type=serial&auth_token=%s", serverId, cmd.API.Token)
 
-	log.Debugf("Executing: termjs-cli %s", termjsUrl)
+	log.Debugf("Executing: %s %s", termjsBin, termjsUrl)
 	// FIXME: check if termjs-cli is installed
-	spawn := exec.Command("termjs-cli", termjsUrl)
+	spawn := exec.Command(termjsBin, termjsUrl)
 	spawn.Stdout = os.Stdout
 	spawn.Stdin = os.Stdin
 	spawn.Stderr = os.Stderr
 	err := spawn.Run()
 	if err != nil {
+		log.Warnf("%v", err)
+		fmt.Sprintf(os.Stderr, `
+You need to install '%s' from https://github.com/moul/term.js-cli
+
+    npm install -g term.js-cli
+
+However, you can access your serial using a web browser:
+
+    %s
+
+`, termjsBin, termjsUrl)
 		os.Exit(1)
 	}
 }
