dfacc09fde4945c37454ce6ce54e38d974809b23
Manfred Touron <m@42.am>
2015-06-09 15:37:07 +0200

    Testing truncIf (#51)

--- utils_test.go
+++ utils_test.go
@@ -9,3 +9,29 @@ func TestWordify(t *testing.T) {
 		t.Errorf("returned value is invalid [actual: %s][expected: %s]", actual, expected)
 	}
 }
+
+func TestTruncIf(t *testing.T) {
+	actual := truncIf("Hello World", 5, false)
+	expected := "Hello World"
+	if actual != expected {
+		t.Errorf("returned value is invalid [actual: %s][expected: %s]", actual, expected)
+	}
+
+	actual = truncIf("Hello World", 5, true)
+	expected = "Hello"
+	if actual != expected {
+		t.Errorf("returned value is invalid [actual: %s][expected: %s]", actual, expected)
+	}
+
+	actual = truncIf("Hello World", 50, false)
+	expected = "Hello World"
+	if actual != expected {
+		t.Errorf("returned value is invalid [actual: %s][expected: %s]", actual, expected)
+	}
+
+	actual = truncIf("Hello World", 50, true)
+	expected = "Hello World"
+	if actual != expected {
+		t.Errorf("returned value is invalid [actual: %s][expected: %s]", actual, expected)
+	}
+}
