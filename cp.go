6f4a46ce91870a1bf56308da9546f705dcda433c
Manfred Touron <m@42.am>
2015-06-10 10:19:44 +0200

    Handling 'scw cp - ...' (#56)

--- cp.go
+++ cp.go
@@ -18,15 +18,17 @@ var cmdCp = &Command{
 	Help:        "Copy files/folders from a PATH on the server to a HOSTDIR on the host\nrunning the command. Use '-' to write the data as a tar file to STDOUT.",
 	Examples: `
     $ scw cp path/to/my/local/file myserver:path
-    $ scw cp myserver:path/to/file path/to/my/local/file
-    $ scw cp myserver:path/to/file myserver2:path/to/file
+    $ scw cp myserver:path/to/file path/to/my/local/dir
+    $ scw cp myserver:path/to/file myserver2:path/to/dir
     $ scw cp myserver:path/to/file - > myserver-pathtofile-backup.tar
     $ scw cp myserver:path/to/file - | tar -tvf -
-    $ scw cp path/to/my/local/dir myserver:path
-    $ scw cp myserver:path/to/dir path/to/my/local/dir
-    $ scw cp myserver:path/to/dir myserver2:path/to/dir
-    $ scw cp myserver:path/to/dir - > myserver-pathtodir-backup.tar
-    $ scw cp myserver:path/to/dir - | tar -tvf -
+    $ scw cp path/to/my/local/dir  myserver:path
+    $ scw cp myserver:path/to/dir  path/to/my/local/dir
+    $ scw cp myserver:path/to/dir  myserver2:path/to/dir
+    $ scw cp myserver:path/to/dir  - > myserver-pathtodir-backup.tar
+    $ scw cp myserver:path/to/dir  - | tar -tvf -
+    $ cat archive.tar | scw cp - myserver:/path
+    $ tar -cvf - . | scw cp - myserver:path
 `,
 }
 
@@ -98,7 +100,7 @@ func runCp(cmd *Command, args []string) {
 
 		io.Copy(os.Stderr, tarErrorStream)
 	} else if source == "-" { // stdin
-		log.Fatalf("'scw cp - ...' is not yet implemented")
+		tarOutputStream = os.Stdin
 	} else { // source host path
 		log.Debugf("Creating tarball of local path %s", source)
 		path, err := filepath.Abs(source)
