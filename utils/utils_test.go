2bbb7d321b77b4bd22ed6870b3c3e0d1095b2107
Manfred Touron <m@42.am>
2015-06-12 17:48:31 +0200

    Fixed tests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -3,7 +3,7 @@ package utils
 import "testing"
 
 func TestWordify(t *testing.T) {
-	actual := wordify("Hello World 42 !!")
+	actual := Wordify("Hello World 42 !!")
 	expected := "Hello_World_42"
 	if actual != expected {
 		t.Errorf("returned value is invalid [actual: %s][expected: %s]", actual, expected)
@@ -11,25 +11,25 @@ func TestWordify(t *testing.T) {
 }
 
 func TestTruncIf(t *testing.T) {
-	actual := truncIf("Hello World", 5, false)
+	actual := TruncIf("Hello World", 5, false)
 	expected := "Hello World"
 	if actual != expected {
 		t.Errorf("returned value is invalid [actual: %s][expected: %s]", actual, expected)
 	}
 
-	actual = truncIf("Hello World", 5, true)
+	actual = TruncIf("Hello World", 5, true)
 	expected = "Hello"
 	if actual != expected {
 		t.Errorf("returned value is invalid [actual: %s][expected: %s]", actual, expected)
 	}
 
-	actual = truncIf("Hello World", 50, false)
+	actual = TruncIf("Hello World", 50, false)
 	expected = "Hello World"
 	if actual != expected {
 		t.Errorf("returned value is invalid [actual: %s][expected: %s]", actual, expected)
 	}
 
-	actual = truncIf("Hello World", 50, true)
+	actual = TruncIf("Hello World", 50, true)
 	expected = "Hello World"
 	if actual != expected {
 		t.Errorf("returned value is invalid [actual: %s][expected: %s]", actual, expected)
