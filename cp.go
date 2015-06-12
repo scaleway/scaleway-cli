fd4a0f5a23e2b8a18c0f7568089af501191fbb1d
Manfred Touron <m@42.am>
2015-06-12 14:01:41 +0200

    Refactored cp (#56)

--- cp.go
+++ cp.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"os/exec"
@@ -39,35 +40,26 @@ func init() {
 // Flags
 var cpHelp bool // -h, --help flag
 
-func runCp(cmd *Command, args []string) {
-	if cpHelp {
-		cmd.PrintUsage()
-	}
-	if len(args) != 2 {
-		cmd.PrintShortUsage()
-	}
-
+func TarFromSource(api *ScalewayAPI, source string) (*io.ReadCloser, error) {
 	var tarOutputStream io.ReadCloser
-	var tarErrorStream io.ReadCloser
 
-	// source
-	source := args[0]
-	if strings.Index(source, ":") > -1 { // source server address
+	// source is a server address + path (scp-like uri)
+	if strings.Index(source, ":") > -1 {
 		log.Debugf("Creating a tarball remotely and streaming it using SSH")
-		serverParts := strings.Split(args[0], ":")
+		serverParts := strings.Split(source, ":")
 		if len(serverParts) != 2 {
-			log.Fatalf("usage: scw %s", cmd.UsageLine)
+			return nil, fmt.Errorf("invalid source uri, see 'scw cp -h' for usage")
 		}
 
-		serverID := cmd.API.GetServerID(serverParts[0])
+		serverID := api.GetServerID(serverParts[0])
 
-		server, err := cmd.API.GetServer(serverID)
+		server, err := api.GetServer(serverID)
 		if err != nil {
-			log.Fatalf("Failed to get server information for %s: %v", serverID, err)
+			return nil, err
 		}
 
 		dir, base := PathToTARPathparts(serverParts[1])
-		log.Debugf("Kind of equivalent of 'scp root@%s:%s/%s ...'", server.PublicAddress.IP, dir, base)
+		log.Debugf("Equivalent to 'scp root@%s:%s/%s ...'", server.PublicAddress.IP, dir, base)
 
 		// remoteCommand is executed on the remote server
 		// it streams a tarball raw content
@@ -86,61 +78,73 @@ func runCp(cmd *Command, args []string) {
 
 		tarOutputStream, err = spawnSrc.StdoutPipe()
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
-		tarErrorStream, err = spawnSrc.StderrPipe()
+		defer tarOutputStream.Close()
+
+		tarErrorStream, err := spawnSrc.StderrPipe()
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
+		defer tarErrorStream.Close()
+		io.Copy(os.Stderr, tarErrorStream)
 
 		err = spawnSrc.Start()
 		if err != nil {
-			log.Fatalf("Failed to start ssh command: %v", err)
+			return nil, err
 		}
-
 		defer spawnSrc.Wait()
 
-		io.Copy(os.Stderr, tarErrorStream)
-	} else if source == "-" { // stdin
-		log.Debugf("Streaming tarball from stdin")
-		tarOutputStream = os.Stdin
-	} else { // source host path
-		log.Debugf("Taring local path %s", source)
-		path, err := filepath.Abs(source)
-		if err != nil {
-			log.Fatalf("Cannot tar local path: %v", err)
-		}
-		path, err = filepath.EvalSymlinks(path)
-		if err != nil {
-			log.Fatalf("Cannot tar local path: %v", err)
-		}
-		log.Debugf("Real local path is %s", path)
-
-		dir, base := PathToTARPathparts(path)
-
-		tarOutputStream, err = archive.TarWithOptions(dir, &archive.TarOptions{
-			Compression:  archive.Uncompressed,
-			IncludeFiles: []string{base},
-		})
-		if err != nil {
-			log.Fatalf("Cannot tar local path: %v", err)
-		}
+		return &tarOutputStream, nil
 	}
 
-	// destination
-	destination := args[1]
-	if strings.Index(destination, ":") > -1 { // destination server address
+	// source is stdin
+	if source == "-" {
+		log.Debugf("Streaming tarball from stdin")
+		tarOutputStream = os.Stdin
+		defer tarOutputStream.Close()
+		return &tarOutputStream, nil
+	}
+
+	// source is a path on localhost
+	log.Debugf("Taring local path %s", source)
+	path, err := filepath.Abs(source)
+	if err != nil {
+		return nil, err
+	}
+	path, err = filepath.EvalSymlinks(path)
+	if err != nil {
+		return nil, err
+	}
+	log.Debugf("Real local path is %s", path)
+
+	dir, base := PathToTARPathparts(path)
+
+	tarOutputStream, err = archive.TarWithOptions(dir, &archive.TarOptions{
+		Compression:  archive.Uncompressed,
+		IncludeFiles: []string{base},
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer tarOutputStream.Close()
+	return &tarOutputStream, nil
+}
+
+func UntarToDest(api *ScalewayAPI, sourceStream *io.ReadCloser, destination string) error {
+	// destination is a server address + path (scp-like uri)
+	if strings.Index(destination, ":") > -1 {
 		log.Debugf("Streaming using ssh and untaring remotely")
 		serverParts := strings.Split(destination, ":")
 		if len(serverParts) != 2 {
-			log.Fatalf("usage: scw %s", cmd.UsageLine)
+			return fmt.Errorf("invalid destination uri, see 'scw cp -h' for usage")
 		}
 
-		serverID := cmd.API.GetServerID(serverParts[0])
+		serverID := api.GetServerID(serverParts[0])
 
-		server, err := cmd.API.GetServer(serverID)
+		server, err := api.GetServer(serverID)
 		if err != nil {
-			log.Fatalf("Failed to get server information for %s: %v", serverID, err)
+			return err
 		}
 
 		// remoteCommand is executed on the remote server
@@ -150,7 +154,7 @@ func runCp(cmd *Command, args []string) {
 		if os.Getenv("DEBUG") == "1" {
 			remoteCommand = append(remoteCommand, "-v")
 		}
-		remoteCommand = append(remoteCommand, "-tf", "-")
+		remoteCommand = append(remoteCommand, "-xf", "-")
 
 		// execCmd contains the ssh connection + the remoteCommand
 		execCmd := append(NewSSHExecCmd(server.PublicAddress.IP, false, remoteCommand))
@@ -159,40 +163,66 @@ func runCp(cmd *Command, args []string) {
 
 		untarInputStream, err := spawnDst.StdinPipe()
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
+		defer untarInputStream.Close()
+
 		untarErrorStream, err := spawnDst.StderrPipe()
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
+		defer untarErrorStream.Close()
+
 		untarOutputStream, err := spawnDst.StdoutPipe()
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
+		defer untarOutputStream.Close()
 
 		err = spawnDst.Start()
 		if err != nil {
-			log.Fatalf("Failed to start ssh command: %v", err)
+			return err
 		}
-
 		defer spawnDst.Wait()
 
-		io.Copy(untarInputStream, tarOutputStream)
+		io.Copy(untarInputStream, *sourceStream)
 		io.Copy(os.Stderr, untarErrorStream)
-		io.Copy(os.Stdout, untarOutputStream)
-	} else if destination == "-" { // stdout
-		log.Debugf("Writing tarOutputStream(%v) to os.Stdout(%v)", tarOutputStream, os.Stdout)
-		written, err := io.Copy(os.Stdout, tarOutputStream)
-		log.Debugf("%d bytes written", written)
-		if err != nil {
-			log.Fatal(err)
-		}
+		_, err = io.Copy(os.Stdout, untarOutputStream)
+		return err
+	}
 
-	} else { // destination host path
-		log.Debugf("Untaring to local path: %s", destination)
-		err := archive.Untar(tarOutputStream, destination, &archive.TarOptions{NoLchown: true})
-		if err != nil {
-			log.Fatalf("Failed to untar the remote archive: %v", err)
-		}
+	// destination is stdout
+	if destination == "-" { // stdout
+		log.Debugf("Writing sourceStream(%v) to os.Stdout(%v)", sourceStream, os.Stdout)
+		_, err := io.Copy(os.Stdout, *sourceStream)
+		return err
+	}
+
+	// destination is a path on localhost
+	log.Debugf("Untaring to local path: %s", destination)
+	err := archive.Untar(*sourceStream, destination, &archive.TarOptions{NoLchown: true})
+	return err
+}
+
+func runCp(cmd *Command, args []string) {
+	if cpHelp {
+		cmd.PrintUsage()
+	}
+	if len(args) != 2 {
+		cmd.PrintShortUsage()
+	}
+
+	if strings.Count(args[0], ":") > 1 || strings.Count(args[1], ":") > 1 {
+		log.Fatalf("usage: scw %s", cmd.UsageLine)
+	}
+
+	sourceStream, err := TarFromSource(cmd.API, args[0])
+	if err != nil {
+		log.Fatalf("Cannot tar from source '%s': %v", args[0], err)
+	}
+
+	err = UntarToDest(cmd.API, sourceStream, args[1])
+	if err != nil {
+		log.Fatalf("Cannot untar to destionation '%s': %v", args[1], err)
 	}
 }
