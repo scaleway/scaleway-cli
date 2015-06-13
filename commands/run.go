8e106db66efb35b5ddb1d2a8826c421457bd49b9
Manfred Touron <m@42.am>
2015-06-13 10:15:09 +0200

    Calling exec for default shell in scw run

--- commands/run.go
+++ commands/run.go
@@ -75,7 +75,7 @@ func runRun(cmd *types.Command, args []string) {
 	// exec -w SERVER COMMAND ARGS...
 	log.Debugf("Executing command")
 	if len(args) < 2 {
-		err = utils.SSHExec(server.PublicAddress.IP, []string{"if [ -x /bin/bash ]; then /bin/bash; else /bin/sh; fi"}, false)
+		err = utils.SSHExec(server.PublicAddress.IP, []string{"if [ -x /bin/bash ]; then exec /bin/bash; else exec /bin/sh; fi"}, false)
 	} else {
 		err = utils.SSHExec(server.PublicAddress.IP, args[1:], false)
 	}
