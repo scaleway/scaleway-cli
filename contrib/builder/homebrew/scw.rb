b966a610dddde585d10b251dbbf0bef4d1762251
Manfred Touron <m@42.am>
2015-07-20 14:14:02 +0200

    Removed Godep dependency

--- contrib/builder/homebrew/scw.rb
+++ contrib/builder/homebrew/scw.rb
@@ -10,22 +10,6 @@ class Scw < Formula
 
   depends_on "go" => :build
 
-  go_resource "github.com/tools/godep" do
-    url "https://github.com/tools/godep.git", :revision => "58d90f262c13357d3203e67a33c6f7a9382f9223"
-  end
-
-  go_resource "github.com/kr/fs" do
-    url "https://github.com/kr/fs.git", :revision => "2788f0dbd16903de03cb8186e5c7d97b69ad387b"
-  end
-
-  go_resource "golang.org/x/tools" do
-    url "https://github.com/golang/tools.git", :revision => "473fd854f8276c0b22f17fb458aa8f1a0e2cf5f5"
-  end
-
-  go_resource "golang.org/x/crypto" do
-    url "https://github.com/golang/crypto.git", :revision => "8b27f58b78dbd60e9a26b60b0d908ea642974b6d"
-  end
-
   def install
     ENV["GOPATH"] = buildpath
     ENV["CGO_ENABLED"] = "0"
@@ -35,11 +19,7 @@ class Scw < Formula
     ln_s buildpath, buildpath/"src/github.com/scaleway/scaleway-cli"
     Language::Go.stage_deps resources, buildpath/"src"
 
-    cd "src/github.com/tools/godep" do
-      system "go", "install"
-    end
-
-    system "./bin/godep", "go", "build", "-o", "scw"
+    system "go", "build", "-o", "scw", "."
     bin.install "scw"
 
     bash_completion.install "contrib/completion/bash/scw"
