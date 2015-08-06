864ee31273ef03065bf03756af24917841961218
Manfred Touron <m@42.am>
2015-08-06 17:57:35 +0200

    Refactored 'info' command (#80)

--- pkg/cli/info.go
+++ pkg/cli/info.go
@@ -5,13 +5,8 @@
 package cli
 
 import (
-	"fmt"
-	"os"
-	"runtime"
-
-	"github.com/scaleway/scaleway-cli/vendor/github.com/kardianos/osext"
-
-	"github.com/scaleway/scaleway-cli/pkg/utils"
+	"github.com/scaleway/scaleway-cli/pkg/commands"
+	"github.com/scaleway/scaleway-cli/vendor/github.com/Sirupsen/logrus"
 )
 
 var cmdInfo = &Command{
@@ -28,34 +23,18 @@ func init() {
 // Flags
 var infoHelp bool // -h, --help flag
 
-func runInfo(cmd *Command, args []string) {
+func runInfo(cmd *Command, rawArgs []string) {
 	if infoHelp {
 		cmd.PrintUsage()
 	}
-	if len(args) != 0 {
+	if len(rawArgs) != 0 {
 		cmd.PrintShortUsage()
 	}
 
-	// FIXME: fmt.Printf("Servers: %s\n", "quantity")
-	// FIXME: fmt.Printf("Images: %s\n", "quantity")
-	fmt.Printf("Debug mode (client): %v\n", os.Getenv("DEBUG") != "")
-
-	fmt.Printf("Organization: %s\n", cmd.API.Organization)
-	// FIXME: add partially-masked token
-	fmt.Printf("API Endpoint: %s\n", os.Getenv("scaleway_api_endpoint"))
-	configPath, _ := utils.GetConfigFilePath()
-	fmt.Printf("RC file: %s\n", configPath)
-	fmt.Printf("User: %s\n", os.Getenv("USER"))
-	fmt.Printf("CPUs: %d\n", runtime.NumCPU())
-	hostname, _ := os.Hostname()
-	fmt.Printf("Hostname: %s\n", hostname)
-	cliPath, _ := osext.Executable()
-	fmt.Printf("CLI Path: %s\n", cliPath)
-
-	fmt.Printf("Cache: %s\n", cmd.API.Cache.Path)
-	fmt.Printf("  Servers: %d\n", cmd.API.Cache.GetNbServers())
-	fmt.Printf("  Images: %d\n", cmd.API.Cache.GetNbImages())
-	fmt.Printf("  Snapshots: %d\n", cmd.API.Cache.GetNbSnapshots())
-	fmt.Printf("  Volumes: %d\n", cmd.API.Cache.GetNbVolumes())
-	fmt.Printf("  Bootscripts: %d\n", cmd.API.Cache.GetNbBootscripts())
+	args := commands.InfoArgs{}
+	ctx := cmd.GetContext(rawArgs)
+	err := commands.RunInfo(ctx, args)
+	if err != nil {
+		logrus.Fatalf("Cannot execute 'info': %v", err)
+	}
 }
