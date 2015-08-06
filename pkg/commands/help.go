2e40afc054b22530d2f4dd62b61afaf4578bf1df
Manfred Touron <m@42.am>
2015-08-06 17:39:34 +0200

    Refactored 'help' command (#80)

---
+++ pkg/commands/help.go
@@ -0,0 +1,7 @@
+// Copyright (C) 2015 Scaleway. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE.md file.
+
+package commands
+
+// nothing here
