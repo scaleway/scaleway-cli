a5230fb17c6638ce6f232b32187f9d5509b78936
Quentin Perez <qperez@ocs.online.net>
2016-07-13 11:43:04 +0200

    _ips: add --detach flag

--- pkg/cli/x_ips.go
+++ pkg/cli/x_ips.go
@@ -6,7 +6,7 @@ package cli
 
 var cmdIPS = &Command{
 	Exec:      runIPS,
-	UsageLine: "_ips [--new|--attach|--delete] [IP_ID [SERVER_ID]]",
+	UsageLine: "_ips [OPTIONS] [IP_ID [SERVER_ID]]",
 
 	Description: "Interacts with your IPs",
 	Hidden:      true,
@@ -17,6 +17,7 @@ var cmdIPS = &Command{
     $ scw _ips --new
     $ scw _ips --attach IP_ID SERVER_ID
     $ scw _ips --delete IP_ID
+    $ scw _ips --detach IP_ID
 `,
 }
 
@@ -24,12 +25,14 @@ func init() {
 	cmdIPS.Flag.BoolVar(&ipHelp, []string{"h", "-help"}, false, "Print usage")
 	cmdIPS.Flag.BoolVar(&ipNew, []string{"n", "-new"}, false, "Add a new IP")
 	cmdIPS.Flag.BoolVar(&ipAttach, []string{"a", "-attach"}, false, "Attach an IP to a server")
+	cmdIPS.Flag.BoolVar(&ipDetach, []string{"-detach"}, false, "Detach an IP from a server")
 	cmdIPS.Flag.StringVar(&ipDelete, []string{"d", "-delete"}, "", "Detele an IP")
 }
 
 var ipHelp bool     // -h, --help flag
 var ipNew bool      // -n, --new flag
 var ipAttach bool   // -a, --attach flag
+var ipDetach bool   // --detach flag
 var ipDelete string // -d, --delete flag
 
 func runIPS(cmd *Command, args []string) error {
@@ -52,6 +55,9 @@ func runIPS(cmd *Command, args []string) error {
 			return cmd.PrintShortUsage()
 		}
 		return cmd.API.AttachIP(args[0], args[1])
+	}
+	if ipDetach {
+		return cmd.API.DetachIP(args[0])
 	}
 	if len(args) == 1 {
 		ip, err := cmd.API.GetIP(args[0])
