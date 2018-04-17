97a187827435074a3a4e54b7f644f9eb0d007b05
Rémy Léone <rleone@online.net>
2018-04-17 13:38:47 +0200

    Remove redundant aliases

--- pkg/cli/x_completion.go
+++ pkg/cli/x_completion.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 
 	"github.com/scaleway/scaleway-cli/pkg/api"
-	utils "github.com/scaleway/scaleway-cli/pkg/utils"
+	"github.com/scaleway/scaleway-cli/pkg/utils"
 )
 
 var cmdCompletion = &Command{
