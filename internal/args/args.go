4b7606a944aa92475e625a1e60d0ae2c7b498fc7
Olivier CANO <ocano@scaleway.com>
2020-01-07 18:02:31 +0100

    fix(args): address golang CI comment

--- internal/args/args.go
+++ internal/args/args.go
@@ -7,7 +7,7 @@ import (
 )
 
 // validArgNameRegex regex to check that args words are lower-case or digit starting and ending with a letter.
-var validArgNameRegex = regexp.MustCompile("^([a-z][a-z0-9-]*)(\\.[a-z0-9-]*)*$")
+var validArgNameRegex = regexp.MustCompile(`^([a-z][a-z0-9-]*)(\.[a-z0-9-]*)*$`)
 
 // RawArgs allows to retrieve a simple []string using UnmarshalStruct()
 type RawArgs []string
