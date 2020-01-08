7473b833f1c1b6670d6c2fe6daf4c508374c5744
Loïc Bourgois <lbourgois@scaleway.com>
2020-01-08 09:38:11 +0100

    wip

--- internal/namespaces/instance/v1/custom_placement_group.go
+++ internal/namespaces/instance/v1/custom_placement_group.go
@@ -2,6 +2,7 @@ package instance
 
 import (
 	"context"
+	"reflect"
 
 	"github.com/scaleway/scaleway-cli/internal/core"
 	"github.com/scaleway/scaleway-sdk-go/api/instance/v1"
@@ -47,4 +48,60 @@ func placementGroupGetBuilder(c *core.Command) *core.Command {
 	}
 
 	return c
+}
+
+// Completes instanceServerUpdate()
+func updateInstanceServerUpdate(c *core.Command) {
+
+	// See
+	type instanceUpdateServerRequest struct {
+		ServerID          string
+		Name              string
+		BootType          string
+		Tags              []string
+		Volumes           []string
+		Bootscript        string
+		DynamicIPRequired bool
+		EnableIPv6        bool
+		Protected         bool
+		SecurityGroupID   string
+		PlacementGroupID  string
+		IP                string
+	}
+
+	ipArgSpec := &core.ArgSpec{
+		Name:       "ip",
+		Short:      `IP`,
+		Required:   true,
+		EnumValues: []string{},
+	}
+	c.ArgSpecs = append(c.ArgSpecs, ipArgSpec)
+	c.ArgsType = reflect.TypeOf(instanceUpdateServerRequest{})
+	c.Run = func(ctx context.Context, argsI interface{}) (i interface{}, e error) {
+
+		args := argsI.(instanceUpdateServerRequest)
+
+		updateServerRequest := &instance.UpdateServerRequest{
+			ServerID: args.ServerID,
+		}
+
+		updateIPRequest := &instance.UpdateIPRequest{
+			IP      string               `json:"-"`
+		}
+
+		client := core.ExtractClient(ctx)
+		api := instance.NewAPI(client)
+
+		_, err := api.UpdateServer(updateServerRequest)
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = api.UpdateIP(updateIPRequest)
+		if err != nil {
+			return nil, err
+		}
+
+		return "", nil
+	}
 }
