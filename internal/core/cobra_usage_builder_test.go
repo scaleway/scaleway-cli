9870c08d69490a30f335be94f516d27702791348
Olivier CANO <ocano@scaleway.com>
2020-01-13 17:05:27 +0100

    update golend

--- internal/core/cobra_usage_builder_test.go
+++ internal/core/cobra_usage_builder_test.go
@@ -31,12 +31,12 @@ type instanceListServerArgsVolume struct {
 }
 
 func Test_buildUsageArgs(t *testing.T) {
-	want := `  [name]                            Filter all servers who contains this name
-  required                          Useless but required
-  [age=1]                           Filter by age
-  [color=red]                       Filter by color (blue | red | pink | green)
-  [multi-word-arg]                  Useless multi word argument
-  [root-volume.name]                Root volume name
+	want := `  [name]                              Filter all servers who contains this name
+  required                            Useless but required
+  [age=1]                             Filter by age
+  [color=red]                         Filter by color (blue | red | pink | green)
+  [multi-word-arg]                    Useless multi word argument
+  [root-volume.name]                  Root volume name
   [additional-volumes.{index}.name]   Additional volume name`
 
 	got := buildUsageArgs(&Command{
