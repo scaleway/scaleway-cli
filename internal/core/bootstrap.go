c7b1693afac4e64251f258f33649caf12c16d30b
Loïc Bourgois <lbourgois@scaleway.com>
2020-01-20 09:59:46 +0100

    address comments

--- internal/core/bootstrap.go
+++ internal/core/bootstrap.go
@@ -76,7 +76,6 @@ func Bootstrap(config *BootstrapConfig) (exitCode int) {
 	case *interactive.InterruptError:
 		return 130
 	}
-	fmt.Println("e", err)
 
 	if err != nil {
 		err = m.Printer.Print(err, nil)
