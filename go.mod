208df14effd20fc3855c67f07fb386240572cfb9
Olivier CANO <ocano@scaleway.com>
2020-01-30 11:16:35 +0100

    feat(sentry): send report on panic

--- go.mod
+++ go.mod
@@ -6,16 +6,19 @@ require (
 	github.com/alecthomas/assert v0.0.0-20170929043011-405dbfeb8e38
 	github.com/alecthomas/colour v0.0.0-20160524082231-60882d9e2721 // indirect
 	github.com/alecthomas/repr v0.0.0-20181024024818-d37bc2a10ba1 // indirect
+	github.com/certifi/gocertifi v0.0.0-20200104152315-a6d78f326758 // indirect
 	github.com/chzyer/logex v1.1.10 // indirect
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e
 	github.com/chzyer/test v0.0.0-20180213035817-a1ea475d72b1 // indirect
 	github.com/dnaeon/go-vcr v1.0.1
 	github.com/dustin/go-humanize v1.0.0
 	github.com/fatih/color v1.7.0
+	github.com/getsentry/raven-go v0.2.0
 	github.com/hashicorp/go-multierror v1.0.0
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/mattn/go-isatty v0.0.9
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/scaleway/scaleway-sdk-go v1.0.0-beta.5.0.20200129095835-23f29e96b36e
 	github.com/sergi/go-diff v1.0.0 // indirect
 	github.com/spf13/cobra v0.0.5
---
+++ internal/sentry/sentry.go
@@ -0,0 +1,61 @@
+package sentry
+
+import (
+	"fmt"
+
+	"github.com/getsentry/raven-go"
+	"github.com/scaleway/scaleway-cli/internal/core"
+	"github.com/scaleway/scaleway-sdk-go/logger"
+)
+
+const (
+	dsn = "https://a3d5d1ef6ae94810952ab245ce61af17@sentry.internal.scaleway.com/186"
+)
+
+// RecoverPanicAndSendReport will recover error if any, log them, and send them to sentry.
+// It must be called with the defer built-in.
+func RecoverPanicAndSendReport(buildInfo *core.BuildInfo) {
+	e := recover()
+
+	if e == nil {
+		return
+	}
+
+	sentryClient, err := newSentryClient(buildInfo)
+	if err != nil {
+		logger.Debugf("cannot create sentry client: %s", err)
+	}
+
+	err, isError := e.(error)
+	if !isError {
+		logAndSentry(sentryClient, err)
+	} else {
+		logAndSentry(sentryClient, fmt.Errorf("unknownw error: %v", e))
+	}
+}
+
+func logAndSentry(sentryClient *raven.Client, err error) {
+	logger.Errorf("%s", err)
+	if sentryClient != nil {
+		logger.Debugf("sending sentry report: %s", sentryClient.CaptureErrorAndWait(err, nil))
+	}
+}
+
+// newSentryClient create a sentry client with build info tags.
+func newSentryClient(buildInfo *core.BuildInfo) (*raven.Client, error) {
+	client, err := raven.New(dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	tagsContext := map[string]string{
+		"version":    buildInfo.Version,
+		"go_arch":    buildInfo.GoArch,
+		"go_os":      buildInfo.GoOS,
+		"go_version": buildInfo.GoVersion,
+	}
+
+	client.SetTagsContext(tagsContext)
+
+	return client, nil
+}
