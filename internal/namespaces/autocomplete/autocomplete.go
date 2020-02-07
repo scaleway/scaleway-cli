79ca8d7e5fc5f949bc2dd75b895f9a8da40918a2
Loïc Bourgois <lbourgois@scaleway.com>
2020-02-07 14:37:27 +0100

    fix: proper file descriptor for shell configuration

--- internal/namespaces/autocomplete/autocomplete.go
+++ internal/namespaces/autocomplete/autocomplete.go
@@ -182,7 +182,7 @@ func InstallCommandRun(ctx context.Context, argsI interface{}) (i interface{}, e
 	}
 
 	// If the file doesn't exist, create it
-	f, err := os.OpenFile(shellConfigurationFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f, err := os.OpenFile(shellConfigurationFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if f != nil {
 		defer f.Close()
 	}
