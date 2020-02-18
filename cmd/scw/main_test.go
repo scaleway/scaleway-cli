f4d01f4fce2bf15dec59b39680977d98da8ccb26
Loïc Bourgois <lbourgois@scaleway.com>
2020-02-18 16:40:32 +0100

    fix

--- cmd/scw/main_test.go
+++ cmd/scw/main_test.go
@@ -18,8 +18,19 @@ func Test_MainUsage(t *testing.T) {
 }
 
 func Test_AllUsage(t *testing.T) {
+
+	automatedTestExceptions := map[string]bool{
+		"init":                true,
+		"config":              true,
+		"autocomplete script": true,
+	}
+
 	for _, command := range getCommands().GetAll() {
 		commandLine := command.GetCommandLine()
+		if _, exists := automatedTestExceptions[commandLine]; exists {
+			continue
+		}
+
 		t.Run(commandLine+" usage", core.Test(&core.TestConfig{
 			Commands: getCommands(),
 			Cmd:      "scw " + commandLine + " -h",
