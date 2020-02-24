4468d6adc68a4263766a58d992bd71a67dfac79e
Loïc Bourgois <lbourgois@scaleway.com>
2020-02-24 15:21:12 +0100

    add tests

---
+++ internal/core/cobra_utils_test.go
@@ -0,0 +1,48 @@
+package core
+
+import (
+	"context"
+	"reflect"
+	"testing"
+)
+
+type testType struct {
+	Name string
+}
+
+func testGetCommands() *Commands {
+	return NewCommands(
+		&Command{
+			Namespace: "test",
+			ArgSpecs: ArgSpecs{
+				{
+					Name: "name",
+				},
+			},
+			ArgsType: reflect.TypeOf(testType{}),
+			Run: func(ctx context.Context, argsI interface{}) (i interface{}, e error) {
+				return "", nil
+			},
+		},
+	)
+}
+
+func Test_handleUnmarshalErrors(t *testing.T) {
+	t.Run("underscore", Test(&TestConfig{
+		Commands: testGetCommands(),
+		Cmd:      "scw test ubuntu_bionic",
+		Check: TestCheckCombine(
+			TestCheckGolden(),
+			TestCheckExitCode(1),
+		),
+	}))
+
+	t.Run("no underscore", Test(&TestConfig{
+		Commands: testGetCommands(),
+		Cmd:      "scw test ubuntu-bionic",
+		Check: TestCheckCombine(
+			TestCheckGolden(),
+			TestCheckExitCode(1),
+		),
+	}))
+}
