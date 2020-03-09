5cc3971e15f70c5ba8a1f73cc04fc5f441a7d4ea
Rémy Léone <rleone@scaleway.com>
2020-03-09 09:51:22 +0100

    FIx

--- internal/namespaces/instance/v1/securitygroup_test.go
+++ internal/namespaces/instance/v1/securitygroup_test.go
@@ -7,12 +7,6 @@ import (
 )
 
 func Test_SecurityGroupClear(t *testing.T) {
-	t.Run("Usage", core.Test(&core.TestConfig{
-		Commands: GetCommands(),
-		Cmd:      "scw instance security-group clear -h",
-		Check:    core.TestCheckGolden(),
-	}))
-
 	t.Run("Simple", core.Test(&core.TestConfig{
 		Commands: GetCommands(),
 		Cmd:      "scw instance security-group clear",
@@ -21,12 +15,6 @@ func Test_SecurityGroupClear(t *testing.T) {
 }
 
 func Test_SecurityGroupCreate(t *testing.T) {
-	t.Run("Usage", core.Test(&core.TestConfig{
-		Commands: GetCommands(),
-		Cmd:      "scw instance security-group create -h",
-		Check:    core.TestCheckGolden(),
-	}))
-
 	t.Run("Simple", core.Test(&core.TestConfig{
 		Commands: GetCommands(),
 		Cmd:      "scw instance security-group create",
@@ -35,12 +23,6 @@ func Test_SecurityGroupCreate(t *testing.T) {
 }
 
 func Test_SecurityGroupDelete(t *testing.T) {
-	t.Run("Usage", core.Test(&core.TestConfig{
-		Commands: GetCommands(),
-		Cmd:      "scw instance security-group delete -h",
-		Check:    core.TestCheckGolden(),
-	}))
-
 	t.Run("Simple", core.Test(&core.TestConfig{
 		Commands: GetCommands(),
 		Cmd:      "scw instance security-group delete",
@@ -49,12 +31,6 @@ func Test_SecurityGroupDelete(t *testing.T) {
 }
 
 func Test_SecurityGroupGet(t *testing.T) {
-	t.Run("Usage", core.Test(&core.TestConfig{
-		Commands: GetCommands(),
-		Cmd:      "scw instance security-group get -h",
-		Check:    core.TestCheckGolden(),
-	}))
-
 	t.Run("Simple", core.Test(&core.TestConfig{
 		Commands: GetCommands(),
 		Cmd:      "scw instance security-group get",
@@ -63,12 +39,6 @@ func Test_SecurityGroupGet(t *testing.T) {
 }
 
 func Test_SecurityGroupList(t *testing.T) {
-	t.Run("Usage", core.Test(&core.TestConfig{
-		Commands: GetCommands(),
-		Cmd:      "scw instance security-group list -h",
-		Check:    core.TestCheckGolden(),
-	}))
-
 	t.Run("Simple", core.Test(&core.TestConfig{
 		Commands: GetCommands(),
 		Cmd:      "scw instance security-group list",
@@ -77,12 +47,6 @@ func Test_SecurityGroupList(t *testing.T) {
 }
 
 func Test_SecurityGroupUpdate(t *testing.T) {
-	t.Run("Usage", core.Test(&core.TestConfig{
-		Commands: GetCommands(),
-		Cmd:      "scw instance security-group update -h",
-		Check:    core.TestCheckGolden(),
-	}))
-
 	t.Run("Simple", core.Test(&core.TestConfig{
 		Commands: GetCommands(),
 		Cmd:      "scw instance security-group update",
