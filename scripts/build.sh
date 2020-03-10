6d0b43a9439ac88a1e455edaaafbf03f6c105bf0
Olivier Cano <kindermoumoute@users.noreply.github.com>
2020-03-10 15:48:12 +0100

    fix(script): add .exe on windows (#762)

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,9 +15,9 @@ LDFLAGS=(
    -X main.BuildDate="$(date -u '+%Y-%m-%dT%I:%M:%S%p')"
 )
 
-VERSION=$(go run cmd/scw/main.go -o json version | jq -r .version)
+VERSION=$(go run cmd/scw/main.go -o json version | jq -r .version | tr . -)
 
 export CGO_ENABLED=0
 GOOS=linux  GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_DIR/scw-$VERSION-linux-x86_64"  cmd/scw/main.go
 GOOS=darwin GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_DIR/scw-$VERSION-darwin-x86_64" cmd/scw/main.go
-GOOS=windows GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_DIR/scw-$VERSION-windows-x86_64" cmd/scw/main.go
+GOOS=windows GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_DIR/scw-$VERSION-windows-x86_64.exe" cmd/scw/main.go
