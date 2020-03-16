e8edbec7d6bb5487c04940f8cc3b12abcdd0acf0
Patrik Cyvoct <pcyvoct@scaleway.com>
2020-03-16 18:18:19 +0100

    fix retry interval
    Signed-off-by: Patrik Cyvoct <pcyvoct@scaleway.com>

--- internal/namespaces/k8s/v1beta4/helpers_test.go
+++ internal/namespaces/k8s/v1beta4/helpers_test.go
@@ -8,6 +8,12 @@ import (
 	k8s "github.com/scaleway/scaleway-sdk-go/api/k8s/v1beta4"
 )
 
+func init() {
+	if !core.UpdateCassettes {
+		k8s.RetryInterval = 0
+	}
+}
+
 const (
 	kapsuleVersion = "1.17.3"
 )
