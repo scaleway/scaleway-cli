ec45e1026dfa7a0b5614d56b685b09f3e3b11421
Rémy Léone <rleone@scaleway.com>
2020-03-17 18:33:54 +0100

    Fix

--- internal/namespaces/baremetal/v1alpha1/custom_server.go
+++ internal/namespaces/baremetal/v1alpha1/custom_server.go
@@ -1,7 +1,33 @@
 package baremetal
 
-import "time"
+import (
+	"context"
+	"time"
+
+	"github.com/scaleway/scaleway-cli/internal/core"
+	baremetal "github.com/scaleway/scaleway-sdk-go/api/baremetal/v1alpha1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+)
 
 const (
 	serverActionTimeout = 10 * time.Minute
 )
+
+const (
+	serverActionReboot = iota
+)
+
+func waitForServerFunc(action int) core.WaitFunc {
+	return func(ctx context.Context, _, respI interface{}) (interface{}, error) {
+		server, err := baremetal.NewAPI(core.ExtractClient(ctx)).WaitForServer(&baremetal.WaitForServerRequest{
+			ServerID: respI.(*baremetal.Server).ID,
+			Zone:     respI.(*baremetal.Server).Zone,
+			Timeout:  scw.DurationPtr(serverActionTimeout),
+		})
+		switch action {
+		case serverActionReboot:
+			return server, err
+		}
+		return nil, err
+	}
+}
