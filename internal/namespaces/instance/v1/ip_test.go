5af22729ebb5a95c14651058e0d83a3dcec877f9
Rémy Léone <rleone@scaleway.com>
2020-03-18 13:41:24 +0100

    feat(instance): add ip testing (#747)
    * feat(instance): add ip testing

---
+++ internal/namespaces/instance/v1/ip_test.go
@@ -0,0 +1,74 @@
+package instance
+
+import (
+	"testing"
+
+	"github.com/scaleway/scaleway-cli/internal/core"
+	"github.com/scaleway/scaleway-sdk-go/api/instance/v1"
+)
+
+func Test_IpCreate(t *testing.T) {
+	t.Run("Simple", core.Test(&core.TestConfig{
+		Commands: GetCommands(),
+		Cmd:      "scw instance ip create",
+		Check: core.TestCheckCombine(
+			core.TestCheckGolden(),
+			core.TestCheckExitCode(0),
+		),
+		AfterFunc: func(ctx *core.AfterFuncCtx) error {
+			ctx.ExecuteCmd("scw instance ip delete ip=" + ctx.CmdResult.(*instance.CreateIPResponse).IP.ID)
+			return nil
+		},
+	}))
+}
+
+func Test_IpDelete(t *testing.T) {
+	t.Run("Simple", core.Test(&core.TestConfig{
+		BeforeFunc: createIP("Ip"),
+		Commands:   GetCommands(),
+		Cmd:        "scw instance ip delete ip={{ .Ip.ID }}",
+		Check: core.TestCheckCombine(
+			core.TestCheckGolden(),
+			core.TestCheckExitCode(0),
+		),
+	}))
+}
+
+func Test_IpGet(t *testing.T) {
+	t.Run("Simple", core.Test(&core.TestConfig{
+		BeforeFunc: createIP("Ip"),
+		Commands:   GetCommands(),
+		Cmd:        "scw instance ip get ip={{ .Ip.ID }}",
+		Check: core.TestCheckCombine(
+			core.TestCheckGolden(),
+			core.TestCheckExitCode(0),
+		),
+		AfterFunc: deleteIP("Ip"),
+	}))
+}
+
+func Test_IpList(t *testing.T) {
+	t.Run("Simple", core.Test(&core.TestConfig{
+		BeforeFunc: createIP("Ip"),
+		Commands:   GetCommands(),
+		Cmd:        "scw instance ip list",
+		Check: core.TestCheckCombine(
+			core.TestCheckGolden(),
+			core.TestCheckExitCode(0),
+		),
+		AfterFunc: deleteIP("Ip"),
+	}))
+}
+
+func Test_IpUpdate(t *testing.T) {
+	t.Run("Simple", core.Test(&core.TestConfig{
+		BeforeFunc: createIP("Ip"),
+		Commands:   GetCommands(),
+		Cmd:        "scw instance ip update ip={{ .Ip.ID }}",
+		Check: core.TestCheckCombine(
+			core.TestCheckGolden(),
+			core.TestCheckExitCode(0),
+		),
+		AfterFunc: deleteIP("Ip"),
+	}))
+}
