c6125254729e8a47b7e345ba50076933206834d6
Patrik Cyvoct <pcyvoct@scaleway.com>
2020-03-19 11:29:54 +0100

    use build script in Dockerfile
    Signed-off-by: Patrik Cyvoct <pcyvoct@scaleway.com>

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM golang:1.14-alpine as builder
 
 # ca-certificates is needed to add the certificates on the next image
 # since it's FROM scratch, it does not have any certificates
-RUN apk update && apk add --no-cache git ca-certificates && update-ca-certificates
+# bash is needed to run the build script
+RUN apk update && apk add --no-cache bash git ca-certificates && update-ca-certificates
 
 WORKDIR /go/src/github.com/scaleway/scaleway-cli
 
@@ -10,11 +11,12 @@ COPY go.mod go.mod
 COPY go.sum go.sum
 RUN go mod download
 
+COPY scripts/ scripts/
 COPY cmd/ cmd/
 COPY internal/ internal/
 COPY .git/ .git/
 
-RUN CGO_ENABLED=0 GOOS=${TARGETOS} GOARCH=${TARGETARCH} go build -a -ldflags "-w -extldflags -static -X main.GitCommit=$(git rev-parse --short HEAD) -X main.GitBranch=$(git symbolic-ref -q --short HEAD || echo HEAD) -X main.BuildDate=$(date -u '+%Y-%m-%dT%I:%M:%S%p')" ./cmd/scw
+RUN bash scripts/build.sh build-in-docker
 
 FROM scratch
 WORKDIR /
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
-BIN_DIR="$ROOT_DIR/bin"
-
-mkdir -p $BIN_DIR
 
 LDFLAGS=(
    -w
@@ -15,9 +10,20 @@ LDFLAGS=(
    -X main.BuildDate="$(date -u '+%Y-%m-%dT%I:%M:%S%p')"
 )
 
-VERSION=$(go run cmd/scw/main.go -o json version | jq -r .version | tr . -)
-
 export CGO_ENABLED=0
-GOOS=linux  GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_DIR/scw-$VERSION-linux-x86_64"  cmd/scw/main.go
-GOOS=darwin GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_DIR/scw-$VERSION-darwin-x86_64" cmd/scw/main.go
-GOOS=windows GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_DIR/scw-$VERSION-windows-x86_64.exe" cmd/scw/main.go
+
+if [[ "${1}" == "build-in-docker" ]]; then
+    GOOS=${TARGETOS} GOARCH=${TARGETARCH} go build -ldflags "${LDFLAGS[*]}" ./cmd/scw
+else
+    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
+    BIN_DIR="$ROOT_DIR/bin"
+
+    mkdir -p $BIN_DIR
+    
+    VERSION=$(go run cmd/scw/main.go -o json version | jq -r .version | tr . -)
+    
+    GOOS=linux  GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_DIR/scw-$VERSION-linux-x86_64"  cmd/scw/main.go
+    GOOS=darwin GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_DIR/scw-$VERSION-darwin-x86_64" cmd/scw/main.go
+    GOOS=windows GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_DIR/scw-$VERSION-windows-x86_64.exe" cmd/scw/main.go
+fi
