54b95c43a4eb806a643e47fb67461f91af67f57c
loicbourgois <lbourgois@scaleway.com>
2020-03-20 16:13:26 +0100

    wip: test 2

--- internal/namespaces/account/custom_test.go
+++ internal/namespaces/account/custom_test.go
@@ -13,10 +13,7 @@ func Test_initCommand(t *testing.T) {
 	t.Run("simple", core.Test(&core.TestConfig{
 		Commands: GetCommands(),
 		BeforeFunc: func(ctx *core.BeforeFuncCtx) error {
-			homeDir, err := os.UserHomeDir()
-			if err != nil {
-				return err
-			}
+			homeDir := os.Getenv("HOME")
 			path_ := path.Join(homeDir, "/.ssh/id_rsa.pub")
 			if _, err := os.Stat(path_); os.IsNotExist(err) {
 				content := "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/wF8Q8LjEexuWDc8TfKmWVZ1CiiHK6KvO0E/Rk9+d6ssqbrvtbJWRsJXZFC8+DGWVM0UFFicmOfTwEjDWzuQPFkYhmpXrD1UiLx9Viku1g1qEJgcsyH2uAwwW3OnsH1W44D6Ni/zOzMButFeKZgPeD8H9YNkpbZBZ9QrKFiAhvEyJDYSY0bsbH1/qR5DE+dLNuGlJ/g3kUMVaXSI6dHNcBHTbK0Mse23Uopk2U3BSpvX9JdbcLaYtHDOytwd16rNYui7el3uOmlR8oUpAXkeKQxBPoxgy3qI/P8/l44L9RFpbklkmdiw2ph2ymiSkRSYCWdvEVIK/A+0D8VFjGXOb email"
