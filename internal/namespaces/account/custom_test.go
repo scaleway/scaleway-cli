34a19fc10f239e6e470289b548a448c798331632
loicbourgois <lbourgois@scaleway.com>
2020-03-20 16:04:20 +0100

    wip: test

--- internal/namespaces/account/custom_test.go
+++ internal/namespaces/account/custom_test.go
@@ -20,7 +20,10 @@ func Test_initCommand(t *testing.T) {
 			path_ := path.Join(homeDir, "/.ssh/id_rsa.pub")
 			if _, err := os.Stat(path_); os.IsNotExist(err) {
 				content := "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/wF8Q8LjEexuWDc8TfKmWVZ1CiiHK6KvO0E/Rk9+d6ssqbrvtbJWRsJXZFC8+DGWVM0UFFicmOfTwEjDWzuQPFkYhmpXrD1UiLx9Viku1g1qEJgcsyH2uAwwW3OnsH1W44D6Ni/zOzMButFeKZgPeD8H9YNkpbZBZ9QrKFiAhvEyJDYSY0bsbH1/qR5DE+dLNuGlJ/g3kUMVaXSI6dHNcBHTbK0Mse23Uopk2U3BSpvX9JdbcLaYtHDOytwd16rNYui7el3uOmlR8oUpAXkeKQxBPoxgy3qI/P8/l44L9RFpbklkmdiw2ph2ymiSkRSYCWdvEVIK/A+0D8VFjGXOb email"
-				_ = os.MkdirAll(path.Join(homeDir, "/.ssh"), 0644)
+				err := os.MkdirAll(path.Join(homeDir, "/.ssh"), 0644)
+				if err != nil {
+					return err
+				}
 				err = ioutil.WriteFile(path_, []byte(content), 0644)
 				return err
 			}
