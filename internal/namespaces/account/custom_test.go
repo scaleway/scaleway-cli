21520bf6f5dc6f85e941bf63514fe45aa2d6d138
loicbourgois <lbourgois@scaleway.com>
2020-03-23 18:26:55 +0100

    wip: test

--- internal/namespaces/account/custom_test.go
+++ internal/namespaces/account/custom_test.go
@@ -14,8 +14,8 @@ func Test_initCommand(t *testing.T) {
 		Commands: GetCommands(),
 		BeforeFunc: func(ctx *core.BeforeFuncCtx) error {
 			tmpDir := os.TempDir()
-			pathToPublicKey := path.Join(tmpDir, ".ssh", "id_rsa.pub")
 			pathShort := path.Join(tmpDir, "id_rsa.pub")
+			pathToPublicKey := path.Join(tmpDir, ".ssh", "id_rsa.pub")
 			_, err := os.Stat(pathToPublicKey)
 			if err != nil {
 				content := "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/wF8Q8LjEexuWDc8TfKmWVZ1CiiHK6KvO0E/Rk9+d6ssqbrvtbJWRsJXZFC8+DGWVM0UFFicmOfTwEjDWzuQPFkYhmpXrD1UiLx9Viku1g1qEJgcsyH2uAwwW3OnsH1W44D6Ni/zOzMButFeKZgPeD8H9YNkpbZBZ9QrKFiAhvEyJDYSY0bsbH1/qR5DE+dLNuGlJ/g3kUMVaXSI6dHNcBHTbK0Mse23Uopk2U3BSpvX9JdbcLaYtHDOytwd16rNYui7el3uOmlR8oUpAXkeKQxBPoxgy3qI/P8/l44L9RFpbklkmdiw2ph2ymiSkRSYCWdvEVIK/A+0D8VFjGXOb email"
