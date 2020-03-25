d1c4cabe9d0ee2c44ee369b67ac35e2928598582
Jerome Quere <jquere@scaleway.com>
2020-03-25 16:13:06 +0100

    chore: build script now generate SHA256 checksum file (#800)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+export CGO_ENABLED=0
 LDFLAGS=(
    -w
    -extldflags
@@ -10,20 +10,26 @@ LDFLAGS=(
    -X main.BuildDate="$(date -u '+%Y-%m-%dT%I:%M:%S%p')"
 )
 
-export CGO_ENABLED=0
-
+# If we are build from the dockerfile only build required binary
 if [[ "${BUILD_IN_DOCKER}" == "true" ]]; then
     GOOS=${TARGETOS} GOARCH=${TARGETARCH} go build -ldflags "${LDFLAGS[*]}" ./cmd/scw
-else
-    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-    ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
-    BIN_DIR="$ROOT_DIR/bin"
+    exit 0
+ fi
 
-    mkdir -p $BIN_DIR
-    
-    VERSION=$(go run cmd/scw/main.go -o json version | jq -r .version | tr . -)
-    
-    GOOS=linux  GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_DIR/scw-$VERSION-linux-x86_64"  cmd/scw/main.go
-    GOOS=darwin GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_DIR/scw-$VERSION-darwin-x86_64" cmd/scw/main.go
-    GOOS=windows GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_DIR/scw-$VERSION-windows-x86_64.exe" cmd/scw/main.go
-fi
+
+BIN_DIR="./bin"
+VERSION=$(go run cmd/scw/main.go -o json version | jq -r .version | tr . -)
+BIN_LINUX="$BIN_DIR/scw-$VERSION-linux-x86_64"
+BIN_DARWIN="$BIN_DIR/scw-$VERSION-darwin-x86_64"
+BIN_WINDOWS="$BIN_DIR/scw-$VERSION-windows-x86_64.exe"
+
+mkdir -p $BIN_DIR
+GOOS=linux  GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_LINUX" cmd/scw/main.go
+GOOS=darwin GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_DARWIN" cmd/scw/main.go
+GOOS=windows GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_WINDOWS" cmd/scw/main.go
+
+shasum -a 256 \
+  "$BIN_LINUX" \
+  "$BIN_DARWIN" \
+  "$BIN_WINDOWS" \
+  | sed -e 's#./bin/##' > "$BIN_DIR/SHA256SUMS"
