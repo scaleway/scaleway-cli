e12b7222e6a69a1e2f303bc923365e8aa169d7ce
Olivier CANO <ocano@scaleway.com>
2020-04-03 10:56:35 +0200

    fix typo

--- internal/qa/qa.go
+++ internal/qa/qa.go
@@ -13,7 +13,7 @@ func LintCommands(commands *core.Commands) []interface{} {
 	errors = append(errors, testShortEndWithDotError(commands)...)
 	errors = append(errors, testShortIsNotPresentError(commands)...)
 	errors = append(errors, testArgMustUseDashError(commands)...)
-	errors = append(errors, testPosArgMustBePositionalError(commands)...)
+	errors = append(errors, testPositionalArgMustBeRequiredError(commands)...)
 	errors = append(errors, testExampleCanHaveOnlyOneTypeOfExampleError(commands)...)
 	errors = append(errors, testDifferentLocalizationForNamespaceError(commands)...)
 	return errors
@@ -83,21 +83,21 @@ func testArgMustUseDashError(commands *core.Commands) []interface{} {
 	return errors
 }
 
-type PosArgMustBePositionalError struct {
+type PositionalArgMustBeRequiredError struct {
 	Command *core.Command
 	Argspec *core.ArgSpec
 }
 
-func (err PosArgMustBePositionalError) Error() string {
+func (err PositionalArgMustBeRequiredError) Error() string {
 	return "positional argument must be required '" + err.Command.GetCommandLine() + "', arg '" + err.Argspec.Name + "'"
 }
 
-func testPosArgMustBePositionalError(commands *core.Commands) []interface{} {
+func testPositionalArgMustBeRequiredError(commands *core.Commands) []interface{} {
 	errors := []interface{}(nil)
 	for _, command := range commands.GetAll() {
 		for _, argSpec := range command.ArgSpecs {
 			if argSpec.Positional && !argSpec.Required {
-				errors = append(errors, &PosArgMustBePositionalError{
+				errors = append(errors, &PositionalArgMustBeRequiredError{
 					Command: command,
 					Argspec: argSpec,
 				})
