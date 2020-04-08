570b7f47d39fc193f5209e91e131f47c59bb51ef
Olivier CANO <ocano@scaleway.com>
2020-04-08 19:06:23 +0200

    remove tests

--- internal/namespaces/instance/v1/custom_image_test.go
+++ internal/namespaces/instance/v1/custom_image_test.go
@@ -24,25 +24,4 @@ func Test_ImageCreate(t *testing.T) {
 			deleteSnapshot("Snapshot"),
 		),
 	}))
-
-	t.Run("List images", core.Test(&core.TestConfig{
-		BeforeFunc: core.BeforeFuncCombine(
-			createServer("Server"),
-			core.ExecStoreBeforeCmd("Snapshot", `scw instance snapshot create volume-id={{ (index .Server.Volumes "0").ID }}`),
-			core.ExecStoreBeforeCmd("DeletedServer", `scw instance server delete {{ .Server.ID }} with-ip=true with-volumes=all`),
-		),
-		Commands: GetCommands(),
-		Cmd:      "scw instance image create snapshot-id={{ .Snapshot.Snapshot.ID }} arch=x86_64",
-		Check: core.TestCheckCombine(
-			core.TestCheckGolden(),
-			core.TestCheckExitCode(0),
-		),
-		AfterFunc: core.AfterFuncCombine(
-			func(ctx *core.AfterFuncCtx) error {
-				ctx.ExecuteCmd("scw instance image delete " + ctx.CmdResult.(*instance.CreateImageResponse).Image.ID)
-				return nil
-			},
-			deleteSnapshot("Snapshot"),
-		),
-	}))
 }
