659d26b31e5abec931d68a237dc02bc3530f0dde
Rémy Léone <rleone@scaleway.com>
2020-04-10 11:27:49 +0200

    baremetal: add install command with a wait flag

---
+++ internal/namespaces/baremetal/v1alpha1/custom_server_install.go
@@ -0,0 +1,39 @@
+package baremetal
+
+import (
+	"context"
+
+	"github.com/scaleway/scaleway-cli/internal/core"
+	baremetal "github.com/scaleway/scaleway-sdk-go/api/baremetal/v1alpha1"
+)
+
+func serverInstallBuilder(c *core.Command) *core.Command {
+	c.Examples = []*core.Example{
+		{
+			Short:   "Install an os on a given server",
+			Request: `{"os-id":"11111111-1111-1111-1111-111111111111","server-id":"11111111-1111-1111-1111-111111111111"}`,
+		},
+	}
+
+	c.SeeAlsos = []*core.SeeAlso{
+		{
+			Short:   "List os (Useful to get all os-id)",
+			Command: "scw baremetal os list",
+		},
+		{
+			Short:   "Create a server",
+			Command: "scw baremetal server create",
+		},
+	}
+
+	c.WaitFunc = func(ctx context.Context, argsI, respI interface{}) (interface{}, error) {
+		api := baremetal.NewAPI(core.ExtractClient(ctx))
+		return api.WaitForServerInstall(&baremetal.WaitForServerInstallRequest{
+			Zone:     argsI.(*baremetal.InstallServerRequest).Zone,
+			ServerID: respI.(*baremetal.Server).ID,
+			Timeout:  serverActionTimeout,
+		})
+	}
+
+	return c
+}
