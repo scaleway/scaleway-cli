72a0f3cdda24fbd059933a59f445f9dfa0f02312
Rémy Léone <rleone@scaleway.com>
2020-04-20 19:39:18 +0200

    Fix

--- internal/namespaces/object/v1/custom.go
+++ internal/namespaces/object/v1/custom.go
@@ -5,17 +5,16 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
 	"reflect"
 	"text/template"
 
 	"github.com/scaleway/scaleway-cli/internal/core"
+	"github.com/scaleway/scaleway-cli/internal/interactive"
 	"github.com/scaleway/scaleway-sdk-go/scw"
 )
-
-type getRequest struct {
-	Region scw.Region
-	Type   string
-}
 
 type s3config struct {
 	AccessKey string
@@ -128,6 +127,10 @@ func objectConfig() *core.Command {
 }
 
 func getCommand() *core.Command {
+	type getRequest struct {
+		Region scw.Region
+		Type   string
+	}
 
 	return &core.Command{
 		Namespace: "object",
@@ -143,7 +146,7 @@ func getCommand() *core.Command {
 				Required:   true,
 				EnumValues: []string{"rclone", "s3cmd", "mc"},
 			},
-			core.RegionArgSpec(),
+			core.RegionArgSpec(scw.RegionFrPar, scw.RegionNlAms),
 		},
 		Examples: []*core.Example{
 			{
@@ -213,7 +216,7 @@ func installCommand() *core.Command {
 	}
 	return &core.Command{
 		Namespace: "object",
-		Resource:  "s3config",
+		Resource:  "config",
 		Verb:      "install",
 		Short:     "Install a S3 related configuration file to its default location",
 		Long:      "Install a S3 related configuration file.",
@@ -225,7 +228,7 @@ func installCommand() *core.Command {
 				Required:   true,
 				EnumValues: []string{"rclone", "s3cmd", "mc"},
 			},
-			core.RegionArgSpec(),
+			core.RegionArgSpec(scw.RegionFrPar, scw.RegionNlAms),
 		},
 		Examples: []*core.Example{
 			{
@@ -249,33 +252,110 @@ func installCommand() *core.Command {
 			},
 		},
 		Run: func(ctx context.Context, argsI interface{}) (interface{}, error) {
-			requestedType := argsI.(*getRequest)
+			requestedType := argsI.(*installRequest)
+			client := core.ExtractClient(ctx)
+			region := requestedType.Region.String()
+			if region == "" {
+				defaultRegion, _ := client.GetDefaultRegion()
+				region = defaultRegion.String()
+			}
 			config := s3config{
 				AccessKey: "pouet",
 				SecretKey: "pouet",
-				Region:    requestedType.Region.String(),
+				Region:    region,
 			}
 
 			switch requestedType.Type {
 			case "s3cmd":
-				_, err := config.exportS3cmdConfig()
-				if err != nil {
-					return nil, err
+				i, err2 := installS3cmd(config)
+				if err2 != nil {
+					return i, err2
 				}
+
 			case "rclone":
-				_, err := config.exportRcloneConfig()
-				if err != nil {
-					return nil, err
+				i, err2 := installRclone(config)
+				if err2 != nil {
+					return i, err2
 				}
+
 			case "mc":
-				_, err := config.exportMcConfig()
-				if err != nil {
-					return nil, err
+				i, err2 := installMc(config)
+				if err2 != nil {
+					return i, err2
 				}
+
 			default:
 				fmt.Println("Unknown tool.")
 			}
 			return nil, nil
 		},
 	}
+}
+
+func installMc(config s3config) (interface{}, error) {
+	newConfig, err := config.exportMcConfig()
+	if err != nil {
+		return nil, err
+	}
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	mcConfigPath := path.Join(homeDir, ".mc", "config.json")
+	i, err2, done := ensureFile(mcConfigPath, newConfig)
+	if done {
+		return i, err2
+	}
+	return nil, nil
+}
+
+func ensureFile(mcConfigPath string, newConfig string) (interface{}, error, bool) {
+	// Ask whether to remove previous configuration file if it exists
+	if _, err := os.Stat(mcConfigPath); err == nil {
+		_, err := interactive.PromptBoolWithConfig(&interactive.PromptBoolConfig{
+			Prompt:       "Do you want to overwrite the existing configuration file (" + mcConfigPath + ")?",
+			DefaultValue: false,
+		})
+		if err != nil {
+			return "", err, true
+		}
+		return nil, ioutil.WriteFile(mcConfigPath, []byte(newConfig), 0644), true
+	}
+	return nil, nil, false
+}
+
+func installS3cmd(config s3config) (interface{}, error) {
+	newConfig, err := config.exportS3cmdConfig()
+	if err != nil {
+		return nil, err
+	}
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	s3cmdConfigPath := path.Join(homeDir, ".s3cfg")
+	i, err2, done := ensureFile(s3cmdConfigPath, newConfig)
+	if done {
+		return i, err2
+	}
+	return nil, nil
+}
+
+func installRclone(config s3config) (interface{}, error) {
+	newConfig, err := config.exportRcloneConfig()
+	if err != nil {
+		return nil, err
+	}
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	// `rclone config file` returns the path of the configuration file
+	rcloneConfigPath := path.Join(homeDir, ".config", "rclone", "rclone.conf")
+	i, err2, done := ensureFile(rcloneConfigPath, newConfig)
+	if done {
+		return i, err2
+	}
+
+	return nil, nil
 }
