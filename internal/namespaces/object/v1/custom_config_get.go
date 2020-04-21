176d6638db5814efdd828ef16ca2f9002fbeb977
Rémy Léone <rleone@scaleway.com>
2020-04-21 15:53:40 +0200

    Fix

--- internal/namespaces/object/v1/custom_config_get.go
+++ internal/namespaces/object/v1/custom_config_get.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
 	"reflect"
 	"text/template"
 
@@ -83,8 +82,11 @@ func getCommand() *core.Command {
 				}
 				return res, nil
 			default:
-				fmt.Println("Unknown tool.")
-				return nil, nil
+				return nil, &core.CliError{
+					Message: "",
+					Details: "",
+					Hint:    "",
+				}
 			}
 		},
 	}
--- internal/namespaces/object/v1/custom_config_install.go
+++ internal/namespaces/object/v1/custom_config_install.go
@@ -63,70 +63,74 @@ func installCommand() *core.Command {
 			if err != nil {
 				return "", err
 			}
-
+			var configPath string
 			switch requestedType.Type {
 			case "s3cmd":
-				i, err2 := installS3cmd(config)
-				if err2 != nil {
-					return i, err2
+				configPath, err = installS3cmd(config)
+				if err != nil {
+					return nil, err
 				}
 
 			case "rclone":
-				i, err2 := installRclone(config)
-				if err2 != nil {
-					return i, err2
+				configPath, err = installRclone(config)
+				if err != nil {
+					return nil, err
 				}
 
 			case "mc":
-				i, err2 := installMc(config)
-				if err2 != nil {
-					return i, err2
+				configPath, err = installMc(config)
+				if err != nil {
+					return nil, err
 				}
 
 			default:
-				fmt.Println("Unknown tool.")
+				return nil, &core.CliError{
+					Message: "",
+					Details: "",
+					Hint:    "",
+				}
 			}
-			return nil, nil
+			return fmt.Sprintf("Configuration file successfully installed at %s", configPath), nil
 		},
 	}
 }
 
-func ensureFile(mcConfigPath string, newConfig string) (interface{}, error, bool) {
+func ensureFile(configPath string, newConfig string) error {
 	// Ask whether to remove previous configuration file if it exists
-	if _, err := os.Stat(mcConfigPath); err == nil {
+	if _, err := os.Stat(configPath); err == nil {
 		_, err := interactive.PromptBoolWithConfig(&interactive.PromptBoolConfig{
-			Prompt:       "Do you want to overwrite the existing configuration file (" + mcConfigPath + ")?",
+			Prompt:       "Do you want to overwrite the existing configuration file (" + configPath + ")?",
 			DefaultValue: false,
 		})
 		if err != nil {
-			return "", err, true
+			return err
 		}
-		return nil, ioutil.WriteFile(mcConfigPath, []byte(newConfig), 0644), true
+		return ioutil.WriteFile(configPath, []byte(newConfig), 0644)
 	}
-	return nil, nil, false
+	return ioutil.WriteFile(configPath, []byte(newConfig), 0644)
 }
 
-func installS3cmd(config s3config) (interface{}, error) {
+func installS3cmd(config s3config) (string, error) {
 	newConfig, err := config.exportS3cmdConfig()
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		return "", err
 	}
 	s3cmdConfigPath := path.Join(homeDir, ".s3cfg")
-	i, err2, done := ensureFile(s3cmdConfigPath, newConfig)
-	if done {
-		return i, err2
+	err = ensureFile(s3cmdConfigPath, newConfig)
+	if err != nil {
+		return "", err
 	}
-	return nil, nil
+	return s3cmdConfigPath, nil
 }
 
-func installRclone(config s3config) (interface{}, error) {
+func installRclone(config s3config) (string, error) {
 	newConfig, err := config.exportRcloneConfig()
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
@@ -134,27 +138,26 @@ func installRclone(config s3config) (interface{}, error) {
 	}
 	// `rclone config file` returns the path of the configuration file
 	rcloneConfigPath := path.Join(homeDir, ".config", "rclone", "rclone.conf")
-	i, err2, done := ensureFile(rcloneConfigPath, newConfig)
-	if done {
-		return i, err2
+	err = ensureFile(rcloneConfigPath, newConfig)
+	if err != nil {
+		return "", err
 	}
-
-	return nil, nil
+	return rcloneConfigPath, nil
 }
 
-func installMc(config s3config) (interface{}, error) {
+func installMc(config s3config) (string, error) {
 	newConfig, err := config.exportMcConfig()
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		return "", err
 	}
 	mcConfigPath := path.Join(homeDir, ".mc", "config.json")
-	i, err2, done := ensureFile(mcConfigPath, newConfig)
-	if done {
-		return i, err2
+	err = ensureFile(mcConfigPath, newConfig)
+	if err != nil {
+		return "", err
 	}
-	return nil, nil
+	return mcConfigPath, nil
 }
