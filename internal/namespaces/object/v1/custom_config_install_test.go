546fb6a2b6ec61f77351f05615f4ca2e0543d222
Rémy Léone <rleone@scaleway.com>
2020-04-21 16:43:31 +0200

    Fix

--- internal/namespaces/object/v1/custom_config_install_test.go
+++ internal/namespaces/object/v1/custom_config_install_test.go
@@ -1,30 +1,57 @@
 package object
 
 import (
+	"os"
+	"path"
 	"testing"
+
+	"github.com/scaleway/scaleway-cli/internal/core"
 )
 
 func Test_ConfigInstall(t *testing.T) {
-	t.Run("rclone", func(t *testing.T) {
+	tmpDir := os.TempDir()
 
-		//t.Run("simple", core.Test(&core.TestConfig{
-		//	Commands: GetCommands(),
-		//	Cmd:      "scw object config install type=rclone",
-		//	Check: core.TestCheckCombine(
-		//		// no golden tests since it's os specific
-		//		func(t *testing.T, ctx *core.CheckFuncCtx) {
-		//			testIfKubeconfigInFile(t, path.Join(os.TempDir(), "cli-test"), "-"+ctx.Meta["Cluster"].(*k8s.Cluster).ID, ctx.Meta["Kubeconfig"].(*k8s.Kubeconfig))
-		//		},
-		//		core.TestCheckExitCode(0),
-		//	),
-		//}))
-	})
+	t.Run("NoExistingConfig", func(t *testing.T) {
+		t.Run("rclone", core.Test(&core.TestConfig{
+			Commands: GetCommands(),
+			Cmd:      "scw object config install type=rclone",
+			Check: core.TestCheckCombine(
+				func(t *testing.T, ctx *core.CheckFuncCtx) {
+					os.Stat(path.Join(tmpDir, ".config", "rclone", "rclone.conf"))
+				},
+				core.TestCheckExitCode(0),
+			),
+			OverrideEnv: map[string]string{
+				"HOME": tmpDir,
+			},
+		}))
 
-	t.Run("mc", func(t *testing.T) {
+		t.Run("mc", core.Test(&core.TestConfig{
+			Commands: GetCommands(),
+			Cmd:      "scw object config install type=mc",
+			Check: core.TestCheckCombine(
+				func(t *testing.T, ctx *core.CheckFuncCtx) {
+					os.Stat(path.Join(tmpDir, ".mc", "config.json"))
+				},
+				core.TestCheckExitCode(0),
+			),
+			OverrideEnv: map[string]string{
+				"HOME": tmpDir,
+			},
+		}))
 
-	})
-
-	t.Run("s3cmd", func(t *testing.T) {
-
+		t.Run("s3cmd", core.Test(&core.TestConfig{
+			Commands: GetCommands(),
+			Cmd:      "scw object config install type=s3cmd",
+			Check: core.TestCheckCombine(
+				func(t *testing.T, ctx *core.CheckFuncCtx) {
+					os.Stat(path.Join(tmpDir, ".s3cfg"))
+				},
+				core.TestCheckExitCode(0),
+			),
+			OverrideEnv: map[string]string{
+				"HOME": tmpDir,
+			},
+		}))
 	})
 }
