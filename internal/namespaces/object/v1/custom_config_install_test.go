2845dd980b7ed747b4becb03edfeaa58e79a6f0a
Rémy Léone <rleone@scaleway.com>
2020-04-23 11:43:51 +0200

    Fix

--- internal/namespaces/object/v1/custom_config_install_test.go
+++ internal/namespaces/object/v1/custom_config_install_test.go
@@ -2,7 +2,6 @@ package object
 
 import (
 	"os"
-	"path"
 	"testing"
 
 	"github.com/scaleway/scaleway-cli/internal/core"
@@ -15,15 +14,7 @@ func Test_ConfigInstall(t *testing.T) {
 		t.Run("rclone", core.Test(&core.TestConfig{
 			Commands: GetCommands(),
 			Cmd:      "scw object config install type=rclone",
-			Check: core.TestCheckCombine(
-				func(t *testing.T, ctx *core.CheckFuncCtx) {
-					_, err := os.Stat(path.Join(tmpDir, ".config", "rclone", "rclone.conf"))
-					if err != nil {
-						t.Fail()
-					}
-				},
-				core.TestCheckExitCode(0),
-			),
+			Check:    core.TestCheckExitCode(0),
 			OverrideEnv: map[string]string{
 				"HOME": tmpDir,
 			},
@@ -32,15 +23,7 @@ func Test_ConfigInstall(t *testing.T) {
 		t.Run("mc", core.Test(&core.TestConfig{
 			Commands: GetCommands(),
 			Cmd:      "scw object config install type=mc",
-			Check: core.TestCheckCombine(
-				func(t *testing.T, ctx *core.CheckFuncCtx) {
-					_, err := os.Stat(path.Join(tmpDir, ".mc", "config.json"))
-					if err != nil {
-						t.Fail()
-					}
-				},
-				core.TestCheckExitCode(0),
-			),
+			Check:    core.TestCheckExitCode(0),
 			OverrideEnv: map[string]string{
 				"HOME": tmpDir,
 			},
@@ -49,15 +32,7 @@ func Test_ConfigInstall(t *testing.T) {
 		t.Run("s3cmd", core.Test(&core.TestConfig{
 			Commands: GetCommands(),
 			Cmd:      "scw object config install type=s3cmd",
-			Check: core.TestCheckCombine(
-				func(t *testing.T, ctx *core.CheckFuncCtx) {
-					_, err := os.Stat(path.Join(tmpDir, ".s3cfg"))
-					if err != nil {
-						t.Fail()
-					}
-				},
-				core.TestCheckExitCode(0),
-			),
+			Check:    core.TestCheckExitCode(0),
 			OverrideEnv: map[string]string{
 				"HOME": tmpDir,
 			},
