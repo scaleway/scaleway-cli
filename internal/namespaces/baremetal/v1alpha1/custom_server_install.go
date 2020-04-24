f10740de5d7d5aba08060f90a45add4e2bc2a950
Rémy Léone <rleone@scaleway.com>
2020-04-24 11:31:58 +0200

    Fix

--- internal/namespaces/baremetal/v1alpha1/custom_server_install.go
+++ internal/namespaces/baremetal/v1alpha1/custom_server_install.go
@@ -29,7 +29,7 @@ func serverInstallBuilder(c *core.Command) *core.Command {
 	c.WaitFunc = func(ctx context.Context, argsI, respI interface{}) (interface{}, error) {
 		api := baremetal.NewAPI(core.ExtractClient(ctx))
 		return api.WaitForServerInstall(&baremetal.WaitForServerInstallRequest{
-			Zone:     argsI.(*baremetal.WaitForServerInstallRequest).Zone,
+			Zone:     argsI.(*baremetal.InstallServerRequest).Zone,
 			ServerID: respI.(*baremetal.Server).ID,
 			Timeout:  serverActionTimeout,
 		})
