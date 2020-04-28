3a02dc3d75f5a16ee5c2911bbe89a34d408ff097
Rémy Léone <rleone@scaleway.com>
2020-04-28 20:43:46 +0200

    Fix

--- internal/namespaces/feedback/custom_test.go
+++ internal/namespaces/feedback/custom_test.go
@@ -1,8 +1,8 @@
 package feedback
 
 import (
-	"fmt"
 	"os/exec"
+	"runtime"
 	"testing"
 
 	"github.com/alecthomas/assert"
@@ -24,10 +24,17 @@ func Test_FeedbackBug(t *testing.T) {
 			GoOS:      "runtime.GOOS",
 		},
 		OverrideExec: func(ctx *core.ExecFuncCtx, cmd *exec.Cmd) (exitCode int, err error) {
-			fmt.Println(cmd.Args[1])
+			var observed string
+			switch runtime.GOOS {
+			case "windows":
+				// 0: "rundll32", 1: "url.dll,FileProtocolHandler" 2: url
+				observed = cmd.Args[2]
+			default:
+				observed = cmd.Args[1]
+			}
 			assert.Equal(t,
 				"https://github.com/scaleway/scaleway-cli/issues/new?body=%0A%23%23+Description%3A%0A%0A%23%23+How+to+reproduce%3A%0A%0A%23%23%23+Command+attempted%0A%0A%23%23%23+Expected+Behavior%0A%0A%23%23%23+Actual+Behavior%0A%0A%23%23+More+info%0A%0A%23%23+Version%0A%0Aversion+++++0.0.0%0Abuild-date++unknown%0Ago-version++runtime.Version%28%29%0Agit-branch++unknown%0Agit-commit++unknown%0Ago-arch+++++runtime.GOARCH%0Ago-os+++++++runtime.GOOS%0A&issueTemplate=bug_report.md&labels=bug",
-				cmd.Args[1])
+				observed)
 
 			return 0, nil
 		}, 
@@ -52,10 +59,17 @@ func Test_FeedbackFeature(t *testing.T) {
 			GoOS:      "runtime.GOOS",
 		},
 		OverrideExec: func(ctx *core.ExecFuncCtx, cmd *exec.Cmd) (exitCode int, err error) {
-			fmt.Println(cmd.Args[1])
+			var observed string
+			switch runtime.GOOS {
+			case "windows":
+				// 0: "rundll32", 1: "url.dll,FileProtocolHandler" 2: url
+				observed = cmd.Args[2]
+			default:
+				observed = cmd.Args[1]
+			}
 			assert.Equal(t,
 				"https://github.com/scaleway/scaleway-cli/issues/new?body=%0A%23%23+Description%0A%0A%23%23+How+this+functionality+would+be+exposed%0A%0A%23%23+References%0A%0A%23%23+Version%0A%0Aversion+++++0.0.0%0Abuild-date++unknown%0Ago-version++runtime.Version%28%29%0Agit-branch++unknown%0Agit-commit++unknown%0Ago-arch+++++runtime.GOARCH%0Ago-os+++++++runtime.GOOS%0A&issueTemplate=feature_request.md&labels=enhancement",
-				cmd.Args[1])
+				observed)
 
 			return 0, nil
 		},
