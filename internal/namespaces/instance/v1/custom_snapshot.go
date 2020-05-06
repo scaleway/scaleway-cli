0e419de504a601a47ce98ea081f2e5965984640a
Rémy Léone <rleone@scaleway.com>
2020-05-06 13:49:57 +0200

    feat(instance): add a wait command for image and snapshots (#996)

--- internal/namespaces/instance/v1/custom_snapshot.go
+++ internal/namespaces/instance/v1/custom_snapshot.go
@@ -3,9 +3,14 @@ package instance
 import (
 	"context"
 	"reflect"
+	"time"
 
 	"github.com/scaleway/scaleway-cli/internal/core"
 	"github.com/scaleway/scaleway-sdk-go/api/instance/v1"
+)
+
+const (
+	snapshotActionTimeout = 60 * time.Minute
 )
 
 // Builders
@@ -68,4 +73,39 @@ func snapshotListBuilder(c *core.Command) *core.Command {
 		return runner(ctx, request)
 	})
 	return c
+}
+
+func snapshotWaitCommand() *core.Command {
+	return &core.Command{
+		Short:     `Wait for snapshot to reach a stable state`,
+		Long:      `Wait for snapshot to reach a stable state. This is similar to using --wait flag on other action commands, but without requiring a new action on the snapshot.`,
+		Namespace: "instance",
+		Resource:  "snapshot",
+		Verb:      "wait",
+		ArgsType:  reflect.TypeOf(instance.WaitForSnapshotRequest{}),
+		Run: func(ctx context.Context, argsI interface{}) (i interface{}, err error) {
+			api := instance.NewAPI(core.ExtractClient(ctx))
+			return api.WaitForSnapshot(&instance.WaitForSnapshotRequest{
+				Zone:       argsI.(*instance.WaitForSnapshotRequest).Zone,
+				SnapshotID: argsI.(*instance.WaitForSnapshotRequest).SnapshotID,
+				Timeout:    snapshotActionTimeout,
+			})
+
+		},
+		ArgSpecs: core.ArgSpecs{
+			{
+				Name:     "snapshot-id",
+				Short:    `ID of the snapshot.`,
+				Required: true,
+				Positional: true,
+			},
+			core.ZoneArgSpec(),
+		},
+		Examples: []*core.Example{
+			{
+				Short:   "Wait for a snapshot to reach a stable state",
+				Request: `{"snapshot_id": "11111111-1111-1111-1111-111111111111"}`,
+			},
+		},
+	}
 }
