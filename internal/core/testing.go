6f95c18523a1ba3f59720ce1e99fee97c82d46e8
Rémy Léone <rleone@scaleway.com>
2020-06-03 19:03:06 +0200

    test(registry): fix test for image list (#1059)

--- internal/core/testing.go
+++ internal/core/testing.go
@@ -453,15 +453,9 @@ func ExecStoreBeforeCmd(metaKey, cmd string) BeforeFunc {
 	}
 }
 
-func BeforeFuncOsExec(cmdStrings ...[]string) BeforeFunc {
+func BeforeFuncOsExec(cmd string, args ...string) BeforeFunc {
 	return func(ctx *BeforeFuncCtx) error {
-		for _, cmdString := range cmdStrings {
-			err := exec.Command(cmdString[0], cmdString[1:len(cmdString)-1]...).Run()
-			if err != nil {
-				return err
-			}
-		}
-		return nil
+		return exec.Command(cmd, args...).Run()
 	}
 }
 
