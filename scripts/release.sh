ae0c52a0e6e58004ea53934093809b58e022b6ee
Rémy Léone <rleone@scaleway.com>
2020-06-08 17:25:33 +0200

    Fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export SCW_SECRET_KEY="$(vault kv get -field scaleway_secret_key agw_kv/prd)"
-export SCW_ACCESS_KEY="$(vault kv get -field scaleway_access_key agw_kv/prd )"
+export SCW_ACCESS_KEY="$(vault kv get -field scaleway_access_key agw_kv/prd)"
 export GITHUB_TOKEN="$(vault kv get -field cli_release_github_token front_kv/opensource)"
 cd scripts/release || (echo "Please run this script from repo root" && exit 1)
 
