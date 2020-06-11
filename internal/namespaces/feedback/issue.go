192fa61b197ec7078a3e33f2ebb021277bcc58e5
Rémy Léone <rleone@scaleway.com>
2020-06-11 11:37:16 +0200

    fix goconst

--- internal/namespaces/feedback/issue.go
+++ internal/namespaces/feedback/issue.go
@@ -19,8 +19,11 @@ const githubURL = "https://github.com/scaleway/scaleway-cli/issues/new"
 type issueTemplate string
 
 const (
-	bug = issueTemplate("bug")
+	bug     = issueTemplate("bug")
 	feature = issueTemplate("feature")
+	linux   = "linux"
+	darwin  = "darwin"
+	windows = "windows"
 )
 
 type issue struct {
@@ -88,11 +91,11 @@ func (i issue) openInBrowser(ctx context.Context) error {
 	var openCmd *exec.Cmd
 
 	switch runtime.GOOS {
-	case "linux":
+	case linux:
 		openCmd = exec.Command("xdg-open", i.getURL())
-	case "windows":
+	case windows:
 		openCmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", i.getURL())
-	case "darwin":
+	case darwin:
 		openCmd = exec.Command("open", i.getURL())
 	default:
 		err = fmt.Errorf("unsupported platform")
