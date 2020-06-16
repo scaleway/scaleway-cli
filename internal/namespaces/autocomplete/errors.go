39f681a60d6150faf3f0d77d7fc79632fd2ac33a
Rémy Léone <rleone@scaleway.com>
2020-06-16 15:08:30 +0200

    Fix

--- internal/namespaces/autocomplete/errors.go
+++ internal/namespaces/autocomplete/errors.go
@@ -27,7 +27,7 @@ func installationCancelledError(shellName string, script string) *core.CliError 
 
 func installationNotFound(shellName string, location string, script string) *core.CliError {
 	return &core.CliError{
-		Err:  fmt.Errorf("not found where to install autocomplete script (tried %s)", location),
+		Err:  fmt.Errorf("cannot find where to install autocomplete script (tried %s)", location),
 		Hint: fmt.Sprintf("You can add this line: `%s` in your %s configuration file", script, shellName),
 	}
 }
