5b6548ea850c92c7dafc40b557a3d1592c981cc0
Rémy Léone <rleone@scaleway.com>
2020-06-19 12:34:29 +0200

    feat: add support for windows and linux i386 (32bits)

--- scripts/build.sh
+++ scripts/build.sh
@@ -20,16 +20,22 @@ if [[ "${BUILD_IN_DOCKER}" == "true" ]]; then
 BIN_DIR="./bin"
 VERSION=$(go run cmd/scw/main.go -o json version | jq -r .version | tr . -)
 BIN_LINUX="$BIN_DIR/scw-$VERSION-linux-x86_64"
+BIN_LINUX_386="$BIN_DIR/scw-$VERSION-linux-386"
 BIN_DARWIN="$BIN_DIR/scw-$VERSION-darwin-x86_64"
 BIN_WINDOWS="$BIN_DIR/scw-$VERSION-windows-x86_64.exe"
+BIN_WINDOWS_386="$BIN_DIR/scw-$VERSION-windows-386.exe"
 
 mkdir -p $BIN_DIR
 GOOS=linux  GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_LINUX" cmd/scw/main.go
+GOOS=linux  GOARCH=386 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_LINUX_386" cmd/scw/main.go
 GOOS=darwin GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_DARWIN" cmd/scw/main.go
 GOOS=windows GOARCH=amd64 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_WINDOWS" cmd/scw/main.go
+GOOS=windows GOARCH=386 go build -ldflags "${LDFLAGS[*]}" -o "$BIN_WINDOWS_386" cmd/scw/main.go
 
 shasum -a 256 \
   "$BIN_LINUX" \
+  "$BIN_LINUX_386" \
   "$BIN_DARWIN" \
   "$BIN_WINDOWS" \
+  "$BIN_WINDOWS_386" \
   | sed -e 's#./bin/##' > "$BIN_DIR/SHA256SUMS"
