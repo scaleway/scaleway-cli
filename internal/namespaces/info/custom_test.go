0ac6e5f9c13876d2bd42b9c2fef80eb92e405e9d
Rémy Léone <rleone@scaleway.com>
2020-06-22 18:29:07 +0200

    Fix

--- internal/namespaces/info/custom_test.go
+++ internal/namespaces/info/custom_test.go
@@ -15,12 +15,12 @@ func Test_Info(t *testing.T) {
 			core.TestCheckExitCode(0),
 		),
 		OverrideEnv: map[string]string{
-			"SCW_SECRET_KEY":         "22222222-2222-2222-2222-222222222222",
-			"SCW_DEFAULT_PROJECT_ID": "22222222-2222-2222-2222-222222222222",
-			"SCW_ACCESS_KEY":         "SCWYYYYYYYYYYYYYYYYY",
-			"SCW_CONFIG_PATH":        "/tmp/.config/scw/config.yaml",
-			"SCW_DEFAULT_REGION":     "fr-par",
-			"SCW_DEFAULT_ZONE":       "fr-par-1",
+			"SCW_SECRET_KEY":              "22222222-2222-2222-2222-222222222222",
+			"SCW_DEFAULT_ORGANIZATION_ID": "22222222-2222-2222-2222-222222222222",
+			"SCW_ACCESS_KEY":              "SCWYYYYYYYYYYYYYYYYY",
+			"SCW_CONFIG_PATH":             "/tmp/.config/scw/config.yaml",
+			"SCW_DEFAULT_REGION":          "fr-par",
+			"SCW_DEFAULT_ZONE":            "fr-par-1",
 		},
 	}))
 
@@ -32,12 +32,12 @@ func Test_Info(t *testing.T) {
 			core.TestCheckExitCode(0),
 		),
 		OverrideEnv: map[string]string{
-			"SCW_SECRET_KEY":         "22222222-2222-2222-2222-222222222222",
-			"SCW_DEFAULT_PROJECT_ID": "22222222-2222-2222-2222-222222222222",
-			"SCW_ACCESS_KEY":         "SCWYYYYYYYYYYYYYYYYY",
-			"SCW_CONFIG_PATH":        "/tmp/.config/scw/config.yaml",
-			"SCW_DEFAULT_REGION":     "fr-par",
-			"SCW_DEFAULT_ZONE":       "fr-par-1",
+			"SCW_SECRET_KEY":              "22222222-2222-2222-2222-222222222222",
+			"SCW_DEFAULT_ORGANIZATION_ID": "22222222-2222-2222-2222-222222222222",
+			"SCW_ACCESS_KEY":              "SCWYYYYYYYYYYYYYYYYY",
+			"SCW_CONFIG_PATH":             "/tmp/.config/scw/config.yaml",
+			"SCW_DEFAULT_REGION":          "fr-par",
+			"SCW_DEFAULT_ZONE":            "fr-par-1",
 		},
 	}))
 }
