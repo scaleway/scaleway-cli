da5173c7387065ee78f38404f15b9a5d7e91e619
Rémy Léone <rleone@scaleway.com>
2020-06-23 17:21:29 +0200

    Fix

--- internal/namespaces/init/custom_init_autocomplete_test.go
+++ internal/namespaces/init/custom_init_autocomplete_test.go
@@ -29,172 +29,163 @@ func Test_InitAutocomplete(t *testing.T) {
 		"with-ssh-key":     "false",
 	}
 
-	t.Run("Without", core.Test(&core.TestConfig{
-		Commands:   GetCommands(),
-		BeforeFunc: baseBeforeFunc(),
-		Cmd:        appendArgs("scw init install-autocomplete=false", defaultSettings),
-		Check:      core.TestCheckGolden(),
-		TmpHomeDir: true,
-	}))
+	runAllShells := func(t *testing.T) {
+		t.Run("Without", core.Test(&core.TestConfig{
+			Commands:   GetCommands(),
+			BeforeFunc: baseBeforeFunc(),
+			Cmd:        appendArgs("scw init install-autocomplete=false", defaultSettings),
+			Check:      core.TestCheckGolden(),
+			TmpHomeDir: true,
+		}))
 
-	t.Run("Zsh", func(t *testing.T) {
-		evalLine := `
+		t.Run("Zsh", func(t *testing.T) {
+			evalLine := `
 # Scaleway CLI autocomplete initialization.
 eval "$(scw autocomplete script shell=zsh)"
 `
-		core.Test(&core.TestConfig{
-			Commands:   GetCommands(),
-			BeforeFunc: baseBeforeFunc(),
-			Cmd:        appendArgs("scw init install-autocomplete=true", defaultSettings),
-			Check: core.TestCheckCombine(
-				core.TestCheckGolden(),
-				func(t *testing.T, ctx *core.CheckFuncCtx) {
-					homeDir := ctx.OverrideEnv["HOME"]
-					filePath := path.Join(homeDir, ".zshrc")
-					fileContent, err := ioutil.ReadFile(filePath)
-					if err != nil {
-						require.NoError(t, err)
-					}
-					require.Equal(t, evalLine, string(fileContent))
+			core.Test(&core.TestConfig{
+				Commands:   GetCommands(),
+				BeforeFunc: baseBeforeFunc(),
+				Cmd:        appendArgs("scw init install-autocomplete=true", defaultSettings),
+				Check: core.TestCheckCombine(
+					core.TestCheckGolden(),
+					func(t *testing.T, ctx *core.CheckFuncCtx) {
+						homeDir := ctx.OverrideEnv["HOME"]
+						filePath := path.Join(homeDir, ".zshrc")
+						fileContent, err := ioutil.ReadFile(filePath)
+						if err != nil {
+							require.NoError(t, err)
+						}
+						require.Equal(t, evalLine, string(fileContent))
+					},
+				),
+				TmpHomeDir: true,
+				OverrideEnv: map[string]string{
+					"SHELL": "/usr/local/bin/zsh",
 				},
-			),
-			TmpHomeDir: true,
-			OverrideEnv: map[string]string{
-				"SHELL": "/usr/local/bin/zsh",
-			},
-			PromptResponseMocks: []string{
-				// What type of shell are you using
-				"zsh",
-				// Do you want to proceed with these changes? (Y/n):
-				"yes",
-			},
-		})(t)
-	})
-
-	t.Run("bash-mac", func(t *testing.T) {
-		if runtime.GOOS != "darwin" {
-			t.SkipNow()
-		}
-		evalLine := `
-# Scaleway CLI autocomplete initialization.
-eval "$(scw autocomplete script shell=bash)"
-`
-		core.Test(&core.TestConfig{
-			Commands:   GetCommands(),
-			BeforeFunc: baseBeforeFunc(),
-			Cmd:        appendArgs("scw init install-autocomplete=true", defaultSettings),
-			Check: core.TestCheckCombine(
-				core.TestCheckGolden(),
-				func(t *testing.T, ctx *core.CheckFuncCtx) {
-					homeDir := ctx.OverrideEnv["HOME"]
-					filePath := path.Join(homeDir, ".bash_profile")
-					fileContent, err := ioutil.ReadFile(filePath)
-					if err != nil {
-						require.NoError(t, err)
-					}
-					require.Equal(t, evalLine, string(fileContent))
+				PromptResponseMocks: []string{
+					// What type of shell are you using
+					"zsh",
+					// Do you want to proceed with these changes? (Y/n):
+					"yes",
 				},
-			),
-			TmpHomeDir: true,
-			OverrideEnv: map[string]string{
-				"SHELL": "/usr/local/bin/bash",
-			},
-			PromptResponseMocks: []string{
-				// What type of shell are you using
-				"bash",
-				// Do you want to proceed with these changes? (Y/n):
-				"yes",
-			},
-		})(t)
-	})
+			})(t)
+		})
 
-	t.Run("bash-linux", func(t *testing.T) {
-		if runtime.GOOS != "linux" {
-			t.SkipNow()
-		}
-		evalLine := `
-# Scaleway CLI autocomplete initialization.
-eval "$(scw autocomplete script shell=bash)"
-`
-		core.Test(&core.TestConfig{
-			BeforeFunc: baseBeforeFunc(),
-			Commands:   GetCommands(),
-			Cmd:        appendArgs("scw init install-autocomplete=true", defaultSettings),
-			Check: core.TestCheckCombine(
-				core.TestCheckGolden(),
-				func(t *testing.T, ctx *core.CheckFuncCtx) {
-					homeDir := ctx.OverrideEnv["HOME"]
-					filePath := path.Join(homeDir, ".bashrc")
-					fileContent, err := ioutil.ReadFile(filePath)
-					if err != nil {
-						t.FailNow()
-					}
-					require.Equal(t, evalLine, string(fileContent))
-				},
-			),
-			TmpHomeDir: true,
-			OverrideEnv: map[string]string{
-				"SHELL": "/usr/local/bin/bash",
-			},
-			PromptResponseMocks: []string{
-				// What type of shell are you using
-				"bash",
-				// Do you want to proceed with these changes? (Y/n):
-				"yes",
-			},
-		})(t)
-	})
-
-	t.Run("fish", func(t *testing.T) {
-		evalLine := `
+		t.Run("fish", func(t *testing.T) {
+			evalLine := `
 # Scaleway CLI autocomplete initialization.
 eval (scw autocomplete script shell=fish)
 `
-		core.Test(&core.TestConfig{
-			Commands: GetCommands(),
-			BeforeFunc: core.BeforeFuncCombine(
-				func(ctx *core.BeforeFuncCtx) error {
-					homeDir := ctx.OverrideEnv["HOME"]
-					configPath := path.Join(homeDir, ".config", "fish", "config.fish")
+			core.Test(&core.TestConfig{
+				Commands: GetCommands(),
+				BeforeFunc: core.BeforeFuncCombine(
+					func(ctx *core.BeforeFuncCtx) error {
+						homeDir := ctx.OverrideEnv["HOME"]
+						configPath := path.Join(homeDir, ".config", "fish", "config.fish")
 
-					// Ensure the subfolders for the configuration files are all created
-					err := os.MkdirAll(filepath.Dir(configPath), 0755)
-					if err != nil {
-						return err
-					}
+						// Ensure the subfolders for the configuration files are all created
+						err := os.MkdirAll(filepath.Dir(configPath), 0755)
+						if err != nil {
+							return err
+						}
 
-					// Write the configuration file
-					err = ioutil.WriteFile(configPath, []byte(``), 0600)
-					if err != nil {
-						return err
-					}
-					return nil
+						// Write the configuration file
+						err = ioutil.WriteFile(configPath, []byte(``), 0600)
+						if err != nil {
+							return err
+						}
+						return nil
+					},
+					baseBeforeFunc(),
+				),
+				Cmd: appendArgs("scw init install-autocomplete=true", defaultSettings),
+				Check: core.TestCheckCombine(
+					core.TestCheckGolden(),
+					func(t *testing.T, ctx *core.CheckFuncCtx) {
+						homeDir := ctx.OverrideEnv["HOME"]
+						filePath := path.Join(homeDir, ".config", "fish", "config.fish")
+						fileContent, err := ioutil.ReadFile(filePath)
+						if err != nil {
+							t.FailNow()
+						}
+						require.Equal(t, evalLine, string(fileContent))
+					},
+				),
+				TmpHomeDir: true,
+				OverrideEnv: map[string]string{
+					"SHELL": "/usr/local/bin/fish",
 				},
-				baseBeforeFunc(),
-			),
-			Cmd: appendArgs("scw init install-autocomplete=true", defaultSettings),
-			Check: core.TestCheckCombine(
-				core.TestCheckGolden(),
-				func(t *testing.T, ctx *core.CheckFuncCtx) {
-					homeDir := ctx.OverrideEnv["HOME"]
-					filePath := path.Join(homeDir, ".config", "fish", "config.fish")
-					fileContent, err := ioutil.ReadFile(filePath)
-					if err != nil {
-						t.FailNow()
-					}
-					require.Equal(t, evalLine, string(fileContent))
+				PromptResponseMocks: []string{
+					// What type of shell are you using
+					"fish",
+					// Do you want to proceed with these changes? (Y/n):
+					"yes",
 				},
-			),
-			TmpHomeDir: true,
-			OverrideEnv: map[string]string{
-				"SHELL": "/usr/local/bin/fish",
-			},
-			PromptResponseMocks: []string{
-				// What type of shell are you using
-				"fish",
-				// Do you want to proceed with these changes? (Y/n):
-				"yes",
-			},
-		})(t)
+			})(t)
+		})
+
+		t.Run("bash", func(t *testing.T) {
+			evalLine := `
+# Scaleway CLI autocomplete initialization.
+eval "$(scw autocomplete script shell=bash)"
+`
+			core.Test(&core.TestConfig{
+				Commands:   GetCommands(),
+				BeforeFunc: baseBeforeFunc(),
+				Cmd:        appendArgs("scw init install-autocomplete=true", defaultSettings),
+				Check: core.TestCheckCombine(
+					core.TestCheckGolden(),
+					func(t *testing.T, ctx *core.CheckFuncCtx) {
+						homeDir := ctx.OverrideEnv["HOME"]
+						filePath := ""
+						switch runtime.GOOS {
+						case "linux":
+							filePath = path.Join(homeDir, ".bashrc")
+						case "darwin":
+							filePath = path.Join(homeDir, ".bash_profile")
+						default:
+							t.Fatalf("unsupported OS")
+						}
+						fileContent, err := ioutil.ReadFile(filePath)
+						if err != nil {
+							require.NoError(t, err)
+						}
+						require.Equal(t, evalLine, string(fileContent))
+					},
+				),
+				TmpHomeDir: true,
+				OverrideEnv: map[string]string{
+					"SHELL": "/usr/local/bin/bash",
+				},
+				PromptResponseMocks: []string{
+					// What type of shell are you using
+					"bash",
+					// Do you want to proceed with these changes? (Y/n):
+					"yes",
+				},
+			})(t)
+		})
+	}
+
+	t.Run("darwin", func(t *testing.T) {
+		if runtime.GOOS != "darwin" {
+			t.SkipNow()
+		}
+		runAllShells(t)
+	})
+
+	t.Run("linux", func(t *testing.T) {
+		if runtime.GOOS != "linux" {
+			t.SkipNow()
+		}
+		runAllShells(t)
+	})
+
+	t.Run("windows", func(t *testing.T) {
+		if runtime.GOOS != "windows" {
+			t.SkipNow()
+		}
+		runAllShells(t)
 	})
 }
