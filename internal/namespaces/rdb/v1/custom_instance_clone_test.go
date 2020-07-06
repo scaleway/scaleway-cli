27475f177bb650761e7c9bac26f9b757469d0d23
Rémy Léone <rleone@scaleway.com>
2020-07-06 15:00:18 +0200

    Fix

--- internal/namespaces/rdb/v1/custom_instance_clone_test.go
+++ internal/namespaces/rdb/v1/custom_instance_clone_test.go
@@ -1,26 +1,24 @@
 package rdb
 
-//func Test_CloneInstance(t *testing.T) {
-//	// Simple use cases
-//	t.Run("Simple", func(t *testing.T) {
-//		t.Run("Default", core.Test(&core.TestConfig{
-//			Commands: GetCommands(),
-//			Cmd:      "scw rdb instance clone -w",
-//			Check: core.TestCheckCombine(
-//				core.TestCheckGolden(),
-//				core.TestCheckExitCode(0),
-//			),
-//			AfterFunc: func(ctx *core.AfterFuncCtx) error {
-//				_, err := baremetal.NewAPI(ctx.Client).DeleteServer(&baremetal.DeleteServerRequest{
-//					ServerID: ctx.CmdResult.(*baremetal.Server).ID,
-//				})
-//				if err != nil {
-//					return err
-//				}
-//				return nil
-//			},
-//			DefaultZone: scw.ZoneFrPar2,
-//		}))
-//	}
-//
-//}
+import (
+	"fmt"
+	"testing"
+
+	"github.com/scaleway/scaleway-cli/internal/core"
+)
+
+func Test_CloneInstance(t *testing.T) {
+	t.Run("Simple", core.Test(&core.TestConfig{
+		Commands: GetCommands(),
+		BeforeFunc: core.ExecStoreBeforeCmd(
+			"StartServer",
+			fmt.Sprintf("scw rdb instance create node-type=db-dev-s is-ha-cluster=false name=%s engine=%s user-name=%s password=%s --wait", name, engine, user, password),
+		),
+		Cmd: "scw rdb instance clone {{ .StartServer.ID }} node-type=db-dev-m name=foobar --wait",
+		Check: core.TestCheckCombine(
+			core.TestCheckGolden(),
+			core.TestCheckExitCode(0),
+		),
+		AfterFunc: core.ExecAfterCmd("scw rdb instance delete {{ .StartServer.ID }}"),
+	}))
+}
