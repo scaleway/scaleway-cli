118b76f0acf3b81c373d6e34f535b791b13b79fa
Rémy Léone <rleone@scaleway.com>
2020-07-06 18:42:14 +0200

    Fix

--- internal/namespaces/rdb/v1/custom_instance_create.go
+++ internal/namespaces/rdb/v1/custom_instance_create.go
@@ -9,7 +9,7 @@ import (
 )
 
 func instanceCreateBuilder(c *core.Command) *core.Command {
-	c.ArgSpecs.GetByName("node-type").Default = core.DefaultValueSetter("db-dev-s")
+	c.ArgSpecs.GetByName("node-type").Default = core.DefaultValueSetter("DB-DEV-S")
 	c.ArgSpecs.GetByName("node-type").EnumValues = nodeTypes
 
 	c.WaitFunc = func(ctx context.Context, argsI, respI interface{}) (interface{}, error) {
