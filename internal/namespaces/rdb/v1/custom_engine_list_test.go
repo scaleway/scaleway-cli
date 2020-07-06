49afdee85e32d0efadc8ce113b5d04224a67bc45
Rémy Léone <rleone@scaleway.com>
2020-07-06 11:48:20 +0200

    Fix

---
+++ internal/namespaces/rdb/v1/custom_engine_list_test.go
@@ -0,0 +1,18 @@
+package rdb
+
+import (
+	"testing"
+
+	"github.com/scaleway/scaleway-cli/internal/core"
+)
+
+func Test_EngineList(t *testing.T) {
+	t.Run("Simple", core.Test(&core.TestConfig{
+		Commands: GetCommands(),
+		Cmd:      "scw rdb engine list",
+		Check: core.TestCheckCombine(
+			core.TestCheckGolden(),
+			core.TestCheckExitCode(0),
+		),
+	}))
+}
