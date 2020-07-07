4977be59b607ca4f62d89ce2975b12da06659398
Rémy Léone <rleone@scaleway.com>
2020-07-07 13:25:12 +0200

    Fix

--- internal/namespaces/rdb/v1/custom_instance_get_test.go
+++ internal/namespaces/rdb/v1/custom_instance_get_test.go
@@ -7,14 +7,14 @@ import (
 	"github.com/scaleway/scaleway-cli/internal/core"
 )
 
-func Test_InstanceMashaller(t *testing.T) {
+func Test_GetInstance(t *testing.T) {
 	t.Run("Simple", core.Test(&core.TestConfig{
 		Commands: GetCommands(),
 		BeforeFunc: core.ExecStoreBeforeCmd(
 			"StartServer",
 			fmt.Sprintf("scw rdb instance create node-type=DB-DEV-S is-ha-cluster=false name=%s engine=%s user-name=%s password=%s --wait", name, engine, user, password),
 		),
-		Cmd:       "scw rdb instance clone {{ .StartServer.ID }} node-type=DB-DEV-M name=foobar --wait",
+		Cmd:       "scw rdb instance get {{ .StartServer.ID }}",
 		Check:     core.TestCheckGolden(),
 		AfterFunc: core.ExecAfterCmd("scw rdb instance delete {{ .StartServer.ID }}"),
 	}))
