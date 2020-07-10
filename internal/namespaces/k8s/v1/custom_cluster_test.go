27a2286008a97d1a3eb5fcf5ebea53970531890c
Rémy Léone <rleone@scaleway.com>
2020-07-10 14:06:31 +0200

    FIx

--- internal/namespaces/k8s/v1/custom_cluster_test.go
+++ internal/namespaces/k8s/v1/custom_cluster_test.go
@@ -21,12 +21,9 @@ func Test_GetCluster(t *testing.T) {
 
 func Test_WaitCluster(t *testing.T) {
 	t.Run("wait for pools", core.Test(&core.TestConfig{
-		Commands: GetCommands(),
-		BeforeFunc: core.ExecStoreBeforeCmd(
-			"Cluster",
-			"scw k8s cluster create name=cli-test version=1.18.5 cni=cilium pools.0.node-type=DEV1-M pools.0.size=3 pools.0.name=default",
-		),
-		Cmd: "scw k8s cluster wait {{ .Cluster.ID }} wait-for-pools=true",
+		Commands:   GetCommands(),
+		BeforeFunc: createCluster("Cluster", kapsuleVersion, 3),
+		Cmd:        "scw k8s cluster wait {{ .Cluster.ID }} wait-for-pools=true",
 		Check: core.TestCheckCombine(
 			core.TestCheckGolden(),
 			core.TestCheckExitCode(0),
