51570a1f0535ac6bdf72aa16144b9027f67fe0c0
Rémy Léone <rleone@scaleway.com>
2020-07-13 17:30:22 +0200

    rdb: add support for wait on backup resource (#1192)
    Co-authored-by: Jerome Quere <jquere@scaleway.com>

--- internal/namespaces/rdb/v1/custom.go
+++ internal/namespaces/rdb/v1/custom.go
@@ -27,11 +27,15 @@ func GetCommands() *core.Commands {
 	cmds.Merge(core.NewCommands(
 		instanceWaitCommand(),
 		instanceConnectCommand(),
+		backupWaitCommand(),
 	))
+	cmds.MustFind("rdb", "backup", "create").Override(backupCreateBuilder)
+	cmds.MustFind("rdb", "backup", "export").Override(backupExportBuilder)
+	cmds.MustFind("rdb", "backup", "restore").Override(backupRestoreBuilder)
+
 	cmds.MustFind("rdb", "instance", "create").Override(instanceCreateBuilder)
 	cmds.MustFind("rdb", "instance", "clone").Override(instanceCloneBuilder)
 	cmds.MustFind("rdb", "instance", "create").Override(instanceCreateBuilder)
-
 	cmds.MustFind("rdb", "instance", "upgrade").Override(instanceUpgradeBuilder)
 
 	cmds.MustFind("rdb", "engine", "list").Override(engineListBuilder)
---
+++ internal/namespaces/rdb/v1/custom_backup.go
@@ -0,0 +1,100 @@
+package rdb
+
+import (
+	"context"
+	"reflect"
+	"time"
+
+	"github.com/scaleway/scaleway-cli/internal/core"
+	"github.com/scaleway/scaleway-sdk-go/api/rdb/v1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+)
+
+const (
+	backupActionTimeout = 20 * time.Minute
+)
+
+type backupWaitRequest struct {
+	DatabaseBackupID string
+	Region           scw.Region
+}
+
+func backupWaitCommand() *core.Command {
+	return &core.Command{
+		Short:     `Wait for a backup to reach a stable state`,
+		Long:      `Wait for a backup to reach a stable state. This is similar to using --wait flag.`,
+		Namespace: "rdb",
+		Resource:  "backup",
+		Verb:      "wait",
+		ArgsType:  reflect.TypeOf(backupWaitRequest{}),
+		Run: func(ctx context.Context, argsI interface{}) (i interface{}, err error) {
+			api := rdb.NewAPI(core.ExtractClient(ctx))
+			return api.WaitForDatabaseBackup(&rdb.WaitForDatabaseBackupRequest{
+				DatabaseBackupID: argsI.(*backupWaitRequest).DatabaseBackupID,
+				Region:           argsI.(*backupWaitRequest).Region,
+				Timeout:          scw.TimeDurationPtr(backupActionTimeout),
+				RetryInterval:    core.DefaultRetryInterval,
+			})
+		},
+		ArgSpecs: core.ArgSpecs{
+			{
+				Name:       "backup-id",
+				Short:      `ID of the backup you want to wait for.`,
+				Required:   true,
+				Positional: true,
+			},
+			core.RegionArgSpec(scw.RegionFrPar, scw.RegionNlAms),
+		},
+		Examples: []*core.Example{
+			{
+				Short:    "Wait for a backup to reach a stable state",
+				ArgsJSON: `{"backup_id": "11111111-1111-1111-1111-111111111111"}`,
+			},
+		},
+	}
+}
+
+func backupCreateBuilder(c *core.Command) *core.Command {
+	timeout := backupActionTimeout
+	c.WaitFunc = func(ctx context.Context, argsI, respI interface{}) (interface{}, error) {
+		api := rdb.NewAPI(core.ExtractClient(ctx))
+		return api.WaitForDatabaseBackup(&rdb.WaitForDatabaseBackupRequest{
+			DatabaseBackupID: respI.(*rdb.DatabaseBackup).ID,
+			Region:           respI.(*rdb.DatabaseBackup).Region,
+			Timeout:          &timeout,
+			RetryInterval:    core.DefaultRetryInterval,
+		})
+	}
+
+	return c
+}
+
+func backupExportBuilder(c *core.Command) *core.Command {
+	timeout := backupActionTimeout
+	c.WaitFunc = func(ctx context.Context, argsI, respI interface{}) (interface{}, error) {
+		api := rdb.NewAPI(core.ExtractClient(ctx))
+		return api.WaitForDatabaseBackup(&rdb.WaitForDatabaseBackupRequest{
+			DatabaseBackupID: respI.(*rdb.DatabaseBackup).ID,
+			Region:           respI.(*rdb.DatabaseBackup).Region,
+			Timeout:          &timeout,
+			RetryInterval:    core.DefaultRetryInterval,
+		})
+	}
+
+	return c
+}
+
+func backupRestoreBuilder(c *core.Command) *core.Command {
+	timeout := backupActionTimeout
+	c.WaitFunc = func(ctx context.Context, argsI, respI interface{}) (interface{}, error) {
+		api := rdb.NewAPI(core.ExtractClient(ctx))
+		return api.WaitForDatabaseBackup(&rdb.WaitForDatabaseBackupRequest{
+			DatabaseBackupID: respI.(*rdb.DatabaseBackup).ID,
+			Region:           respI.(*rdb.DatabaseBackup).Region,
+			Timeout:          &timeout,
+			RetryInterval:    core.DefaultRetryInterval,
+		})
+	}
+
+	return c
+}
