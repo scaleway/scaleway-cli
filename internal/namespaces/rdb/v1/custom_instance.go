20ea7bbc353fca01459abac7f34f4969958f0f32
Rémy Léone <rleone@scaleway.com>
2020-07-13 11:17:04 +0200

    Fix

--- internal/namespaces/rdb/v1/custom_instance.go
+++ internal/namespaces/rdb/v1/custom_instance.go
@@ -2,12 +2,18 @@ package rdb
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
 	"reflect"
+	"runtime"
 	"strings"
 	"time"
 
 	"github.com/scaleway/scaleway-cli/internal/core"
 	"github.com/scaleway/scaleway-cli/internal/human"
+	"github.com/scaleway/scaleway-cli/internal/interactive"
 	"github.com/scaleway/scaleway-sdk-go/api/rdb/v1"
 	"github.com/scaleway/scaleway-sdk-go/scw"
 )
@@ -162,6 +168,191 @@ func instanceWaitCommand() *core.Command {
 				Short:    "Wait for an instance to reach a stable state",
 				ArgsJSON: `{"instance_id": "11111111-1111-1111-1111-111111111111"}`,
 			},
+		},
+	}
+}
+
+type instanceConnectArgs struct {
+	Region     scw.Region
+	InstanceID string
+	Username   string
+	Database   *string
+	CliDb      *string
+}
+
+type engineFamily string
+
+const (
+	Unknown        = engineFamily("Unknown")
+	PostgreSQL     = engineFamily("PostgreSQL")
+	MySQL          = engineFamily("MySQL")
+	postgreSQLHint = `
+psql supports password file to avoid typing your password manually.
+Learn more at: https://www.postgresql.org/docs/current/libpq-pgpass.html`
+	mySQLHint = `
+mysql supports loading your password from a file to avoid typing them manually.
+Learn more at: https://dev.mysql.com/doc/refman/8.0/en/option-files.html`
+)
+
+func passwordFileExist(ctx context.Context, family engineFamily) bool {
+	passwordFilePath := ""
+	switch family {
+	case PostgreSQL:
+		switch runtime.GOOS {
+		case "windows":
+			passwordFilePath = path.Join(core.ExtractUserHomeDir(ctx), core.ExtractEnv(ctx, "APPDATA"), "postgresql", "pgpass.conf")
+		default:
+			passwordFilePath = path.Join(core.ExtractUserHomeDir(ctx), ".pgpass")
+		}
+	case MySQL:
+		passwordFilePath = path.Join(core.ExtractUserHomeDir(ctx), ".my.cnf")
+	default:
+		return false
+	}
+	if passwordFilePath == "" {
+		return false
+	}
+	_, err := os.Stat(passwordFilePath)
+	return err == nil
+}
+
+func passwordFileHint(family engineFamily) string {
+	switch family {
+	case PostgreSQL:
+		return postgreSQLHint
+	case MySQL:
+		return mySQLHint
+	default:
+		return ""
+	}
+}
+
+func detectEngineFamily(instance *rdb.Instance) (engineFamily, error) {
+	if instance == nil {
+		return Unknown, fmt.Errorf("instance engine is nil")
+	}
+	if strings.HasPrefix(instance.Engine, string(PostgreSQL)) {
+		return PostgreSQL, nil
+	}
+	if strings.HasPrefix(instance.Engine, string(MySQL)) {
+		return MySQL, nil
+	}
+	return Unknown, fmt.Errorf("unknown engine: %s", instance.Engine)
+}
+
+func createConnectCommandLineArgs(instance *rdb.Instance, family engineFamily, args *instanceConnectArgs) ([]string, error) {
+	database := "rdb"
+	if args.Database != nil {
+		database = *args.Database
+	}
+
+	switch family {
+	case PostgreSQL:
+		clidb := "psql"
+		if args.CliDb != nil {
+			clidb = *args.CliDb
+		}
+
+		// psql -h 51.159.25.206 --port 13917 -d rdb -U username
+		return []string{
+			clidb,
+			"--host", instance.Endpoint.IP.String(),
+			"--port", fmt.Sprintf("%d", instance.Endpoint.Port),
+			"--username", args.Username,
+			"--dbname", database,
+		}, nil
+	case MySQL:
+		clidb := "mysql"
+		if args.CliDb != nil {
+			clidb = *args.CliDb
+		}
+
+		// mysql -h 195.154.69.163 --port 12210 -p -u username
+		return []string{
+			clidb,
+			"--host", instance.Endpoint.IP.String(),
+			"--port", fmt.Sprintf("%d", instance.Endpoint.Port),
+			"--database", database,
+			"--user", args.Username,
+		}, nil
+	}
+
+	return nil, fmt.Errorf("unrecognize database engine: %s", instance.Engine)
+}
+
+func instanceConnectCommand() *core.Command {
+	return &core.Command{
+		Namespace: "rdb",
+		Resource:  "instance",
+		Verb:      "connect",
+		Short:     "Connect to an instance using local database cli",
+		Long:      "Connect to an instance using local database cli such as psql or mysql.",
+		ArgsType:  reflect.TypeOf(instanceConnectArgs{}),
+		ArgSpecs: core.ArgSpecs{
+			{
+				Name:       "instance-id",
+				Short:      `UUID of the instance`,
+				Required:   true,
+				Positional: true,
+			},
+			{
+				Name:     "username",
+				Short:    "Name of the user to connect with to the database",
+				Required: true,
+			},
+			{
+				Name:  "database",
+				Short: "Name of the database",
+			},
+			{
+				Name:  "cli-db",
+				Short: "Command line tool to use, default to psql/mysql",
+			},
+			core.RegionArgSpec(scw.RegionFrPar, scw.RegionNlAms),
+		},
+		Run: func(ctx context.Context, argsI interface{}) (interface{}, error) {
+			args := argsI.(*instanceConnectArgs)
+
+			client := core.ExtractClient(ctx)
+			api := rdb.NewAPI(client)
+			instance, err := api.GetInstance(&rdb.GetInstanceRequest{
+				Region:     args.Region,
+				InstanceID: args.InstanceID,
+			})
+			if err != nil {
+				return nil, err
+			}
+
+			engineFamily, err := detectEngineFamily(instance)
+			if err != nil {
+				return nil, err
+			}
+
+			cmdArgs, err := createConnectCommandLineArgs(instance, engineFamily, args)
+			if err != nil {
+				return nil, err
+			}
+
+			if !passwordFileExist(ctx, engineFamily) {
+				interactive.Println(passwordFileHint(engineFamily))
+			}
+
+			// Run command
+			cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...) //nolint:gosec
+			//cmd.Stdin = os.Stdin
+			core.ExtractLogger(ctx).Debugf("executing: %s\n", cmd.Args)
+			exitCode, err := core.ExecCmd(ctx, cmd)
+
+			if err != nil {
+				return nil, err
+			}
+			if exitCode != 0 {
+				return nil, &core.CliError{Empty: true, Code: exitCode}
+			}
+
+			return &core.SuccessResult{
+				Empty: true, // the program will output the success message
+			}, nil
 		},
 	}
 }
--- internal/namespaces/rdb/v1/custom_instance_test.go
+++ internal/namespaces/rdb/v1/custom_instance_test.go
@@ -10,7 +10,7 @@ import (
 func Test_CloneInstance(t *testing.T) {
 	t.Run("Simple", core.Test(&core.TestConfig{
 		Commands:   GetCommands(),
-		BeforeFunc: createInstance(),
+		BeforeFunc: createInstance("PostgreSQL-12"),
 		Cmd:        "scw rdb instance clone {{ .Instance.ID }} node-type=DB-DEV-M name=foobar --wait",
 		Check:      core.TestCheckGolden(),
 		AfterFunc:  deleteInstance(),
@@ -29,7 +29,7 @@ func Test_CreateInstance(t *testing.T) {
 func Test_GetInstance(t *testing.T) {
 	t.Run("Simple", core.Test(&core.TestConfig{
 		Commands:   GetCommands(),
-		BeforeFunc: createInstance(),
+		BeforeFunc: createInstance("PostgreSQL-12"),
 		Cmd:        "scw rdb instance get {{ .Instance.ID }}",
 		Check:      core.TestCheckGolden(),
 		AfterFunc:  deleteInstance(),
@@ -39,9 +39,47 @@ func Test_GetInstance(t *testing.T) {
 func Test_UpgradeInstance(t *testing.T) {
 	t.Run("Simple", core.Test(&core.TestConfig{
 		Commands:   GetCommands(),
-		BeforeFunc: createInstance(),
+		BeforeFunc: createInstance("PostgreSQL-12"),
 		Cmd:        "scw rdb instance upgrade {{ .Instance.ID }} node-type=DB-DEV-M --wait",
 		Check:      core.TestCheckGolden(),
 		AfterFunc:  deleteInstance(),
+	}))
+}
+
+func Test_Connect(t *testing.T) {
+	t.Run("mysql", core.Test(&core.TestConfig{
+		Commands: GetCommands(),
+		BeforeFunc: core.BeforeFuncCombine(
+			func(ctx *core.BeforeFuncCtx) error {
+				ctx.Meta["username"] = user
+				return nil
+			},
+			createInstance("MySQL-8"),
+		),
+		Cmd: "scw rdb instance connect {{ .Instance.ID }} username={{ .username }}",
+		Check: core.TestCheckCombine(
+			core.TestCheckGolden(),
+			core.TestCheckExitCode(0),
+		),
+		OverrideExec: core.OverrideExecSimple("mysql --host {{ .Instance.Endpoint.IP }} --port {{ .Instance.Endpoint.Port }} --database rdb --user {{ .username }}", 0),
+		AfterFunc:    deleteInstance(),
+	}))
+
+	t.Run("psql", core.Test(&core.TestConfig{
+		Commands: GetCommands(),
+		BeforeFunc: core.BeforeFuncCombine(
+			func(ctx *core.BeforeFuncCtx) error {
+				ctx.Meta["username"] = user
+				return nil
+			},
+			createInstance("PostgreSQL-12"),
+		),
+		Cmd: "scw rdb instance connect {{ .Instance.ID }} username={{ .username }}",
+		Check: core.TestCheckCombine(
+			core.TestCheckGolden(),
+			core.TestCheckExitCode(0),
+		),
+		OverrideExec: core.OverrideExecSimple("psql --host {{ .Instance.Endpoint.IP }} --port {{ .Instance.Endpoint.Port }} --username {{ .username }} --dbname rdb", 0),
+		AfterFunc:    deleteInstance(),
 	}))
 }
