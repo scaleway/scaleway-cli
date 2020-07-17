9497a039cd0b9c0ec5f4de31f31065a6f3a41e78
Rémy Léone <rleone@scaleway.com>
2020-07-17 15:49:29 +0200

    lb: add color for stock status

--- internal/namespaces/lb/v1/custom.go
+++ internal/namespaces/lb/v1/custom.go
@@ -1,8 +1,14 @@
 package lb
 
-import "github.com/scaleway/scaleway-cli/internal/core"
+import (
+	"github.com/scaleway/scaleway-cli/internal/core"
+	"github.com/scaleway/scaleway-cli/internal/human"
+	"github.com/scaleway/scaleway-sdk-go/api/lb/v1"
+)
 
 func GetCommands() *core.Commands {
+	human.RegisterMarshalerFunc(lb.LbTypeStock(0), human.EnumMarshalFunc(lbTypeStockMarshalSpecs))
+
 	cmds := GetGeneratedCommands()
 	return cmds
 }
