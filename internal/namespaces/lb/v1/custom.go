ae15d6da64bc2a8b265abd0e7ade2dfb3a847eff
Rémy Léone <rleone@scaleway.com>
2020-07-20 12:33:43 +0200

    feat(lb): add human marshalling for lb-types stocks (#1270)

--- internal/namespaces/lb/v1/custom.go
+++ internal/namespaces/lb/v1/custom.go
@@ -2,9 +2,13 @@ package lb
 
 import (
 	"github.com/scaleway/scaleway-cli/internal/core"
+	"github.com/scaleway/scaleway-cli/internal/human"
+	"github.com/scaleway/scaleway-sdk-go/api/lb/v1"
 )
 
 func GetCommands() *core.Commands {
+	human.RegisterMarshalerFunc(lb.LBTypeStock(0), human.EnumMarshalFunc(lbTypeStockMarshalSpecs))
+
 	cmds := GetGeneratedCommands()
 	return cmds
 }
