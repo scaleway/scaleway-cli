9d1c344439827394649e5f2f0fb3bf929b02bbf3
Rémy Léone <rleone@scaleway.com>
2020-07-22 11:56:57 +0200

    feat(lb): add human marshaller for acl accept/deny (#1284)

--- internal/namespaces/lb/v1/custom.go
+++ internal/namespaces/lb/v1/custom.go
@@ -8,6 +8,7 @@ import (
 
 func GetCommands() *core.Commands {
 	human.RegisterMarshalerFunc(lb.LBTypeStock(0), human.EnumMarshalFunc(lbTypeStockMarshalSpecs))
+	human.RegisterMarshalerFunc(lb.ACLActionType(0), human.EnumMarshalFunc(aclMarshalSpecs))
 
 	cmds := GetGeneratedCommands()
 
