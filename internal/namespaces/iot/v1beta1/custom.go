037ef92b22799d5bb6a5ad15822ebba882e7333d
Rémy Léone <rleone@scaleway.com>
2020-08-12 15:37:12 +0200

    feat(iot): add human marshallers for enums
    - HubStatus,
    - DeviceMessageFiltersPolicy
    - DeviceStatus

--- internal/namespaces/iot/v1beta1/custom.go
+++ internal/namespaces/iot/v1beta1/custom.go
@@ -2,10 +2,16 @@ package iot
 
 import (
 	"github.com/scaleway/scaleway-cli/internal/core"
+	"github.com/scaleway/scaleway-cli/internal/human"
+	iot "github.com/scaleway/scaleway-sdk-go/api/iot/v1beta1"
 )
 
 func GetCommands() *core.Commands {
 	cmds := GetGeneratedCommands()
+
+	human.RegisterMarshalerFunc(iot.HubStatus(0), human.EnumMarshalFunc(hubStatusMarshalSpecs))
+	human.RegisterMarshalerFunc(iot.DeviceMessageFiltersPolicy(0), human.EnumMarshalFunc(deviceMessageFiltersPolicyMarshalSpecs))
+	human.RegisterMarshalerFunc(iot.DeviceStatus(0), human.EnumMarshalFunc(deviceStatusMarshalSpecs))
 
 	return cmds
 }
