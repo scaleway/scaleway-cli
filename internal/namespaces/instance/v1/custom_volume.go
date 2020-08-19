cf96cc516da7def09da68553827a093c869e21d9
dri42 <rib.david@gmail.com>
2020-08-19 16:51:19 +0200

    chore: bump sdk version (#1348)

--- internal/namespaces/instance/v1/custom_volume.go
+++ internal/namespaces/instance/v1/custom_volume.go
@@ -39,7 +39,7 @@ func volumeMapMarshalerFunc(i interface{}, opt *human.MarshalOpt) (string, error
 func volumeCreateBuilder(c *core.Command) *core.Command {
 	type customCreateVolumeRequest struct {
 		*instance.CreateVolumeRequest
-		OrganizationID string
+		OrganizationID *string
 	}
 
 	renameOrganizationIDArgSpec(c.ArgSpecs)
