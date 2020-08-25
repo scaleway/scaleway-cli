9a0e923545086b057d7a3b47a76b7943c8f3e02d
Rémy Léone <rleone@scaleway.com>
2020-08-25 14:04:13 +0200

    feat(printer): add support for YAML output (#1308)

--- internal/core/printer_test.go
+++ internal/core/printer_test.go
@@ -65,3 +65,76 @@ func Test_CorePrinter(t *testing.T) {
 		Check:    TestCheckGolden(),
 	}))
 }
+
+func Test_YamlPrinter(t *testing.T) {
+	type Human struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	}
+
+	commands := NewCommands(
+		&Command{
+			Namespace: "get",
+			ArgsType:  reflect.TypeOf(struct{}{}),
+			Run: func(ctx context.Context, argsI interface{}) (interface{}, error) {
+				return Human{
+					ID:   "111111111-111111111",
+					Name: "David Copperfield",
+				}, nil
+			},
+		},
+		&Command{
+			Namespace: "list",
+			ArgsType:  reflect.TypeOf(struct{}{}),
+			Run: func(ctx context.Context, argsI interface{}) (interface{}, error) {
+				return []*Human{
+					{ID: "111111111-111111111", Name: "David Copperfield"},
+					{ID: "222222222-222222222", Name: "Xavier Niel"},
+				}, nil
+			},
+		},
+		&Command{
+			Namespace: "NilSlice",
+			ArgsType:  reflect.TypeOf(struct{}{}),
+			Run: func(ctx context.Context, argsI interface{}) (interface{}, error) {
+				return []Human{}, nil
+			},
+		},
+	)
+
+	t.Run("human-simple-without-option", Test(&TestConfig{
+		Commands: commands,
+		Cmd:      "scw get -o yaml",
+		Check:    TestCheckGolden(),
+	}))
+
+	t.Run("human-simple-with-options", Test(&TestConfig{
+		Commands: commands,
+		Cmd:      "scw get -o yaml=ID,Name",
+		Check:    TestCheckGolden(),
+	}))
+
+	t.Run("human-list-without-option", Test(&TestConfig{
+		Commands: commands,
+		Cmd:      "scw list -o yaml",
+		Check:    TestCheckGolden(),
+	}))
+
+	t.Run("human-list-with-options", Test(&TestConfig{
+		Commands: commands,
+		Cmd:      "scw list -o yaml=Name,ID",
+		Check:    TestCheckGolden(),
+	}))
+
+	t.Run("human-list-with-options-unknown-column", Test(&TestConfig{
+		Commands: commands,
+		Cmd:      "scw -D list -o yaml=Name,ID,Unknown",
+		Check:    TestCheckGolden(),
+	}))
+
+	t.Run("nil-slice", Test(&TestConfig{
+		Commands: commands,
+		Cmd:      "scw NilSlice -o yaml",
+		Check:    TestCheckGolden(),
+	}))
+}
