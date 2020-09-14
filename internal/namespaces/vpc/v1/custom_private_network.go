ae05828ae88375d645397fde2acd9ee81f02ad49
Rémy Léone <rleone@scaleway.com>
2020-09-14 15:37:19 +0200

    feat(vpc): add support to see all servers in a given private network

---
+++ internal/namespaces/vpc/v1/custom_private_network.go
@@ -0,0 +1,64 @@
+package vpc
+
+import (
+	"context"
+
+	"github.com/scaleway/scaleway-cli/internal/core"
+	"github.com/scaleway/scaleway-sdk-go/api/instance/v1"
+	"github.com/scaleway/scaleway-sdk-go/api/vpc/v1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+)
+
+func privateNetworkGetBuilder(c *core.Command) *core.Command {
+	type customServer struct {
+		ServerID   string `json:"server_id"`
+		NicID      string `json:"nic_id"`
+		MacAddress string `json:"mac"`
+	}
+
+	c.Interceptor = func(ctx context.Context, argsI interface{}, runner core.CommandRunner) (interface{}, error) {
+		getPNResp, err := runner(ctx, argsI)
+		if err != nil {
+			return getPNResp, err
+		}
+		pn := getPNResp.(*vpc.PrivateNetwork)
+
+		client := core.ExtractClient(ctx)
+		instanceAPI := instance.NewAPI(client)
+		listServers, err := instanceAPI.ListServers(&instance.ListServersRequest{
+			PrivateNetwork: &pn.ID,
+		}, scw.WithAllPages())
+		if err != nil {
+			return getPNResp, err
+		}
+
+		customServers := []customServer{}
+		for _, server := range listServers.Servers {
+			for _, nic := range server.PrivateNics {
+				if nic.PrivateNetworkID == pn.ID {
+					customServers = append(customServers, customServer{
+						NicID:      nic.ID,
+						ServerID:   nic.ServerID,
+						MacAddress: nic.MacAddress,
+					})
+				}
+			}
+		}
+
+		return &struct {
+			*vpc.PrivateNetwork
+			Servers []customServer `json:"servers"`
+		}{
+			pn,
+			customServers,
+		}, nil
+	}
+
+	c.View = &core.View{
+		Sections: []*core.ViewSection{
+			{FieldName: "Servers", Title: "Servers"},
+		},
+	}
+
+	return c
+}
