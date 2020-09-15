3f79ff24b58f6873b174a23fe13980db4532f28e
Jerome Quere <jquere@scaleway.com>
2020-09-15 16:36:36 +0200

    chore: add docker login in release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,7 @@
 export SCW_SECRET_KEY="$(vault kv get -field scaleway_secret_key agw_kv/prd)"
 export SCW_ACCESS_KEY="$(vault kv get -field scaleway_access_key agw_kv/prd)"
 export GITHUB_TOKEN="$(vault kv get -field cli_release_github_token front_kv/opensource)"
+vault kv get -field cli_release_dockerhub_token front_kv/opensource | docker login --password-stdin -u "$(vault kv get -field dockerhub_bot_username front_kv/opensource)"
 cd scripts/release || (echo "Please run this script from repo root" && exit 1)
 
 yarn install --frozen-lock-file
