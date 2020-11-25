16bf8e5f87c59b39caeca7346aa38c10ee0538bc
Rémy Léone <rleone@scaleway.com>
2020-11-25 10:37:58 +0100

    feat(k8s): add marshallers for get versions

--- internal/namespaces/k8s/v1/custom_version.go
+++ internal/namespaces/k8s/v1/custom_version.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/scaleway/scaleway-cli/internal/core"
+	"github.com/scaleway/scaleway-cli/internal/human"
 	k8s "github.com/scaleway/scaleway-sdk-go/api/k8s/v1"
 )
 
@@ -19,4 +20,40 @@ func versionListBuilder(c *core.Command) *core.Command {
 	})
 
 	return c
+}
+
+func versionMarshalerFunc(i interface{}, opt *human.MarshalOpt) (string, error) {
+	type tmp k8s.Version
+	version := tmp(i.(k8s.Version))
+
+	// Sections
+	opt.Sections = []*human.MarshalSection{
+		{
+			FieldName: "AvailableCnis",
+			Title:     "Available CNIs",
+		},
+		{
+			FieldName: "AvailableIngresses",
+			Title:     "Available Ingresses",
+		},
+		{
+			FieldName: "AvailableContainerRuntimes",
+			Title:     "Available Container Runtimes",
+		},
+		{
+			FieldName: "AvailableFeatureGates",
+			Title:     "Available Feature Gates",
+		},
+		{
+			FieldName: "AvailableAdmissionPlugins",
+			Title:     "Available Admission Plugins",
+		},
+	}
+
+	str, err := human.Marshal(version, opt)
+	if err != nil {
+		return "", err
+	}
+
+	return str, nil
 }
