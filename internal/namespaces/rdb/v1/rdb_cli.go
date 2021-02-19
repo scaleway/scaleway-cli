1bdd5dd2c38a6febd87ad53b217e47e8ed6841a8
Rémy Léone <rleone@scaleway.com>
2021-02-19 17:12:35 +0100

    fix

--- internal/namespaces/rdb/v1/rdb_cli.go
+++ internal/namespaces/rdb/v1/rdb_cli.go
@@ -29,7 +29,6 @@ func GetGeneratedCommands() *core.Commands {
 		rdbDatabase(),
 		rdbNodeType(),
 		rdbLog(),
-		rdbSnapshot(),
 		rdbEngineList(),
 		rdbNodeTypeList(),
 		rdbBackupList(),
@@ -158,16 +157,6 @@ func rdbLog() *core.Command {
 		Long:      `Instance logs management commands.`,
 		Namespace: "rdb",
 		Resource:  "log",
-	}
-}
-
-func rdbSnapshot() *core.Command {
-	return &core.Command{
-		Short: `Block snapshot management`,
-		Long: `Create, restore and manage block snapshot
-`,
-		Namespace: "rdb",
-		Resource:  "snapshot",
 	}
 }
 
