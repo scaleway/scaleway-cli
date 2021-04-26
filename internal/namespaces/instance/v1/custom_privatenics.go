4bf28eaafdc00b605a453f7ad23fe246c477d6cb
Jerome Quere <jquere@scaleway.com>
2021-04-26 12:58:23 +0200

    feat: update scaleway sdk

--- internal/namespaces/instance/v1/custom_privatenics.go
+++ internal/namespaces/instance/v1/custom_privatenics.go
@@ -3,8 +3,18 @@ package instance
 import (
 	"context"
 
+	"github.com/fatih/color"
 	"github.com/scaleway/scaleway-cli/internal/core"
+	"github.com/scaleway/scaleway-cli/internal/human"
 	"github.com/scaleway/scaleway-sdk-go/api/instance/v1"
+)
+
+var (
+	privateNICStateMarshalSpecs = human.EnumMarshalSpecs{
+		instance.PrivateNICStateAvailable:    &human.EnumMarshalSpec{Attribute: color.FgGreen},
+		instance.PrivateNICStateSyncing:      &human.EnumMarshalSpec{Attribute: color.FgBlue},
+		instance.PrivateNICStateSyncingError: &human.EnumMarshalSpec{Attribute: color.FgRed},
+	}
 )
 
 func privateNicListBuilder(c *core.Command) *core.Command {
