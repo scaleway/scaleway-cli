452cdb36fffbf887f5fc465cafe4725bfa02656b
Rémy Léone <rleone@scaleway.com>
2021-10-21 17:08:41 +0200

    Fix

--- internal/namespaces/flexibleip/v1alpha1/custom_mac.go
+++ internal/namespaces/flexibleip/v1alpha1/custom_mac.go
@@ -9,9 +9,9 @@ import (
 var (
 	macAddressStatusMarshalSpecs = human.EnumMarshalSpecs{
 		flexibleip.MACAddressStatusUnknown:  &human.EnumMarshalSpec{Attribute: color.Faint},
-		flexibleip.MACAddressStatusReady:    &human.EnumMarshalSpec{Attribute: color.FgGreen},
+		flexibleip.MACAddressStatusReady:    &human.EnumMarshalSpec{Attribute: color.FgCyan},
 		flexibleip.MACAddressStatusUpdating: &human.EnumMarshalSpec{Attribute: color.FgBlue},
-		// flexibleip.MACAddressStatusUsed:     &human.EnumMarshalSpec{Attribute: color.Faint},
+		flexibleip.MACAddressStatusUsed:     &human.EnumMarshalSpec{Attribute: color.FgGreen},
 		flexibleip.MACAddressStatusError:    &human.EnumMarshalSpec{Attribute: color.FgRed},
 		flexibleip.MACAddressStatusDeleting: &human.EnumMarshalSpec{Attribute: color.FgBlue},
 	}
