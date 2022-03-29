8dc5b6e6d5580648038772b1557bc8c1d5b52095
Rémy Léone <rleone@scaleway.com>
2022-03-29 14:28:33 +0200

    Fix

--- internal/namespaces/rdb/v1/rdb_cli.go
+++ internal/namespaces/rdb/v1/rdb_cli.go
@@ -30,7 +30,6 @@ func GetGeneratedCommands() *core.Commands {
 		rdbNodeType(),
 		rdbLog(),
 		rdbSnapshot(),
-		rdbReadReplica(),
 		rdbEngineList(),
 		rdbNodeTypeList(),
 		rdbBackupList(),
@@ -179,16 +178,6 @@ func rdbSnapshot() *core.Command {
 `,
 		Namespace: "rdb",
 		Resource:  "snapshot",
-	}
-}
-
-func rdbReadReplica() *core.Command {
-	return &core.Command{
-		Short: `Read replica management`,
-		Long: `A read replica is a live copy of the main database instance only available for reading. Read replica allows you to scale your database instance for read-heavy database workloads. Read replicas can also be used for Business Intelligence workloads.
-`,
-		Namespace: "rdb",
-		Resource:  "read-replica",
 	}
 }
 
