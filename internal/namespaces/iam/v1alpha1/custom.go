57e2951c116f8d26c771b8589f7ee8c404ba91b4
Olivier CANO <ocano@scaleway.com>
2022-08-22 17:57:51 +0200

    fix(iam): set organization ID filter on listing commands

--- internal/namespaces/iam/v1alpha1/custom.go
+++ internal/namespaces/iam/v1alpha1/custom.go
@@ -1,9 +1,33 @@
 package iam
 
-import "github.com/scaleway/scaleway-cli/v2/internal/core"
+import (
+	"context"
+
+	"github.com/scaleway/scaleway-cli/v2/internal/core"
+)
 
 func GetCommands() *core.Commands {
 	cmds := GetGeneratedCommands()
 
+	// These commands have an "optional" organization-id that is required for now.
+	for _, commandPath := range [][]string{
+		{"iam", "group", "list"},
+		{"iam", "api-key", "list"},
+		{"iam", "ssh-key", "list"},
+		{"iam", "user", "list"},
+		{"iam", "policy", "list"},
+		{"iam", "application", "list"},
+	} {
+		cmds.MustFind(commandPath...).Override(setOrganizationDefaultValue)
+	}
+
 	return cmds
+}
+
+func setOrganizationDefaultValue(c *core.Command) *core.Command {
+	c.ArgSpecs.GetByName("organization-id").Default = func(ctx context.Context) (value string, doc string) {
+		organizationID := core.GetOrganizationIDFromContext(ctx)
+		return organizationID, "<retrieved from config>"
+	}
+	return c
 }
