d19e6adba25e31120099294833af0b76633bf0ac
Yacine Fodil <105779815+yfodil@users.noreply.github.com>
2022-11-16 13:02:15 +0100

    feat(iot): improve network create output (#2584)

---
+++ internal/namespaces/iot/v1/custom_network.go
@@ -0,0 +1,35 @@
+package iot
+
+import (
+	"strings"
+
+	"github.com/fatih/color"
+	"github.com/scaleway/scaleway-cli/v2/internal/human"
+	"github.com/scaleway/scaleway-cli/v2/internal/terminal"
+	"github.com/scaleway/scaleway-sdk-go/api/iot/v1"
+)
+
+func iotNetworkCreateResponsedMarshalerFunc(i interface{}, opt *human.MarshalOpt) (string, error) {
+	type tmp iot.CreateNetworkResponse
+	networkCreateResponse := tmp(i.(iot.CreateNetworkResponse))
+
+	networkContent, err := human.Marshal(networkCreateResponse.Network, opt)
+	if err != nil {
+		return "", err
+	}
+	networkView := terminal.Style("Network:\n", color.Bold) + networkContent
+
+	secretContent, err := human.Marshal(networkCreateResponse.Secret, opt)
+	if err != nil {
+		return "", err
+	}
+	secretView := terminal.Style("Secret: ", color.Bold) + secretContent
+
+	warningSecret := "WARNING: The secret below is displayed only once, we do not keep it. Make sure to store it securely"
+
+	return strings.Join([]string{
+		networkView,
+		warningSecret,
+		secretView,
+	}, "\n\n"), nil
+}
