0f1e1948b58200569151e29b99daaf52c092fb97
Jules Castéran <jcasteran@scaleway.com>
2022-11-19 18:38:54 +0100

    add tasks cleanup

--- internal/tasks/tasks.go
+++ internal/tasks/tasks.go
@@ -8,11 +8,15 @@ import (
 	"github.com/briandowns/spinner"
 )
 
-type Task func(interface{}) (interface{}, error)
+type Task func(args interface{}) (nextArgs interface{}, err error)
+type TaskWithCleanup func(args interface{}) (nextArgs interface{}, cleanUpArgs interface{}, err error)
+type CleanUpTask func(cleanUpArgs interface{}) error
 
 type taskInfo struct {
-	Name     string
-	function Task
+	Name          string
+	function      TaskWithCleanup
+	cleanFunction CleanUpTask
+	cleanUpArgs   interface{}
 }
 
 type Tasks struct {
@@ -25,9 +29,39 @@ func Begin() *Tasks {
 
 func (ts *Tasks) Add(name string, task Task) {
 	ts.tasks = append(ts.tasks, taskInfo{
-		Name:     name,
-		function: task,
+		Name: name,
+		function: func(i interface{}) (passedData interface{}, cleanUpData interface{}, err error) {
+			passedData, err = task(i)
+			return
+		},
 	})
+}
+
+func (ts *Tasks) AddWithCleanUp(name string, task TaskWithCleanup, clean CleanUpTask) {
+	ts.tasks = append(ts.tasks, taskInfo{
+		Name:          name,
+		function:      task,
+		cleanFunction: clean,
+	})
+}
+
+// Cleanup execute all tasks cleanup function before failed one in reverse order
+func (ts *Tasks) Cleanup(failed int) {
+	totalTasks := len(ts.tasks)
+
+	i := failed - 1
+	for ; i >= 0; i -= 1 {
+		task := ts.tasks[i]
+
+		if task.cleanFunction != nil {
+			fmt.Printf("[%d/%d] Cleaning task %q\n", i+1, totalTasks, task.Name)
+
+			err := task.cleanFunction(task.cleanUpArgs)
+			if err != nil {
+				fmt.Printf("task %d failed to cleanup: %s", i+1, err.Error())
+			}
+		}
+	}
 }
 
 func (ts *Tasks) Execute(data interface{}) (interface{}, error) {
@@ -36,14 +70,19 @@ func (ts *Tasks) Execute(data interface{}) (interface{}, error) {
 	rand.Seed(time.Now().UnixNano())
 	spin := spinner.New(spinner.CharSets[rand.Int()%37], 100*time.Millisecond)
 
-	for i, task := range ts.tasks {
+	for i := range ts.tasks {
+		task := &ts.tasks[i]
 		fmt.Printf("[%d/%d] %s\n", i+1, totalTasks, task.Name)
 		spin.Start()
-		data, err = task.function(data)
+
+		data, task.cleanUpArgs, err = task.function(data)
 		if err != nil {
 			spin.Stop()
-			return nil, fmt.Errorf("task [%d/%d] failed: %w", i, totalTasks, err)
+			fmt.Println("task failed, cleaning up created resources")
+			ts.Cleanup(i)
+			return nil, fmt.Errorf("task %d %q failed: %w", i+1, task.Name, err)
 		}
+
 		spin.Stop()
 	}
 
