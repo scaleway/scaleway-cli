a33a8a0d23148ada7dc743b7e5d8c99775f8ca7d
Jules Castéran <jcasteran@scaleway.com>
2022-12-16 16:04:28 +0100

    feat(container): add waiter to container deploy (#2681)
    Co-authored-by: Rémy Léone <rleone@scaleway.com>

--- internal/namespaces/container/v1beta1/custom_container.go
+++ internal/namespaces/container/v1beta1/custom_container.go
@@ -1,12 +1,19 @@
 package container
 
 import (
+	"context"
+	"time"
+
 	"github.com/fatih/color"
+	"github.com/scaleway/scaleway-cli/v2/internal/core"
 	"github.com/scaleway/scaleway-cli/v2/internal/human"
 	container "github.com/scaleway/scaleway-sdk-go/api/container/v1beta1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
 )
 
 var (
+	containerDeployTimeout = 12*time.Minute + 30*time.Second
+
 	containerStatusMarshalSpecs = human.EnumMarshalSpecs{
 		container.ContainerStatusCreated:  &human.EnumMarshalSpec{Attribute: color.FgGreen},
 		container.ContainerStatusCreating: &human.EnumMarshalSpec{Attribute: color.FgBlue},
@@ -18,3 +25,19 @@ var (
 		container.ContainerStatusUnknown:  &human.EnumMarshalSpec{Attribute: color.Faint},
 	}
 )
+
+func containerContainerDeployBuilder(command *core.Command) *core.Command {
+	command.WaitFunc = func(ctx context.Context, argsI, respI interface{}) (interface{}, error) {
+		req := argsI.(*container.DeployContainerRequest)
+
+		client := core.ExtractClient(ctx)
+		api := container.NewAPI(client)
+		return api.WaitForContainer(&container.WaitForContainerRequest{
+			ContainerID:   req.ContainerID,
+			Region:        req.Region,
+			Timeout:       scw.TimeDurationPtr(containerDeployTimeout),
+			RetryInterval: core.DefaultRetryInterval,
+		})
+	}
+	return command
+}
