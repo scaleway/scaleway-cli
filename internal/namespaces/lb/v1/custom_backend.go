7a3575380b0c0932167aeaf25db2a58cd123e974
Yacine Fodil <105779815+yfodil@users.noreply.github.com>
2022-12-16 15:38:04 +0100

    feat(lb): add servers to backend via id (#2679)

--- internal/namespaces/lb/v1/custom_backend.go
+++ internal/namespaces/lb/v1/custom_backend.go
@@ -2,11 +2,15 @@ package lb
 
 import (
 	"context"
+	"fmt"
+	"reflect"
 	"strings"
 
 	"github.com/fatih/color"
 	"github.com/scaleway/scaleway-cli/v2/internal/core"
 	"github.com/scaleway/scaleway-cli/v2/internal/human"
+	"github.com/scaleway/scaleway-sdk-go/api/baremetal/v1"
+	"github.com/scaleway/scaleway-sdk-go/api/instance/v1"
 	"github.com/scaleway/scaleway-sdk-go/api/lb/v1"
 )
 
@@ -68,7 +72,116 @@ func backendGetBuilder(c *core.Command) *core.Command {
 }
 
 func backendCreateBuilder(c *core.Command) *core.Command {
+	type lbBackendCreateRequestCustom struct {
+		lb.ZonedAPICreateBackendRequest
+		InstanceServerID          []string
+		BaremetalServerID         []string
+		UseInstanceServerPublicIP bool
+	}
+
+	c.ArgsType = reflect.TypeOf(lbBackendCreateRequestCustom{})
+
+	c.ArgSpecs.AddBefore("server-ip.{index}", &core.ArgSpec{
+		Name:  "instance-server-id.{index}",
+		Short: "UIID of the instance server.",
+	})
+
+	c.ArgSpecs.AddBefore("server-ip.{index}", &core.ArgSpec{
+		Name:  "use-instance-server-public-ip",
+		Short: "Use public IP address of the instance instead of the private one",
+	})
+
+	c.ArgSpecs.AddBefore("server-ip.{index}", &core.ArgSpec{
+		Name:  "baremetal-server-id.{index}",
+		Short: "UIID of the baremetal server.",
+	})
+
+	c.Run = func(ctx context.Context, argsI interface{}) (interface{}, error) {
+		client := core.ExtractClient(ctx)
+		api := lb.NewZonedAPI(client)
+
+		tmpRequest := argsI.(*lbBackendCreateRequestCustom)
+
+		request := &lb.ZonedAPICreateBackendRequest{
+			Zone:                     tmpRequest.Zone,
+			LBID:                     tmpRequest.LBID,
+			Name:                     tmpRequest.Name,
+			ForwardProtocol:          tmpRequest.ForwardProtocol,
+			ForwardPort:              tmpRequest.ForwardPort,
+			ForwardPortAlgorithm:     tmpRequest.ForwardPortAlgorithm,
+			StickySessions:           tmpRequest.StickySessions,
+			StickySessionsCookieName: tmpRequest.StickySessionsCookieName,
+			HealthCheck:              tmpRequest.HealthCheck,
+			ServerIP:                 tmpRequest.ServerIP,
+			TimeoutServer:            tmpRequest.TimeoutServer,
+			TimeoutConnect:           tmpRequest.TimeoutConnect,
+			TimeoutTunnel:            tmpRequest.TimeoutTunnel,
+			OnMarkedDownAction:       tmpRequest.OnMarkedDownAction,
+			ProxyProtocol:            tmpRequest.ProxyProtocol,
+			FailoverHost:             tmpRequest.FailoverHost,
+			SslBridging:              tmpRequest.SslBridging,
+			IgnoreSslServerVerify:    tmpRequest.IgnoreSslServerVerify,
+		}
+
+		// IP/ID management
+		if len(tmpRequest.InstanceServerID) != 0 {
+			var serverIPs []string
+
+			for _, instanceID := range tmpRequest.InstanceServerID {
+				instanceAPI := instance.NewAPI(core.ExtractClient(ctx))
+
+				server, err := instanceAPI.GetServer(&instance.GetServerRequest{
+					Zone:     tmpRequest.Zone,
+					ServerID: instanceID,
+				})
+				if err != nil {
+					return nil, err
+				}
+
+				if tmpRequest.UseInstanceServerPublicIP {
+					if server.Server.PublicIP == nil {
+						return nil, &core.CliError{
+							Message: fmt.Sprintf("server %s (%s) does not have a public ip", server.Server.ID, server.Server.Name),
+						}
+					}
+					serverIPs = append(serverIPs, server.Server.PublicIP.Address.String())
+				} else {
+					if server.Server.PrivateIP == nil {
+						return nil, &core.CliError{
+							Message: fmt.Sprintf("server %s (%s) does not have a private ip", server.Server.ID, server.Server.Name),
+							Hint:    fmt.Sprintf("Private ip are assigned when the server boots, start yours with: scw instance server start %s", server.Server.ID),
+						}
+					}
+					serverIPs = append(serverIPs, *server.Server.PrivateIP)
+				}
+
+				request.ServerIP = append(request.ServerIP, serverIPs...)
+			}
+		}
+
+		if len(tmpRequest.BaremetalServerID) != 0 {
+			for _, baremetalID := range tmpRequest.BaremetalServerID {
+				baremetalAPI := baremetal.NewAPI(core.ExtractClient(ctx))
+
+				server, err := baremetalAPI.GetServer(&baremetal.GetServerRequest{
+					Zone:     tmpRequest.Zone,
+					ServerID: baremetalID,
+				})
+				if err != nil {
+					return nil, err
+				}
+
+				for _, ip := range server.IPs {
+					request.ServerIP = append(request.ServerIP, ip.Address.String())
+				}
+			}
+		}
+
+		return api.CreateBackend(request)
+	}
+
 	c.Interceptor = interceptBackend()
+
 	return c
 }
 
@@ -83,16 +196,295 @@ func backendDeleteBuilder(c *core.Command) *core.Command {
 }
 
 func backendAddServersBuilder(c *core.Command) *core.Command {
+	type lbBackendAddBackendServersRequestCustom struct {
+		lb.ZonedAPIAddBackendServersRequest
+		InstanceServerID          []string
+		BaremetalServerID         []string
+		UseInstanceServerPublicIP bool
+	}
+
+	c.ArgsType = reflect.TypeOf(lbBackendAddBackendServersRequestCustom{})
+
+	c.ArgSpecs.AddBefore("server-ip.{index}", &core.ArgSpec{
+		Name:  "instance-server-id.{index}",
+		Short: "UIID of the instance server.",
+	})
+
+	c.ArgSpecs.AddBefore("server-ip.{index}", &core.ArgSpec{
+		Name:  "use-instance-server-public-ip",
+		Short: "Use public IP address of the instance instead of the private one",
+	})
+
+	c.ArgSpecs.AddBefore("server-ip.{index}", &core.ArgSpec{
+		Name:  "baremetal-server-id.{index}",
+		Short: "UIID of the baremetal server.",
+	})
+
+	c.Run = func(ctx context.Context, argsI interface{}) (interface{}, error) {
+		client := core.ExtractClient(ctx)
+		api := lb.NewZonedAPI(client)
+
+		tmpRequest := argsI.(*lbBackendAddBackendServersRequestCustom)
+
+		request := &lb.ZonedAPIAddBackendServersRequest{
+			Zone:      tmpRequest.Zone,
+			BackendID: tmpRequest.BackendID,
+			ServerIP:  tmpRequest.ServerIP,
+		}
+
+		// IP/ID management
+		if len(tmpRequest.InstanceServerID) != 0 {
+			var serverIPs []string
+
+			for _, instanceID := range tmpRequest.InstanceServerID {
+				instanceAPI := instance.NewAPI(core.ExtractClient(ctx))
+
+				server, err := instanceAPI.GetServer(&instance.GetServerRequest{
+					Zone:     tmpRequest.Zone,
+					ServerID: instanceID,
+				})
+				if err != nil {
+					return nil, err
+				}
+
+				if tmpRequest.UseInstanceServerPublicIP {
+					if server.Server.PublicIP == nil {
+						return nil, &core.CliError{
+							Message: fmt.Sprintf("server %s (%s) does not have a public ip", server.Server.ID, server.Server.Name),
+						}
+					}
+					serverIPs = append(serverIPs, server.Server.PublicIP.Address.String())
+				} else {
+					if server.Server.PrivateIP == nil {
+						return nil, &core.CliError{
+							Message: fmt.Sprintf("server %s (%s) does not have a private ip", server.Server.ID, server.Server.Name),
+							Hint:    fmt.Sprintf("Private ip are assigned when the server boots, start yours with: scw instance server start %s", server.Server.ID),
+						}
+					}
+					serverIPs = append(serverIPs, *server.Server.PrivateIP)
+				}
+
+				request.ServerIP = append(request.ServerIP, serverIPs...)
+			}
+		}
+
+		if len(tmpRequest.BaremetalServerID) != 0 {
+			for _, baremetalID := range tmpRequest.BaremetalServerID {
+				baremetalAPI := baremetal.NewAPI(core.ExtractClient(ctx))
+
+				server, err := baremetalAPI.GetServer(&baremetal.GetServerRequest{
+					Zone:     tmpRequest.Zone,
+					ServerID: baremetalID,
+				})
+				if err != nil {
+					return nil, err
+				}
+
+				for _, ip := range server.IPs {
+					request.ServerIP = append(request.ServerIP, ip.Address.String())
+				}
+			}
+		}
+
+		return api.AddBackendServers(request)
+	}
+
 	c.Interceptor = interceptBackend()
 	return c
 }
 
 func backendRemoveServersBuilder(c *core.Command) *core.Command {
+	type lbBackendRemoveBackendServersRequestCustom struct {
+		lb.ZonedAPIRemoveBackendServersRequest
+		InstanceServerID          []string
+		BaremetalServerID         []string
+		UseInstanceServerPublicIP bool
+	}
+
+	c.ArgsType = reflect.TypeOf(lbBackendRemoveBackendServersRequestCustom{})
+
+	c.ArgSpecs.AddBefore("server-ip.{index}", &core.ArgSpec{
+		Name:  "instance-server-id.{index}",
+		Short: "UIID of the instance server.",
+	})
+
+	c.ArgSpecs.AddBefore("server-ip.{index}", &core.ArgSpec{
+		Name:  "use-instance-server-public-ip",
+		Short: "Use public IP address of the instance instead of the private one",
+	})
+
+	c.ArgSpecs.AddBefore("server-ip.{index}", &core.ArgSpec{
+		Name:  "baremetal-server-id.{index}",
+		Short: "UIID of the baremetal server.",
+	})
+
+	c.Run = func(ctx context.Context, argsI interface{}) (interface{}, error) {
+		client := core.ExtractClient(ctx)
+		api := lb.NewZonedAPI(client)
+
+		tmpRequest := argsI.(*lbBackendRemoveBackendServersRequestCustom)
+
+		request := &lb.ZonedAPIRemoveBackendServersRequest{
+			Zone:      tmpRequest.Zone,
+			BackendID: tmpRequest.BackendID,
+			ServerIP:  tmpRequest.ServerIP,
+		}
+
+		// IP/ID management
+		if len(tmpRequest.InstanceServerID) != 0 {
+			var serverIPs []string
+
+			for _, instanceID := range tmpRequest.InstanceServerID {
+				instanceAPI := instance.NewAPI(core.ExtractClient(ctx))
+
+				server, err := instanceAPI.GetServer(&instance.GetServerRequest{
+					Zone:     tmpRequest.Zone,
+					ServerID: instanceID,
+				})
+				if err != nil {
+					return nil, err
+				}
+
+				if tmpRequest.UseInstanceServerPublicIP {
+					if server.Server.PublicIP == nil {
+						return nil, &core.CliError{
+							Message: fmt.Sprintf("server %s (%s) does not have a public ip", server.Server.ID, server.Server.Name),
+						}
+					}
+					serverIPs = append(serverIPs, server.Server.PublicIP.Address.String())
+				} else {
+					if server.Server.PrivateIP == nil {
+						return nil, &core.CliError{
+							Message: fmt.Sprintf("server %s (%s) does not have a private ip", server.Server.ID, server.Server.Name),
+							Hint:    fmt.Sprintf("Private ip are assigned when the server boots, start yours with: scw instance server start %s", server.Server.ID),
+						}
+					}
+					serverIPs = append(serverIPs, *server.Server.PrivateIP)
+				}
+
+				request.ServerIP = append(request.ServerIP, serverIPs...)
+			}
+		}
+
+		if len(tmpRequest.BaremetalServerID) != 0 {
+			for _, baremetalID := range tmpRequest.BaremetalServerID {
+				baremetalAPI := baremetal.NewAPI(core.ExtractClient(ctx))
+
+				server, err := baremetalAPI.GetServer(&baremetal.GetServerRequest{
+					Zone:     tmpRequest.Zone,
+					ServerID: baremetalID,
+				})
+				if err != nil {
+					return nil, err
+				}
+
+				for _, ip := range server.IPs {
+					request.ServerIP = append(request.ServerIP, ip.Address.String())
+				}
+			}
+		}
+
+		return api.RemoveBackendServers(request)
+	}
+
 	c.Interceptor = interceptBackend()
 	return c
 }
 
 func backendSetServersBuilder(c *core.Command) *core.Command {
+	type lbBackendSetBackendServersRequestCustom struct {
+		lb.ZonedAPISetBackendServersRequest
+		InstanceServerID          []string
+		BaremetalServerID         []string
+		UseInstanceServerPublicIP bool
+	}
+
+	c.ArgsType = reflect.TypeOf(lbBackendSetBackendServersRequestCustom{})
+
+	c.ArgSpecs.AddBefore("server-ip.{index}", &core.ArgSpec{
+		Name:  "instance-server-id.{index}",
+		Short: "UIID of the instance server.",
+	})
+
+	c.ArgSpecs.AddBefore("server-ip.{index}", &core.ArgSpec{
+		Name:  "use-instance-server-public-ip",
+		Short: "Use public IP address of the instance instead of the private one",
+	})
+
+	c.ArgSpecs.AddBefore("server-ip.{index}", &core.ArgSpec{
+		Name:  "baremetal-server-id.{index}",
+		Short: "UIID of the baremetal server.",
+	})
+
+	c.Run = func(ctx context.Context, argsI interface{}) (interface{}, error) {
+		client := core.ExtractClient(ctx)
+		api := lb.NewZonedAPI(client)
+
+		tmpRequest := argsI.(*lbBackendSetBackendServersRequestCustom)
+
+		request := &lb.ZonedAPISetBackendServersRequest{
+			Zone:      tmpRequest.Zone,
+			BackendID: tmpRequest.BackendID,
+			ServerIP:  tmpRequest.ServerIP,
+		}
+
+		// IP/ID management
+		if len(tmpRequest.InstanceServerID) != 0 {
+			var serverIPs []string
+
+			for _, instanceID := range tmpRequest.InstanceServerID {
+				instanceAPI := instance.NewAPI(core.ExtractClient(ctx))
+
+				server, err := instanceAPI.GetServer(&instance.GetServerRequest{
+					Zone:     tmpRequest.Zone,
+					ServerID: instanceID,
+				})
+				if err != nil {
+					return nil, err
+				}
+
+				if tmpRequest.UseInstanceServerPublicIP {
+					if server.Server.PublicIP == nil {
+						return nil, &core.CliError{
+							Message: fmt.Sprintf("server %s (%s) does not have a public ip", server.Server.ID, server.Server.Name),
+						}
+					}
+					serverIPs = append(serverIPs, server.Server.PublicIP.Address.String())
+				} else {
+					if server.Server.PrivateIP == nil {
+						return nil, &core.CliError{
+							Message: fmt.Sprintf("server %s (%s) does not have a private ip", server.Server.ID, server.Server.Name),
+							Hint:    fmt.Sprintf("Private ip are assigned when the server boots, start yours with: scw instance server start %s", server.Server.ID),
+						}
+					}
+					serverIPs = append(serverIPs, *server.Server.PrivateIP)
+				}
+
+				request.ServerIP = append(request.ServerIP, serverIPs...)
+			}
+		}
+
+		if len(tmpRequest.BaremetalServerID) != 0 {
+			for _, baremetalID := range tmpRequest.BaremetalServerID {
+				baremetalAPI := baremetal.NewAPI(core.ExtractClient(ctx))
+
+				server, err := baremetalAPI.GetServer(&baremetal.GetServerRequest{
+					Zone:     tmpRequest.Zone,
+					ServerID: baremetalID,
+				})
+				if err != nil {
+					return nil, err
+				}
+
+				for _, ip := range server.IPs {
+					request.ServerIP = append(request.ServerIP, ip.Address.String())
+				}
+			}
+		}
+
+		return api.SetBackendServers(request)
+	}
+
 	c.Interceptor = interceptBackend()
 	return c
 }
--- internal/namespaces/lb/v1/custom_backend_test.go
+++ internal/namespaces/lb/v1/custom_backend_test.go
@@ -27,3 +27,71 @@ func Test_GetBackend(t *testing.T) {
 		),
 	}))
 }
+
+func Test_CreateBackend(t *testing.T) {
+	cmds := GetCommands()
+	cmds.Merge(instance.GetCommands())
+
+	t.Run("With instance ID", core.Test(&core.TestConfig{
+		Commands: cmds,
+		BeforeFunc: core.BeforeFuncCombine(
+			createLB(),
+			createRunningInstance(),
+		),
+		Cmd:   "scw lb backend create lb-id={{ .LB.ID }} name=cli-test instance-server-id.0={{ .Instance.ID }} forward-protocol=tcp forward-port=80 forward-port-algorithm=roundrobin sticky-sessions=none health-check.port=8888 health-check.check-max-retries=5",
+		Check: core.TestCheckGolden(),
+		AfterFunc: core.AfterFuncCombine(
+			deleteLB(),
+			deleteRunningInstance(),
+		),
+	}))
+
+	t.Run("With instance ID public IP", core.Test(&core.TestConfig{
+		Commands: cmds,
+		BeforeFunc: core.BeforeFuncCombine(
+			createLB(),
+			createRunningInstance(),
+		),
+		Cmd:   "scw lb backend create lb-id={{ .LB.ID }} name=cli-test instance-server-id.0={{ .Instance.ID }} use-instance-server-public-ip=true forward-protocol=tcp forward-port=80 forward-port-algorithm=roundrobin sticky-sessions=none health-check.port=8888 health-check.check-max-retries=5",
+		Check: core.TestCheckGolden(),
+		AfterFunc: core.AfterFuncCombine(
+			deleteLB(),
+			deleteRunningInstance(),
+		),
+	}))
+}
+
+func Test_AddBackendServers(t *testing.T) {
+	cmds := GetCommands()
+	cmds.Merge(instance.GetCommands())
+
+	t.Run("With instance ID", core.Test(&core.TestConfig{
+		Commands: cmds,
+		BeforeFunc: core.BeforeFuncCombine(
+			createLB(),
+			createRunningInstance(),
+			createBackend(80),
+		),
+		Cmd:   "scw lb backend add-servers {{ .Backend.ID }} instance-server-id.0={{ .Instance.ID }}",
+		Check: core.TestCheckGolden(),
+		AfterFunc: core.AfterFuncCombine(
+			deleteLB(),
+			deleteRunningInstance(),
+		),
+	}))
+
+	t.Run("With instance ID public IP", core.Test(&core.TestConfig{
+		Commands: cmds,
+		BeforeFunc: core.BeforeFuncCombine(
+			createLB(),
+			createRunningInstance(),
+			createBackend(80),
+		),
+		Cmd:   "scw lb backend add-servers {{ .Backend.ID }} instance-server-id.0={{ .Instance.ID }} use-instance-server-public-ip=true",
+		Check: core.TestCheckGolden(),
+		AfterFunc: core.AfterFuncCombine(
+			deleteLB(),
+			deleteRunningInstance(),
+		),
+	}))
+}
