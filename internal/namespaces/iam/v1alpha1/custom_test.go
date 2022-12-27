a4f16523cf9bd6d38649651d69bf922a8c96ada8
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2022-12-27 17:32:58 +0100

    fix(init): update test init simple with ssh-key

--- internal/namespaces/iam/v1alpha1/custom_test.go
+++ internal/namespaces/iam/v1alpha1/custom_test.go
@@ -1,42 +1,11 @@
 package iam
 
 import (
-	"os"
-	"path"
 	"testing"
 
 	"github.com/scaleway/scaleway-cli/v2/internal/core"
 	iamsdk "github.com/scaleway/scaleway-sdk-go/api/iam/v1alpha1"
 )
-
-func Test_initCommand(t *testing.T) {
-	tmpDir := os.TempDir()
-	t.Run("simple", core.Test(&core.TestConfig{
-		Commands: GetCommands(),
-		BeforeFunc: func(ctx *core.BeforeFuncCtx) error {
-			pathToPublicKey := path.Join(tmpDir, ".ssh", "id_rsa.pub")
-			_, err := os.Stat(pathToPublicKey)
-			if err != nil {
-				content := "ssh-rsa AAAAB3NzaC1yc2EAAAEDAQABAAABAQC/wF8Q8LjEexuWDc8TfKmWVZ1CiiHK6KvO0E/Rk9+d6ssqbrvtbJWRsJXZFC8+DGWVM0UFFicmOfTwEjDWzuQPFkYhmpXrD1UiLx9Viku1g1qEJgcsyH2uAwwW3OnsH1W44D6Ni/zOzMButFeKZgPeD8H9YNkpbZBZ9QrKFiAhvEyJDYSY0bsbH1/qR5DE+dLNuGlJ/g3kUMVaXSI6dHNcBHTbK0Mse23Uopk2U3BSpvX9JdbcLaYtHDOytwd16rNYui7el3uOmlR8oUpAXkeKQxBPoxgy3qI/P8/l44L9RFpbklkmdiw2ph2ymiSkRSYCWdvEVIK/A+0D8VFjGXOb email"
-				err := os.MkdirAll(path.Join(tmpDir, ".ssh"), 0755)
-				if err != nil {
-					return err
-				}
-				err = os.WriteFile(pathToPublicKey, []byte(content), 0644)
-				return err
-			}
-			return err
-		},
-		Cmd: `scw iam ssh-key init with-ssh-key=true`,
-		Check: core.TestCheckCombine(
-			core.TestCheckGolden(),
-			core.TestCheckExitCode(0),
-		),
-		OverrideEnv: map[string]string{
-			"HOME": tmpDir,
-		},
-	}))
-}
 
 func Test_SSHKeyAddCommand(t *testing.T) {
 	key := `ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBieay3nO9wViPkuvFVgGGaA1IRlkFrr946yqvg9LxZIRhsnZ61yLCPmIOhvUAZ/gTxZGmhgtMDxkenSUTsG3F0= foobar@foobar`
--- internal/namespaces/init/init_test.go
+++ internal/namespaces/init/init_test.go
@@ -2,6 +2,7 @@ package init
 
 import (
 	"fmt"
+	"os"
 	"path"
 	"testing"
 
@@ -33,6 +34,46 @@ func beforeFuncSaveConfig(config *scw.Config) core.BeforeFunc {
 		// Persist the dummy Config in the temp directory
 		return config.SaveTo(path.Join(ctx.OverrideEnv["HOME"], ".config", "scw", "config.yaml"))
 	}
+}
+
+func Test_initCommand(t *testing.T) {
+	tmpDir := os.TempDir()
+	defaultArgs := map[string]string{
+		"access-key":           "{{ .AccessKey }}",
+		"secret-key":           "{{ .SecretKey }}",
+		"send-telemetry":       "true",
+		"install-autocomplete": "false",
+		"with-ssh-key":         "true",
+	}
+
+	t.Run("simple", core.Test(&core.TestConfig{
+		Commands:   GetCommands(),
+		TmpHomeDir: true,
+		BeforeFunc: func(ctx *core.BeforeFuncCtx) error {
+			ctx.Meta["AccessKey"], _ = ctx.Client.GetAccessKey()
+			ctx.Meta["SecretKey"], _ = ctx.Client.GetSecretKey()
+			pathToPublicKey := path.Join(tmpDir, ".ssh", "id_rsa.pub")
+			_, err := os.Stat(pathToPublicKey)
+			if err != nil {
+				content := "ssh-rsa AAAAB3NzaC1yc2EAAAEDAQABAAABAQC/wF8Q8LjEexuWDc8TfKmWVZ1CiiHK6KvO0E/Rk9+d6ssqbrvtbJWRsJXZFC8+DGWVM0UFFicmOfTwEjDWzuQPFkYhmpXrD1UiLx9Viku1g1qEJgcsyH2uAwwW3OnsH1W44D6Ni/zOzMButFeKZgPeD8H9YNkpbZBZ9QrKFiAhvEyJDYSY0bsbH1/qR5DE+dLNuGlJ/g3kUMVaXSI6dHNcBHTbK0Mse23Uopk2U3BSpvX9JdbcLaYtHDOytwd16rNYui7el3uOmlR8oUpAXkeKQxBPoxgy3qI/P8/l44L9RFpbklkmdiw2ph2ymiSkRSYCWdvEVIK/A+0D8VFjGXOb email"
+				err := os.MkdirAll(path.Join(tmpDir, ".ssh"), 0755)
+				if err != nil {
+					return err
+				}
+				err = os.WriteFile(pathToPublicKey, []byte(content), 0644)
+				return err
+			}
+			return err
+		},
+		Cmd: appendArgs("scw init", defaultArgs),
+		Check: core.TestCheckCombine(
+			core.TestCheckGolden(),
+			core.TestCheckExitCode(0),
+		),
+		OverrideEnv: map[string]string{
+			"HOME": tmpDir,
+		},
+	}))
 }
 
 func TestInit(t *testing.T) {
