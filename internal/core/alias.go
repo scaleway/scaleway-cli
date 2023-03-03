90160b1d5de0c48a400894d5d61df618bc8845fe
Jules Casteran <jcasteran@scaleway.com>
2023-03-03 18:13:26 +0100

    add init to alias disabled commands

--- internal/core/alias.go
+++ internal/core/alias.go
@@ -3,9 +3,7 @@ package core
 // aliasDisabled returns if alias are disabled for a command
 func aliasDisabled(cmd string) bool {
 	switch cmd {
-	case "alias":
-		return true
-	case "autocomplete":
+	case "alias", "autocomplete", "init":
 		return true
 	}
 	return false
