43d76c3e85023a7b265e5ae4fe81f1663d59ea79
Jules Casteran <jcasteran@scaleway.com>
2023-03-06 13:46:55 +0100

    add tests for alias delete and list

--- internal/namespaces/alias/alias_test.go
+++ internal/namespaces/alias/alias_test.go
@@ -1,10 +1,13 @@
 package alias_test
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/alecthomas/assert"
 	"github.com/scaleway/scaleway-cli/v2/internal/core"
+	"github.com/scaleway/scaleway-cli/v2/internal/human"
 	"github.com/scaleway/scaleway-cli/v2/internal/namespaces"
 )
 
@@ -39,6 +42,53 @@ func Test_Alias(t *testing.T) {
 			core.TestCheckGolden(),
 			func(t *testing.T, ctx *core.CheckFuncCtx) {
 				assert.Contains(t, string(ctx.Stderr), "instance server list")
+			},
+		),
+		TmpHomeDir: true,
+	}))
+
+	t.Run("list aliases", core.Test(&core.TestConfig{
+		BeforeFunc: core.BeforeFuncCombine(
+			core.ExecBeforeCmd("scw alias create myalias command=iam"),
+		),
+		Commands:      namespaces.GetCommands(),
+		Cmd:           "scw alias list",
+		EnableAliases: true,
+		Check: core.TestCheckCombine(
+			core.TestCheckGolden(),
+			func(t *testing.T, ctx *core.CheckFuncCtx) {
+				assert.Contains(t, string(ctx.Stdout), "myalias")
+				assert.Contains(t, string(ctx.Stdout), "iam")
+			},
+		),
+		TmpHomeDir: true,
+	}))
+
+	t.Run("delete alias", core.Test(&core.TestConfig{
+		BeforeFunc: core.BeforeFuncCombine(
+			core.ExecBeforeCmd("scw alias create i command=instance"),
+		),
+		Commands:      namespaces.GetCommands(),
+		Cmd:           "scw alias delete i",
+		EnableAliases: true,
+		AfterFunc: core.AfterFuncCombine(
+			func(ctx *core.AfterFuncCtx) error {
+				res := ctx.ExecuteCmd([]string{"scw", "alias", "list"})
+				resString, err := human.Marshal(res, nil)
+				if err != nil {
+					return err
+				}
+
+				if strings.Contains(resString, "instance") {
+					return fmt.Errorf("alias list should not contain instance")
+				}
+				return nil
+			},
+		),
+		Check: core.TestCheckCombine(
+			core.TestCheckGolden(),
+			func(t *testing.T, ctx *core.CheckFuncCtx) {
+				assert.Contains(t, string(ctx.Stdout), "Deleted")
 			},
 		),
 		TmpHomeDir: true,
