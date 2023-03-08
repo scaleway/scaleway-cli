772d5828216d416fd2b5955a333bcb20c29498bf
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2023-03-08 10:38:18 +0100

    fix(secret): update test mashal human

--- internal/human/marshal_test.go
+++ internal/human/marshal_test.go
@@ -245,7 +245,7 @@ func Test_getStructFieldsIndex(t *testing.T) {
 					Strings: []string{"aa", "ab"}},
 				),
 			},
-			want: [][]int{{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}},
+			want: [][]int{{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}},
 		},
 	}
 	for _, tt := range tests {
