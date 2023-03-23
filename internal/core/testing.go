977ed528d11235e743f761010420f274d8f5c603
Nathanael DEMACON <ndemacon@scaleway.com>
2023-03-23 15:12:09 +0100

    replace `//./pipe/docker_engine` by `/var/run/docker.sock`

--- internal/core/testing.go
+++ internal/core/testing.go
@@ -710,6 +710,7 @@ func getHTTPRecoder(t *testing.T, update bool) (client *http.Client, cleanup fun
 		delete(i.Request.Headers, "X-Auth-Token")
 		i.Request.URL = regexp.MustCompile("organization_id=[0-9a-f-]{36}").ReplaceAllString(i.Request.URL, "organization_id=11111111-1111-1111-1111-111111111111")
 		i.Request.URL = regexp.MustCompile(`api\.scaleway\.com/account/v1/tokens/[0-9a-f-]{36}`).ReplaceAllString(i.Request.URL, "api.scaleway.com/account/v1/tokens/11111111-1111-1111-1111-111111111111")
+		i.Request.URL = strings.ReplaceAll(i.Request.URL, "//./pipe/docker_engine", "/var/run/docker.sock")
 		return nil
 	})
 
