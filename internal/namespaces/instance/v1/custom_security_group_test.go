53d94756e9689cb8e41b2a288d20240609183c65
Jules Casteran <jcasteran@scaleway.com>
2023-03-24 15:23:57 +0100

    fix linter

--- internal/namespaces/instance/v1/custom_security_group_test.go
+++ internal/namespaces/instance/v1/custom_security_group_test.go
@@ -12,7 +12,7 @@ func Test_SecurityGroupGet(t *testing.T) {
 		BeforeFunc: core.BeforeFuncCombine(
 			createSecurityGroup("SecurityGroup"),
 		),
-		Cmd:       "scw instance security-group get {{ .SecurityGroup.ID }}",
+		Cmd: "scw instance security-group get {{ .SecurityGroup.ID }}",
 		Check: core.TestCheckCombine(
 			core.TestCheckGolden(),
 			core.TestCheckExitCode(0),
