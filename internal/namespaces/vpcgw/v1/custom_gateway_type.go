c0b14034070605e3da380e3a023a20a86f48ff3d
Yacine Fodil <105779815+yfodil@users.noreply.github.com>
2023-03-28 15:36:29 +0200

    feat(vpcgw): improve gateway-type list visualization (#2963)

---
+++ internal/namespaces/vpcgw/v1/custom_gateway_type.go
@@ -0,0 +1,22 @@
+package vpcgw
+
+import (
+	"context"
+
+	"github.com/scaleway/scaleway-cli/v2/internal/core"
+	"github.com/scaleway/scaleway-sdk-go/api/vpcgw/v1"
+)
+
+func vpcgwGatewayTypeListBuilder(c *core.Command) *core.Command {
+	c.AddInterceptors(func(ctx context.Context, argsI interface{}, runner core.CommandRunner) (interface{}, error) {
+		res, err := runner(ctx, argsI)
+		if err != nil {
+			return nil, err
+		}
+
+		typesResponse := res.(*vpcgw.ListGatewayTypesResponse)
+		return typesResponse.Types, nil
+	})
+
+	return c
+}
