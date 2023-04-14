5566bf3974133545afc3b4ccd342f65342aaaee3
Jules Casteran <jcasteran@scaleway.com>
2023-04-14 14:39:38 +0200

    fix(instance): delete with-volumes none worked like all

--- internal/namespaces/instance/v1/custom_server.go
+++ internal/namespaces/instance/v1/custom_server.go
@@ -1013,10 +1013,11 @@ func serverDeleteCommand() *core.Command {
 			}
 
 			deletedVolumeMessages := [][2]string(nil)
+		volumeDelete:
 			for index, volume := range server.Server.Volumes {
 				switch {
 				case deleteServerArgs.WithVolumes == withVolumesNone:
-					break
+					break volumeDelete
 				case deleteServerArgs.WithVolumes == withVolumesRoot && index != "0":
 					continue
 				case deleteServerArgs.WithVolumes == withVolumesLocal && volume.VolumeType != instance.VolumeServerVolumeTypeLSSD:
