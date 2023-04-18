2dfca5df47cf97e380173124555a36890996696f
Jules Castéran <jcasteran@scaleway.com>
2023-04-18 10:31:41 +0200

    feat(autocomplete): global flag profile values (#3036)

--- internal/core/autocomplete_utils.go
+++ internal/core/autocomplete_utils.go
@@ -14,6 +14,50 @@ import (
 
 var autoCompleteCache *cache.Cache
 
+// getGlobalFlags returns the list of flags that should be added to all commands
+func getGlobalFlags(ctx context.Context) []FlagSpec {
+	printerTypes := []string{
+		PrinterTypeHuman.String(),
+		PrinterTypeJSON.String(),
+		PrinterTypeYAML.String(),
+		PrinterTypeTemplate.String(),
+	}
+	profiles := []string(nil)
+	cfg := extractConfig(ctx)
+	if cfg != nil {
+		for profile := range cfg.Profiles {
+			profiles = append(profiles, profile)
+		}
+	}
+
+	return []FlagSpec{
+		{Name: "-c"},
+		{Name: "--config"},
+		{Name: "-D"},
+		{Name: "--debug"},
+		{Name: "-h"},
+		{Name: "--help"},
+		{
+			Name:       "-o",
+			EnumValues: printerTypes,
+		},
+		{
+			Name:       "--output",
+			EnumValues: printerTypes,
+		},
+		{
+			Name:             "-p",
+			HasVariableValue: true,
+			EnumValues:       profiles,
+		},
+		{
+			Name:             "--profile",
+			HasVariableValue: true,
+			EnumValues:       profiles,
+		},
+	}
+}
+
 func AutocompleteProfileName() AutoCompleteArgFunc {
 	return func(ctx context.Context, prefix string) AutocompleteSuggestions {
 		res := AutocompleteSuggestions(nil)
