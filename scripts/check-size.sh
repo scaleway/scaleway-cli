325288652225fb2fe9b1d290388a9c10a92ddfbf
Jules Castéran <jcasteran@scaleway.com>
2023-04-27 10:49:47 +0200

    ci: strip binaries and check size (#3092)

---
+++ scripts/check-size.sh
@@ -0,0 +1,28 @@
+#/bin/env bash
+
+MAX_BINARY_SIZE=25000000
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+function usage() {
+  echo "Usage:"
+  echo "  $SCRIPT_DIR [BINARY_FILE]"
+
+  echo ""
+  exit "$1";
+}
+
+if [[ $# -ne 1 ]]; then
+  usage
+fi
+
+BINARY_SIZE=$(stat -c %s $1)
+
+echo "Binary size: $BINARY_SIZE"
+
+if (( $BINARY_SIZE > $MAX_BINARY_SIZE )); then
+  echo "Size is greater than limit"
+  exit 1
+else
+  echo "Size is valid"
+fi
