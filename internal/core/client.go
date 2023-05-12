f3c5eb72df34f9cf80b802c22359a128360683a3
Jules Casteran <jcasteran@scaleway.com>
2023-05-12 17:22:51 +0200

    fix init_ssh tests

--- internal/core/client.go
+++ internal/core/client.go
@@ -28,7 +28,7 @@ func createClientError(err error) error {
 	credentialsHint := "You can get your credentials here: https://console.scaleway.com/iam/api-keys"
 
 	if clientError, isClientError := err.(*platform.ClientError); isClientError {
-		return &CliError{
+		err = &CliError{
 			Err:     clientError.Err,
 			Details: clientError.Details,
 			Hint:    credentialsHint,
