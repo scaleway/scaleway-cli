2359de415d178743c5a82ee59afd2a2ac2a1d058
Jules Casteran <jcasteran@scaleway.com>
2023-05-12 08:44:35 +0200

    fix linter

--- internal/platform/web.go
+++ internal/platform/web.go
@@ -11,7 +11,7 @@ type Web struct {
 	JWT string
 }
 
-func (p *Web) CreateClient(client *http.Client, configPath string, profileName string) (*scw.Client, error) {
+func (p *Web) CreateClient(client *http.Client, _ string, _ string) (*scw.Client, error) {
 	opts := []scw.ClientOption{
 		scw.WithDefaultRegion(scw.RegionFrPar),
 		scw.WithDefaultZone(scw.ZoneFrPar1),
@@ -28,8 +28,6 @@ func (p *Web) ScwConfig() *scw.Config {
 	return nil
 }
 
-func (p *Web) SetScwConfig(cfg *scw.Config) {
-	return
-}
+func (p *Web) SetScwConfig(_ *scw.Config) {}
 
 var _ Platform = (*Web)(nil)
