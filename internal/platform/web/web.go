a4d6ee587a969b3a29ec6ede96085f11f93cf486
Jules Casteran <jcasteran@scaleway.com>
2023-06-14 10:54:16 +0200

    feat(wasm): add defaultProjectID to run config

--- internal/platform/web/web.go
+++ internal/platform/web/web.go
@@ -7,20 +7,24 @@ import (
 )
 
 type Platform struct {
-	UserAgent string
-	JWT       string
+	UserAgent        string
+	JWT              string
+	DefaultProjectID string
 }
 
 func (p *Platform) CreateClient(client *http.Client, _ string, _ string) (*scw.Client, error) {
 	opts := []scw.ClientOption{
 		scw.WithDefaultRegion(scw.RegionFrPar),
 		scw.WithDefaultZone(scw.ZoneFrPar1),
-		scw.WithDefaultZone(scw.ZoneFrPar1),
 		scw.WithUserAgent(p.UserAgent),
 		scw.WithUserAgent("cli/web"),
 		scw.WithProfile(scw.LoadEnvProfile()),
 		scw.WithHTTPClient(client),
 		scw.WithJWT(p.JWT),
+	}
+
+	if p.DefaultProjectID != "" {
+		opts = append(opts, scw.WithDefaultProjectID(p.DefaultProjectID))
 	}
 
 	return scw.NewClient(opts...)
