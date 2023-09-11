4e395056cf0da40ee9cc80381f5f59005bcea854
Rémy Léone <rleone@scaleway.com>
2023-09-11 14:11:19 +0200

    chore: add support for prettier

--- wasm/src/jshelpers.test.ts
+++ wasm/src/jshelpers.test.ts
@@ -1,40 +1,39 @@
-
-import {describe, it, expect, afterAll, beforeAll} from 'vitest'
+import { describe, it, expect, afterAll, beforeAll } from 'vitest'
 
 import '../wasm_exec_node.cjs'
 import '../wasm_exec.cjs'
-import {RunConfig} from '../cli'
+import { RunConfig } from '../cli'
 import * as fs from 'fs'
-import {loadWasmBinary} from "./utils";
+import { loadWasmBinary } from './utils'
 
 type CLITester = {
-    stop: () => Promise<void>
-    FromSlice: () => string[]
+  stop: () => Promise<void>
+  FromSlice: () => string[]
 }
 
 describe('With test environment', async () => {
-    let cli: CLITester
+  let cli: CLITester
 
-    beforeAll(async () => {
-        // @ts-ignore
-        cli = await loadWasmBinary('./cliTester.wasm') as CLITester
-    })
+  beforeAll(async () => {
+    // @ts-ignore
+    cli = (await loadWasmBinary('./cliTester.wasm')) as CLITester
+  })
 
-    it('can return array', async () => {
-        const array = cli.FromSlice()
+  it('can return array', async () => {
+    const array = cli.FromSlice()
 
-        expect(array).toHaveLength(3)
-        expect(array).toContain("1")
-        expect(array).toContain("2")
-        expect(array).toContain("3")
-    })
+    expect(array).toHaveLength(3)
+    expect(array).toContain('1')
+    expect(array).toContain('2')
+    expect(array).toContain('3')
+  })
 
-    afterAll(async () => {
-        try {
-            await cli.stop()
-            go._resume()
-        } catch (e) {
-            console.log(e)
-        }
-    })
+  afterAll(async () => {
+    try {
+      await cli.stop()
+      go._resume()
+    } catch (e) {
+      console.log(e)
+    }
+  })
 })
