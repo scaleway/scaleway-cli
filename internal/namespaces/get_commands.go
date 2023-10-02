69ac09ee97dc247736862fd9cba16c37b8a541ce
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2023-10-02 09:57:36 +0200

    fix(documentdb): generate goldens

--- internal/namespaces/get_commands.go
+++ internal/namespaces/get_commands.go
@@ -40,13 +40,12 @@ import (
 	"github.com/scaleway/scaleway-cli/v2/internal/namespaces/vpc/v2"
 	"github.com/scaleway/scaleway-cli/v2/internal/namespaces/vpcgw/v1"
 	webhosting "github.com/scaleway/scaleway-cli/v2/internal/namespaces/webhosting/v1alpha1"
-	"github.com/scaleway/scaleway-sdk-go/scw"
 )
 
 var labs = os.Getenv("SCW_ENABLE_LABS") == "true"
 
 // Enable beta in the code when products are in beta
-var beta = os.Getenv(scw.ScwEnableBeta) == "true"
+//var beta = os.Getenv(scw.ScwEnableBeta) == "true"
 
 // GetCommands returns a list of all commands in the CLI.
 // It is used by both scw and scw-qa.
@@ -94,8 +93,8 @@ func GetCommands() *core.Commands {
 	if labs {
 		commands.Merge(ipfs.GetCommands())
 	}
-	if beta {
-	}
+	//if beta {
+	//}
 
 	return commands
 }
