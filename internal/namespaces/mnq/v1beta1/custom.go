51f80073a391ec98518af4223961252206af9b58
Jules Casteran <jcasteran@scaleway.com>
2023-10-13 16:22:56 +0200

    feat(mnq): enable v1beta1 commands

---
+++ internal/namespaces/mnq/v1beta1/custom.go
@@ -0,0 +1,14 @@
+package mnq
+
+import (
+	"github.com/scaleway/scaleway-cli/v2/internal/core"
+	"github.com/scaleway/scaleway-cli/v2/internal/human"
+	mnq "github.com/scaleway/scaleway-sdk-go/api/mnq/v1beta1"
+)
+
+func GetCommands() *core.Commands {
+
+	human.RegisterMarshalerFunc(mnq.SnsInfoStatus(""), human.EnumMarshalFunc(mnqSqsInfoStatusMarshalSpecs))
+
+	return nil
+}
