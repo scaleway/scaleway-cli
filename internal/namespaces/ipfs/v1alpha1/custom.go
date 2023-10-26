d0baaa9496d832dcdc8c58f87d1bbc7d1bb1263e
albert VALENTIN <avalentin@scaleway.com>
2023-10-26 16:19:53 +0200

    feat(ipfs): update custom.go to add groups labs in generated commands

--- internal/namespaces/ipfs/v1alpha1/custom.go
+++ internal/namespaces/ipfs/v1alpha1/custom.go
@@ -4,31 +4,11 @@ import (
 	"github.com/scaleway/scaleway-cli/v2/internal/core"
 )
 
-func newIpfsRoot() *core.Command {
-	return &core.Command{
-		Short:     `IPFS Pinning service API`,
-		Long:      `IPFS Pinning service API.`,
-		Namespace: "ipfs",
-		Groups:    []string{"labs"},
-	}
-}
-
-func newIpnsRoot() *core.Command {
-	return &core.Command{
-		Short:     `IPFS Naming service API`,
-		Long:      ``,
-		Namespace: "ipns",
-		Groups:    []string{"labs"},
-	}
-}
-
 // GetCommands returns the list of commands for the 'ipfs' namespace.
 func GetCommands() *core.Commands {
 	cmds := GetGeneratedCommands()
 
-	commands := cmds.GetAll()
-	commands = append(commands[:0], commands[2:]...)
-	commands = append([]*core.Command{newIpfsRoot(), newIpnsRoot()}, commands...)
-
-	return core.NewCommands(commands...)
+	cmds.MustFind("ipns").Groups = []string{"labs"}
+	cmds.MustFind("ipfs").Groups = []string{"labs"}
+	return cmds
 }
