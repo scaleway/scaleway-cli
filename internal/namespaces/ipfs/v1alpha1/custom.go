ee2dfd0665dfd2b9112d6491e10d85268e62aa7e
Albert Valentin <72015680+OnsagerHe@users.noreply.github.com>
2023-10-26 16:29:00 +0200

    feat(ipfs): add command ipns in labs commands (#3459)
    Co-authored-by: albert VALENTIN <avalentin@scaleway.com>

--- internal/namespaces/ipfs/v1alpha1/custom.go
+++ internal/namespaces/ipfs/v1alpha1/custom.go
@@ -4,22 +4,11 @@ import (
 	"github.com/scaleway/scaleway-cli/v2/internal/core"
 )
 
-func newIpfsRoot() *core.Command {
-	return &core.Command{
-		Short:     `IPFS Pinning service API`,
-		Long:      `IPFS Pinning service API.`,
-		Namespace: "ipfs",
-		Groups:    []string{"labs"},
-	}
-}
-
 // GetCommands returns the list of commands for the 'ipfs' namespace.
 func GetCommands() *core.Commands {
 	cmds := GetGeneratedCommands()
 
-	commands := cmds.GetAll()
-	commands = append(commands[:0], commands[1:]...)
-	commands = append([]*core.Command{newIpfsRoot()}, commands...)
-
-	return core.NewCommands(commands...)
+	cmds.MustFind("ipns").Groups = []string{"labs"}
+	cmds.MustFind("ipfs").Groups = []string{"labs"}
+	return cmds
 }
