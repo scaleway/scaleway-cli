0ba8bec663ca19f99bbe3b5a26ea9845a928d98a
lmasson <lmasson@scaleway.com>
2024-02-07 17:34:30 +0100

    fix(vpcgw):improve vizualization gateway and IP

--- internal/namespaces/vpcgw/v1/custom.go
+++ internal/namespaces/vpcgw/v1/custom.go
@@ -11,6 +11,7 @@ func GetCommands() *core.Commands {
 
 	human.RegisterMarshalerFunc(vpcgw.GatewayNetworkStatus(""), human.EnumMarshalFunc(gatewayNetworkStatusMarshalSpecs))
 	human.RegisterMarshalerFunc(vpcgw.GatewayStatus(""), human.EnumMarshalFunc(gatewayStatusMarshalSpecs))
+	human.RegisterMarshalerFunc(vpcgw.Gateway{}, gatewayNetworkMarshallerFunc)
 
 	cmds.MustFind("vpc-gw", "gateway-type", "list").Override(vpcgwGatewayTypeListBuilder)
 	cmds.MustFind("vpc-gw", "gateway", "create").Override(gatewayCreateBuilder)
