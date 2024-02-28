9d8b26ec620bebaea896f6dfbe4789061166c91e
lmasson <lmasson@scaleway.com>
2024-02-28 12:36:38 +0100

    fix(redis): cluster list crash

--- internal/namespaces/redis/v1/custom.go
+++ internal/namespaces/redis/v1/custom.go
@@ -10,7 +10,7 @@ func GetCommands() *core.Commands {
 	cmds := GetGeneratedCommands()
 
 	human.RegisterMarshalerFunc(redis.Cluster{}, redisClusterGetMarshalerFunc)
-	human.RegisterMarshalerFunc(redis.Cluster{}.Endpoints, redisEndpointsClusterGetMarshalerFunc)
+	human.RegisterMarshalerFunc(redis.Endpoint{}, redisEndpointsClusterGetMarshalerFunc)
 
 	cmds.Merge(core.NewCommands(clusterWaitCommand()))
 	cmds.MustFind("redis", "cluster", "create").Override(clusterCreateBuilder)
