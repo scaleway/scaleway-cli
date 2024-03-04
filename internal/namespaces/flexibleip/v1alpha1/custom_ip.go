8614621450f9dd76564983c492be0010888b7f01
lmasson <lmasson@scaleway.com>
2024-03-04 12:00:54 +0100

    feat(fip): add waiter create

--- internal/namespaces/flexibleip/v1alpha1/custom_ip.go
+++ internal/namespaces/flexibleip/v1alpha1/custom_ip.go
@@ -1,9 +1,14 @@
 package flexibleip
 
 import (
+	"context"
+	"time"
+
 	"github.com/fatih/color"
+	"github.com/scaleway/scaleway-cli/v2/internal/core"
 	"github.com/scaleway/scaleway-cli/v2/internal/human"
 	flexibleip "github.com/scaleway/scaleway-sdk-go/api/flexibleip/v1alpha1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
 )
 
 var (
@@ -17,3 +22,21 @@ var (
 		flexibleip.FlexibleIPStatusUpdating:  &human.EnumMarshalSpec{Attribute: color.FgBlue},
 	}
 )
+
+const (
+	flexibleIpTimeout = 60 * time.Minute
+)
+
+func createIPBuilder(c *core.Command) *core.Command {
+	c.WaitFunc = func(ctx context.Context, argsI, respI interface{}) (interface{}, error) {
+		getResp := respI.(*flexibleip.FlexibleIP)
+		api := flexibleip.NewAPI(core.ExtractClient(ctx))
+		return api.WaitForFlexibleIP(&flexibleip.WaitForFlexibleIPRequest{
+			FipID:         getResp.ID,
+			Zone:          getResp.Zone,
+			Timeout:       scw.TimeDurationPtr(flexibleIpTimeout),
+			RetryInterval: core.DefaultRetryInterval,
+		})
+	}
+	return c
+}
