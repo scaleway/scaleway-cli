191738cb7ee41d65418a5f044b0bcbf282f650c9
Jules Casteran <jcasteran@scaleway.com>
2024-04-09 13:31:29 +0200

    feat(iam): show rules with policy

--- internal/namespaces/iam/v1alpha1/custom.go
+++ internal/namespaces/iam/v1alpha1/custom.go
@@ -47,25 +47,8 @@ func GetCommands() *core.Commands {
 	}
 
 	// Autocomplete permission set names using IAM API.
-	cmds.MustFind("iam", "policy", "create").Override(func(c *core.Command) *core.Command {
-		c.ArgSpecs.GetByName("rules.{index}.permission-set-names.{index}").AutoCompleteFunc = func(ctx context.Context, _ string, _ any) core.AutocompleteSuggestions {
-			client := core.ExtractClient(ctx)
-			api := iam.NewAPI(client)
-			// TODO: store result in a CLI cache
-			resp, err := api.ListPermissionSets(&iam.ListPermissionSetsRequest{
-				PageSize: scw.Uint32Ptr(100),
-			}, scw.WithAllPages())
-			if err != nil {
-				return nil
-			}
-			suggestions := core.AutocompleteSuggestions{}
-			for _, ps := range resp.PermissionSets {
-				suggestions = append(suggestions, ps.Name)
-			}
-			return suggestions
-		}
-		return c
-	})
+	cmds.MustFind("iam", "policy", "create").Override(iamPolicyCreateBuilder)
+	cmds.MustFind("iam", "policy", "get").Override(iamPolicyGetBuilder)
 
 	return cmds
 }
---
+++ internal/namespaces/iam/v1alpha1/custom_policy.go
@@ -0,0 +1,70 @@
+package iam
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/scaleway/scaleway-cli/v2/internal/core"
+	iam "github.com/scaleway/scaleway-sdk-go/api/iam/v1alpha1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+)
+
+func iamPolicyCreateBuilder(c *core.Command) *core.Command {
+	c.ArgSpecs.GetByName("rules.{index}.permission-set-names.{index}").AutoCompleteFunc = func(ctx context.Context, _ string, _ any) core.AutocompleteSuggestions {
+		client := core.ExtractClient(ctx)
+		api := iam.NewAPI(client)
+		// TODO: store result in a CLI cache
+		resp, err := api.ListPermissionSets(&iam.ListPermissionSetsRequest{
+			PageSize: scw.Uint32Ptr(100),
+		}, scw.WithAllPages())
+		if err != nil {
+			return nil
+		}
+		suggestions := core.AutocompleteSuggestions{}
+		for _, ps := range resp.PermissionSets {
+			suggestions = append(suggestions, ps.Name)
+		}
+		return suggestions
+	}
+	return c
+}
+
+func iamPolicyGetBuilder(c *core.Command) *core.Command {
+	type customIamPolicyGetResponse struct {
+		*iam.Policy
+		Rules []*iam.Rule
+	}
+	c.View = &core.View{
+		Title: "Policy",
+		Sections: []*core.ViewSection{
+			{
+				Title:     "Rules",
+				FieldName: "Rules",
+			},
+		},
+	}
+	c.AddInterceptors(func(ctx context.Context, argsI interface{}, runner core.CommandRunner) (interface{}, error) {
+		args := argsI.(*iam.GetPolicyRequest)
+		api := iam.NewAPI(core.ExtractClient(ctx))
+
+		respI, err := runner(ctx, argsI)
+		if err != nil {
+			return respI, err
+		}
+		resp := &customIamPolicyGetResponse{
+			Policy: respI.(*iam.Policy),
+		}
+
+		rules, err := api.ListRules(&iam.ListRulesRequest{
+			PolicyID: args.PolicyID,
+		}, scw.WithContext(ctx), scw.WithAllPages())
+		if err != nil {
+			return nil, fmt.Errorf("failed to list rules for given policy: %w", err)
+		}
+		resp.Rules = rules.Rules
+
+		return resp, nil
+	})
+
+	return c
+}
