e2e9309d9671fbf96583933a4c0786424c067920
Jules Castéran <jcasteran@scaleway.com>
2024-04-10 11:22:46 +0200

    feat(iam): improve rules handling (#3753)

---
+++ internal/namespaces/iam/v1alpha1/custom_policy_test.go
@@ -0,0 +1,35 @@
+package iam_test
+
+import (
+	"testing"
+
+	"github.com/alecthomas/assert"
+	"github.com/scaleway/scaleway-cli/v2/internal/core"
+	"github.com/scaleway/scaleway-cli/v2/internal/namespaces/account/v3"
+	iam "github.com/scaleway/scaleway-cli/v2/internal/namespaces/iam/v1alpha1"
+)
+
+func Test_getPolicyWithRules(t *testing.T) {
+	t.Run("simple", core.Test(&core.TestConfig{
+		Commands: core.NewCommandsMerge(
+			iam.GetCommands(),
+			account.GetCommands(),
+		),
+		BeforeFunc: core.BeforeFuncCombine(
+			core.ExecStoreBeforeCmd("Project", "scw account project create name=test-cli-get-policy"),
+			core.ExecStoreBeforeCmd("Policy", "scw iam policy create name=test-cli-get-policy no-principal=true rules.0.permission-set-names.0=IPAMReadOnly rules.0.project-ids.0={{ .Project.ID }}"),
+		),
+		Cmd: `scw iam policy get {{ .Policy.ID }}`,
+		Check: core.TestCheckCombine(
+			func(t *testing.T, ctx *core.CheckFuncCtx) {
+				assert.Contains(t, string(ctx.Stdout), "IPAMReadOnly")
+			},
+			core.TestCheckGolden(),
+			core.TestCheckExitCode(0),
+		),
+		AfterFunc: core.AfterFuncCombine(
+			core.ExecAfterCmd("scw iam policy delete {{ .Policy.ID }}"),
+			core.ExecAfterCmd("scw account project delete project-id={{ .Project.ID }}"),
+		),
+	}))
+}
