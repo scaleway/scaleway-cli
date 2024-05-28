d57dac290c42a3935d0600270d78923a97808c65
lmasson <lmasson@scaleway.com>
2024-05-28 10:33:42 +0200

    fix(inference): update namespace

--- internal/namespaces/get_commands.go
+++ internal/namespaces/get_commands.go
@@ -19,6 +19,7 @@ import (
 	function "github.com/scaleway/scaleway-cli/v2/internal/namespaces/function/v1beta1"
 	"github.com/scaleway/scaleway-cli/v2/internal/namespaces/help"
 	iam "github.com/scaleway/scaleway-cli/v2/internal/namespaces/iam/v1alpha1"
+	inference "github.com/scaleway/scaleway-cli/v2/internal/namespaces/inference/v1beta1"
 	"github.com/scaleway/scaleway-cli/v2/internal/namespaces/info"
 	initNamespace "github.com/scaleway/scaleway-cli/v2/internal/namespaces/init"
 	"github.com/scaleway/scaleway-cli/v2/internal/namespaces/instance/v1"
@@ -96,6 +97,7 @@ func GetCommands() *core.Commands {
 		jobs.GetCommands(),
 		serverless_sqldb.GetCommands(),
 		llm_inference.GetCommands(),
+		inference.GetCommands(),
 	)
 
 	//if beta {}
---
+++ internal/namespaces/inference/v1beta1/custom_endpoint.go
@@ -0,0 +1,47 @@
+package inference
+
+import (
+	"context"
+	"reflect"
+
+	"github.com/scaleway/scaleway-cli/v2/internal/core"
+	inference "github.com/scaleway/scaleway-sdk-go/api/inference/v1beta1"
+)
+
+func endpointCreateBuilder(c *core.Command) *core.Command {
+	type llmInferenceEndpointSpecCustom struct {
+		*inference.EndpointSpec
+		IsPublic bool `json:"is-public"`
+	}
+
+	type createEndpointRequestCustom struct {
+		*inference.CreateEndpointRequest
+		Endpoint *llmInferenceEndpointSpecCustom `json:"endpoint"`
+	}
+
+	c.ArgSpecs.AddBefore("endpoint.private-network.private-network-id", &core.ArgSpec{
+		Name:     "endpoint.is-public",
+		Short:    "Will configure your public endpoint if true",
+		Required: false,
+		Default:  core.DefaultValueSetter("false"),
+	})
+
+	c.ArgsType = reflect.TypeOf(createEndpointRequestCustom{})
+
+	c.Interceptor = func(ctx context.Context, argsI interface{}, runner core.CommandRunner) (interface{}, error) {
+		createEndpointCustomRequest := argsI.(*createEndpointRequestCustom)
+		createEndpointreq := createEndpointCustomRequest.CreateEndpointRequest
+		endpoint := createEndpointCustomRequest.Endpoint
+		if endpoint.IsPublic {
+			publicEndpoint := &inference.EndpointSpecPublic{}
+			endpointToCreate := inference.EndpointSpec{
+				Public:         publicEndpoint,
+				PrivateNetwork: nil,
+				DisableAuth:    endpoint.DisableAuth,
+			}
+			createEndpointreq.Endpoint = &endpointToCreate
+		}
+		return runner(ctx, createEndpointreq)
+	}
+	return c
+}
