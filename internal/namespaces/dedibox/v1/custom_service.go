3202cce11d8b4a56f56900c974e46a3198d63663
lmasson <lmasson@scaleway.com>
2024-08-01 11:12:29 +0200

    feat(dedibox): waiter service support

--- internal/namespaces/dedibox/v1/custom_service.go
+++ internal/namespaces/dedibox/v1/custom_service.go
@@ -2,13 +2,49 @@ package dedibox
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"github.com/scaleway/scaleway-cli/v2/internal/core"
 	"github.com/scaleway/scaleway-sdk-go/api/dedibox/v1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+	"net/http"
+	"time"
+)
+
+const (
+	serviceActionTimeout = time.Minute * 60
+)
+
+const (
+	serviceActionCreate = iota
+	serviceActionDelete
 )
 
 func serviceCreateBuilder(c *core.Command) *core.Command {
-	c.WaitFunc = func(ctx context.Context, argsI, respI interface{}) (interface{}, error) {
-		api := dedibox.NewAPI(core.ExtractClient(ctx))
+	c.WaitFunc = waitForServiceFunc(serviceActionCreate)
+	return c
+}
 
+func waitForServiceFunc(action int) core.WaitFunc {
+	return func(ctx context.Context, argsI, respI interface{}) (interface{}, error) {
+		service, err := dedibox.NewAPI(core.ExtractClient(ctx)).WaitForService(&dedibox.WaitForServiceRequest{
+			ServiceID:     respI.(*dedibox.Service).ID,
+			Zone:          argsI.(*dedibox.CreateServerRequest).Zone,
+			Timeout:       scw.TimeDurationPtr(serviceActionTimeout),
+			RetryInterval: core.DefaultRetryInterval,
+		})
+		switch action {
+		case serviceActionCreate:
+			return service, err
+		case serviceActionDelete:
+			if err != nil {
+				notFoundError := &scw.ResourceNotFoundError{}
+				responseError := &scw.ResponseError{}
+				if errors.As(err, &responseError) && responseError.StatusCode == http.StatusNotFound || errors.As(err, &notFoundError) {
+					return fmt.Sprintf("Server %s successfully deleted.", respI.(*dedibox.Service).ID), nil
+				}
+			}
+		}
+		return nil, err
 	}
 }
