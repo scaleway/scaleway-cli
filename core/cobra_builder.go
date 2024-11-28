138fa1cc111874774a7dc8bcc09bf342bf1483a8
Rémy Léone <rleone@scaleway.com>
2024-11-28 15:26:35 +0100

    Fix

--- core/cobra_builder.go
+++ core/cobra_builder.go
@@ -28,9 +28,8 @@ type cobraBuilder struct {
 func (b *cobraBuilder) build() *cobra.Command {
 	groups := map[string]*cobra.Group{
 		"available": {ID: "available", Title: "AVAILABLE"},
-		//"labs":      {ID: "labs", Title: "AVAILABLE LABS"},
-		"config":  {ID: "config", Title: "CONFIGURATION"},
-		"utility": {ID: "utility", Title: "UTILITY"},
+		"config":    {ID: "config", Title: "CONFIGURATION"},
+		"utility":   {ID: "utility", Title: "UTILITY"},
 	}
 
 	commands := b.commands.GetAll()
