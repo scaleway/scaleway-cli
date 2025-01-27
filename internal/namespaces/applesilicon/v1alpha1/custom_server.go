65f96e5960e1b0a3a123fdb0320d2c5a10a63391
Laure-di <masson.laure.l@gmail.com>
2025-01-27 09:54:51 +0100

    add check is contract sign

--- internal/namespaces/applesilicon/v1alpha1/custom_server.go
+++ internal/namespaces/applesilicon/v1alpha1/custom_server.go
@@ -40,16 +40,22 @@ func serverCreateBuilder(c *core.Command) *core.Command {
 
 	c.AddInterceptors(func(ctx context.Context, argsI interface{}, runner core.CommandRunner) (i interface{}, err error) {
 		args := argsI.(*applesilicon.CreateServerRequest)
-		req := &account.ProjectAPIGetProjectRequest{ProjectID: args.ProjectID}
-		accountAPI := account.NewProjectAPI(core.ExtractClient(ctx))
-		project, err := accountAPI.GetProject(req)
-		if err != nil {
-			return nil, err
+		client := core.ExtractClient(ctx)
+		contractAPI := account.NewContractAPI(client)
+		orgaID, _ := client.GetDefaultProjectID()
+		req := &account.ContractAPICheckContractSignatureRequest{
+			OrganizationID: orgaID,
+			ContractType:   "BAREMETAL",
+			ContractName:   "Specific Conditions Services Baremetal",
 		}
+		contractInfo, err := contractAPI.CheckContractSignature(req)
 		bareMetalLink := "\u001B[38;2;121;45;212;4m\u001B]8;;https://console.scaleway.com/organization/contracts\u001B\\Bare Metal Specific Conditions\u001B]8;;\u001B\\\u001B[0m (\u001B[38;2;121;45;212mhttps://console.scaleway.com/organization/contracts\u001B[0m)"
 		appleLink := "\u001B[4m\u001B[38;2;121;45;212m\u001B]8;;https://www.apple.com/legal/sla/\u001B\\macOS License Agreement\u001B]8;;\u001B\\\u001B[0m (https://www.apple.com/legal/sla/)"
-
-		fmt.Println("Please note: Signing the " + bareMetalLink + " and the " + appleLink + " is mandatory.")
+		if contractInfo.Validated {
+			fmt.Println("Please note: Signing the " + appleLink + " is mandatory.")
+		} else {
+			fmt.Println("Please note: Signing the " + bareMetalLink + " and the " + appleLink + " is mandatory.")
+		}
 		return runner(ctx, args)
 	},
 	)
