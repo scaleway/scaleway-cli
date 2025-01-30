23fbbe7cbbbad9852ffaba5c668da005ff7c2114
Jules Casteran <jcasteran@scaleway.com>
2025-01-30 16:49:31 +0100

    update lb tests

--- internal/namespaces/lb/v1/helper_test.go
+++ internal/namespaces/lb/v1/helper_test.go
@@ -38,14 +38,14 @@ func createInstance() core.BeforeFunc {
 func createRunningInstance() core.BeforeFunc {
 	return core.ExecStoreBeforeCmd(
 		"Instance",
-		"scw instance server create type=DEV1-S image=ubuntu_bionic -w",
+		"scw instance server create type=DEV1-S image=ubuntu_jammy -w",
 	)
 }
 
 func createRunningInstanceWithTag() core.BeforeFunc {
 	return core.ExecStoreBeforeCmd(
 		"Instance",
-		"scw instance server create type=DEV1-S image=ubuntu_bionic tags.0=foo -w",
+		"scw instance server create type=DEV1-S image=ubuntu_jammy tags.0=foo -w",
 	)
 }
 
