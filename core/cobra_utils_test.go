3fd25eb977baeaa428ead1d3da765d224e125fcb
Olivier Cano <kindermoumoute@users.noreply.github.com>
2025-03-31 09:54:57 +0200

    feat(core): support positional args as dynamic args as well (#4621)
    Signed-off-by: Olivier Cano <ocano@scaleway.com>

--- core/cobra_utils_test.go
+++ core/cobra_utils_test.go
@@ -17,6 +17,11 @@ type testType struct {
 	Tag    string
 }
 
+type testTypeManyTags struct {
+	NameID string
+	Tags   []string
+}
+
 type testDate struct {
 	Date *time.Time
 }
@@ -74,6 +79,45 @@ func testGetCommands() *core.Commands {
 			AcceptMultiplePositionalArgs: true,
 			AllowAnonymousClient:         true,
 			ArgsType:                     reflect.TypeOf(testAcceptMultiPositionalArgsType{}),
+			Run: func(_ context.Context, argsI interface{}) (i interface{}, e error) {
+				return argsI, nil
+			},
+		},
+		&core.Command{
+			Namespace: "test",
+			Resource:  "many-positional",
+			ArgSpecs: core.ArgSpecs{
+				{
+					Name:       "name-id",
+					Positional: true,
+				},
+				{
+					Name:       "tag",
+					Positional: true,
+				},
+			},
+			AllowAnonymousClient: true,
+			ArgsType:             reflect.TypeOf(testType{}),
+			Run: func(_ context.Context, argsI interface{}) (i interface{}, e error) {
+				return argsI, nil
+			},
+		},
+		&core.Command{
+			Namespace: "test",
+			Resource:  "many-multi-positional",
+			ArgSpecs: core.ArgSpecs{
+				{
+					Name:       "name-id",
+					Positional: true,
+				},
+				{
+					Name:       "tags",
+					Positional: true,
+				},
+			},
+			AcceptMultiplePositionalArgs: true,
+			AllowAnonymousClient:         true,
+			ArgsType:                     reflect.TypeOf(testTypeManyTags{}),
 			Run: func(_ context.Context, argsI interface{}) (i interface{}, e error) {
 				return argsI, nil
 			},
@@ -256,6 +300,41 @@ func Test_PositionalArg(t *testing.T) {
 		Check: core.TestCheckCombine(
 			core.TestCheckExitCode(0),
 			core.TestCheckGolden(),
+		),
+	}))
+
+	t.Run("many positional", core.Test(&core.TestConfig{
+		Commands: testGetCommands(),
+		Cmd:      "scw test many-positional tag1 name-id=plop",
+		Check:    core.TestCheckExitCode(0),
+	}))
+
+	t.Run("many positional", core.Test(&core.TestConfig{
+		Commands: testGetCommands(),
+		Cmd:      "scw test many-positional tag1 name-id=plop",
+		Check: core.TestCheckCombine(
+			core.TestCheckExitCode(0),
+			func(t *testing.T, ctx *core.CheckFuncCtx) {
+				t.Helper()
+				res := ctx.Result.(*testType)
+				assert.Equal(t, "plop", res.NameID)
+				assert.Equal(t, "tag1", res.Tag)
+			},
+		),
+	}))
+
+	t.Run("many multi-positional", core.Test(&core.TestConfig{
+		Commands: testGetCommands(),
+		Cmd:      "scw test many-multi-positional pos1 pos2 name-id=plop",
+		Check: core.TestCheckCombine(
+			core.TestCheckExitCode(0),
+			func(t *testing.T, ctx *core.CheckFuncCtx) {
+				t.Helper()
+				res := ctx.Result.(*testTypeManyTags)
+				assert.Equal(t, "plop", res.NameID)
+				assert.Equal(t, "pos1", res.Tags[0])
+				assert.Equal(t, "pos2", res.Tags[1])
+			},
 		),
 	}))
 }
