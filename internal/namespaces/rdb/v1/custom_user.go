55f1ccd78177133270061534394191f84f6687fd
devtools-ci-cd <jremy@scaleway.com>
2025-05-15 16:16:03 +0200

    fix lint

--- internal/namespaces/rdb/v1/custom_user.go
+++ internal/namespaces/rdb/v1/custom_user.go
@@ -184,7 +184,8 @@ func userUpdateBuilder(c *core.Command) *core.Command {
 					return nil, err
 				}
 				*updateUserRequest.Password = pwd
-				_, err = interactive.Println("Your generated password is", *updateUserRequest.Password)
+				
+				_, err = interactive.Println("Your generated password is", pwd)
 				if err != nil {
 					return nil, err
 				}
