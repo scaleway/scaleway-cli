45fbdcf395cd22adb0e05a621cef95c4c4247631
Jonathan R. <jremy@scaleway.com>
2025-05-16 15:26:29 +0200

    fix(rdb): allow toggling is-admin without password when generate-pass… (#4719)
    Co-authored-by: Rémy Léone <rleone@scaleway.com>

--- internal/namespaces/rdb/v1/custom_user.go
+++ internal/namespaces/rdb/v1/custom_user.go
@@ -7,6 +7,7 @@ import (
 	"reflect"
 
 	"github.com/scaleway/scaleway-cli/v2/core"
+	"github.com/scaleway/scaleway-cli/v2/internal/interactive"
 	"github.com/scaleway/scaleway-cli/v2/internal/passwordgenerator"
 	"github.com/scaleway/scaleway-sdk-go/api/rdb/v1"
 	"github.com/scaleway/scaleway-sdk-go/scw"
@@ -170,24 +171,33 @@ func userUpdateBuilder(c *core.Command) *core.Command {
 		api := rdb.NewAPI(client)
 
 		customRequest := argsI.(*rdbUpdateUserRequestCustom)
-
 		updateUserRequest := customRequest.UpdateUserRequest
 
 		var err error
-		if customRequest.GeneratePassword && customRequest.Password == nil {
-			updateUserRequest.Password = new(string)
-			*updateUserRequest.Password, err = passwordgenerator.GeneratePassword(21, 1, 1, 1, 1)
-			if err != nil {
-				return nil, err
-			}
-			fmt.Printf("Your generated password is %v \n", *updateUserRequest.Password)
-			fmt.Printf("\n")
-		}
 
-		if !customRequest.GeneratePassword && customRequest.Password == nil {
-			return nil, errors.New(
-				"you must provide a password when generate-password is set to false",
-			)
+		if customRequest.GeneratePassword || customRequest.Password != nil {
+			switch {
+			case customRequest.GeneratePassword && customRequest.Password == nil:
+				updateUserRequest.Password = new(string)
+				pwd, err := passwordgenerator.GeneratePassword(21, 1, 1, 1, 1)
+				if err != nil {
+					return nil, err
+				}
+				*updateUserRequest.Password = pwd
+
+				_, err = interactive.Println("Your generated password is", pwd)
+				if err != nil {
+					return nil, err
+				}
+
+			case !customRequest.GeneratePassword && customRequest.Password == nil:
+				return nil, errors.New(
+					"you must provide a password when generate-password is set to false",
+				)
+
+			default:
+				updateUserRequest.Password = customRequest.Password
+			}
 		}
 
 		user, err := api.UpdateUser(updateUserRequest)
@@ -195,9 +205,14 @@ func userUpdateBuilder(c *core.Command) *core.Command {
 			return nil, err
 		}
 
+		respPwd := ""
+		if updateUserRequest.Password != nil {
+			respPwd = *updateUserRequest.Password
+		}
+
 		result := rdbUpdateUserResponseCustom{
 			User:     user,
-			Password: *updateUserRequest.Password,
+			Password: respPwd,
 		}
 
 		return result, nil
