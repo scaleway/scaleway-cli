66b1638fb41d6bac08a4c77281851a06cca26b8e
Yacine FODIL <yfodil@scaleway.com>
2025-05-21 09:56:36 +0200

    fix

--- internal/namespaces/vpc/v2/custom_private_network_test.go
+++ internal/namespaces/vpc/v2/custom_private_network_test.go
@@ -20,7 +20,7 @@ func Test_GetPrivateNetwork(t *testing.T) {
 	cmds.Merge(redis.GetCommands())
 	cmds.Merge(mongodb.GetCommands())
 
-	/*	t.Run("Simple", core.Test(&core.TestConfig{
+	t.Run("Simple", core.Test(&core.TestConfig{
 		Commands: cmds,
 		BeforeFunc: core.BeforeFuncCombine(
 			createPN(),
@@ -33,7 +33,7 @@ func Test_GetPrivateNetwork(t *testing.T) {
 			deleteInstance(),
 			deletePN(),
 		),
-	}))*/
+	}))
 
 	t.Run("Multiple", core.Test(&core.TestConfig{
 		Commands: cmds,
