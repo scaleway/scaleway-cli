bc680bbd007c67caf48b8a06fd6c310db95239e7
Laure-di <lmasson@scaleway.com>
2025-05-26 12:42:48 +0200

    add Command Groups

--- internal/namespaces/baremetal/v3/custom.go
+++ internal/namespaces/baremetal/v3/custom.go
@@ -4,6 +4,7 @@ import "github.com/scaleway/scaleway-cli/v2/core"
 
 func GetCommands() *core.Commands {
 	cmds := GetGeneratedCommands()
+	cmds.MustFind("baremetal").Groups = []string{"baremetal"}
 
 	return cmds
 }
