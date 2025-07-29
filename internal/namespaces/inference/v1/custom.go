b7eeb182cf56fb374683e0e31e029d4a540e9a87
Rémy Léone <rleone@scaleway.com>
2025-07-29 11:14:49 +0200

    chore: add support for grouping inference in ai commands

--- internal/namespaces/inference/v1/custom.go
+++ internal/namespaces/inference/v1/custom.go
@@ -5,5 +5,7 @@ import "github.com/scaleway/scaleway-cli/v2/core"
 func GetCommands() *core.Commands {
 	cmds := GetGeneratedCommands()
 
+	cmds.MustFind("inference").Groups = []string{"ai"}
+
 	return cmds
 }
