a3c36ceba5133707a9562b8015b7e97ab327252b
Rémy Léone <rleone@scaleway.com>
2025-10-22 16:54:50 +0200

    Fix

--- wasm/src/vite.config.js
+++ wasm/src/vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import { codecovVitePlugin } from "@codecov/vite-plugin";
+import { defineConfig } from 'vite'
+import { codecovVitePlugin } from '@codecov/vite-plugin'
 
 export default defineConfig({
-    plugins: [
-        // Put the Codecov vite plugin after all other plugins
-        codecovVitePlugin({
-            enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-            bundleName: "<bundle project name>",
-            uploadToken: process.env.CODECOV_TOKEN,
-        }),
-    ],
-});
+  plugins: [
+    // Put the Codecov vite plugin after all other plugins
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: '<bundle project name>',
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
+})
