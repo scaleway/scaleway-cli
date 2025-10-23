29c666b79a6485c3dc3da27e6da3959005428641
Leïla Marabese <lmarabese@scaleway.com>
2025-10-23 10:18:00 +0200

    wip: store image in meta in different function to be executed on replay and record (not working)

--- internal/testhelpers/helpers_registry.go
+++ internal/testhelpers/helpers_registry.go
@@ -6,26 +6,25 @@ import (
 	"github.com/scaleway/scaleway-cli/v2/core"
 )
 
-func PushRegistryImage(dockerImage, namespaceMetaKey, imageName, metaKey string) core.BeforeFunc {
+func PushRegistryImage(dockerImage, namespaceMetaKey, imageName string) core.BeforeFunc {
 	return func(ctx *core.BeforeFuncCtx) error {
 		namespaceEndpoint := ctx.Meta.Render(fmt.Sprintf("{{ .%s.Endpoint }}", namespaceMetaKey))
-		namespaceID := ctx.Meta.Render(fmt.Sprintf("{{ .%s.ID }}", namespaceMetaKey))
 		remote := fmt.Sprintf("%s/%s:latest", namespaceEndpoint, imageName)
 
 		return core.BeforeFuncCombine(
 			core.BeforeFuncOsExec("docker", "pull", dockerImage),
 			core.BeforeFuncOsExec("docker", "tag", dockerImage, remote),
 			core.BeforeFuncOsExec("docker", "push", remote),
-			core.ExecStoreBeforeCmd(
-				metaKey,
-				fmt.Sprintf(
-					"scw registry image list namespace-id=%s name=%s",
-					namespaceID,
-					imageName,
-				),
-			),
 		)(
 			ctx,
 		)
+	}
+}
+
+func StoreImageInMeta(metaKey, namespaceMetaKey, imageName string) core.BeforeFunc {
+	return func(ctx *core.BeforeFuncCtx) error {
+		namespaceID := ctx.Meta.Render(fmt.Sprintf("{{ .%s.ID }}", namespaceMetaKey))
+
+		return core.ExecStoreBeforeCmd(metaKey, fmt.Sprintf("scw registry image list namespace-id=%s name=%s", namespaceID, imageName))(ctx)
 	}
 }
