3fb55c100093f197da0250648a04a0bfab582c34
Leïla Marabese <lmarabese@scaleway.com>
2025-11-10 18:28:55 +0100

    fix error on windows

--- core/testing.go
+++ core/testing.go
@@ -316,7 +316,7 @@ var foldersUsingVCRv4 = []string{
 }
 
 func folderUsesVCRv4(fullFolderPath string) bool {
-	fullPathSplit := strings.Split(fullFolderPath, "/")
+	fullPathSplit := strings.Split(fullFolderPath, string(os.PathSeparator))
 
 	folder := fullPathSplit[len(fullPathSplit)-2]
 	for _, migratedFolder := range foldersUsingVCRv4 {
