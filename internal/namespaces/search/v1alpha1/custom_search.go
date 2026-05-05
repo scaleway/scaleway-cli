91c76960dbf39a2a26a31eaef2c1ecc9607b1735
Rémy Léone <rleone@scaleway.com>
2026-05-05 16:29:46 +0200

    fix

--- internal/namespaces/search/v1alpha1/custom_search.go
+++ internal/namespaces/search/v1alpha1/custom_search.go
@@ -28,17 +28,3 @@ func searchBuilder(c *core.Command) *core.Command {
 
 	return c
 }
-
-//
-//Resources.0.ID              4f59cb01-97ec-400a-88c7-92935d36c8fa
-//Resources.0.Name            scw-nostalgic-golick-system
-//Resources.0.ProjectID       951df375-e094-4d26-97c1-ba548eeb9c42
-//Resources.0.OrganizationID  951df375-e094-4d26-97c1-ba548eeb9c42
-//Resources.0.Type            sbs_volume
-//Resources.0.Zone            fr-par-2
-//Resources.1.ID              a31ba2c0-878e-43aa-97c6-9cbf1537c209
-//Resources.1.Name            scw-nostalgic-golick
-//Resources.1.ProjectID       951df375-e094-4d26-97c1-ba548eeb9c42
-//Resources.1.OrganizationID  951df375-e094-4d26-97c1-ba548eeb9c42
-//Resources.1.Type            instance_server
-//Resources.1.Zone            fr-par-2
