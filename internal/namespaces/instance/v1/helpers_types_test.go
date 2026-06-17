98b45ee8ca07e8d3e44bdad6a1899b2947be0a98
Sai Asish Y <say.apm35@gmail.com>
2026-06-17 12:31:36 -0700

    fix(instance): avoid index out of range in server type deprecation warning
    Signed-off-by: Sai Asish Y <say.apm35@gmail.com>

---
+++ internal/namespaces/instance/v1/helpers_types_test.go
@@ -0,0 +1,29 @@
+package instance
+
+import (
+	"context"
+	"testing"
+
+	instance "github.com/scaleway/scaleway-sdk-go/api/instance/v1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWarningServerTypeDeprecated(t *testing.T) {
+	// Point the client at an unreachable endpoint so the catalog and
+	// compatible-types lookups fail and the function returns early.
+	client, err := scw.NewClient(
+		scw.WithAuth("SCWXXXXXXXXXXXXXXXXX", "11111111-1111-1111-1111-111111111111"),
+		scw.WithAPIURL("http://127.0.0.1:1"),
+		scw.WithDefaultZone(scw.ZoneFrPar1),
+		scw.WithDefaultRegion(scw.RegionFrPar),
+	)
+	require.NoError(t, err)
+
+	server := &instance.Server{CommercialType: "DEV1-S", Zone: scw.ZoneFrPar1}
+	warnings := warningServerTypeDeprecated(context.Background(), client, server)
+
+	require.NotEmpty(t, warnings)
+	assert.Contains(t, warnings[0], "EndOfService")
+}
