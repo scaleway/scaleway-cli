acf2f93523a39ca9547bdb1abdd28015a8b65608
Geoffrey JOUNT <gjount@scaleway.com>
2026-08-03 08:11:06 +0200

    use path style in the s3 client

--- internal/namespaces/object/v1/s3_helpers.go
+++ internal/namespaces/object/v1/s3_helpers.go
@@ -74,6 +74,7 @@ func newS3Client(
 		BaseEndpoint: new(customEndpoint),
 		Region:       region.String(),
 		HTTPClient:   httpClient,
+		UsePathStyle: true, // Required for custom S3 path
 	})
 }
 
