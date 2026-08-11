e551fa18a95f76f9d8fe70ee1d1256b7a50c2c89
Mia-Cross <lmarabese@scaleway.com>
2026-08-11 11:18:49 +0200

    tests(container): use sdk vcr (go-vcr.v4) for container package (#5580)

--- core/testing_recorder.go
+++ core/testing_recorder.go
@@ -19,7 +19,6 @@ import (
 func cassetteRequestFilter(i *cassette.Interaction) error {
 	delete(i.Request.Headers, "x-auth-token")
 	delete(i.Request.Headers, "X-Auth-Token")
-	delete(i.Request.Headers, "X-Registry-Auth")
 	orgIDRegex := regexp.MustCompile(`(.+)organization_id=[0-9a-f-]{36}(.+)`)
 	tokenRegex := regexp.MustCompile(`^https://api\.scaleway\.com/account/v1/tokens/[0-9a-f-]{36}$`)
 
@@ -37,88 +36,10 @@ func cassetteResponseFilter(i *cassette.Interaction) error {
 	i.Response.Body = regexp.MustCompile(`"secret_key":"[0-9a-f-]{36}"`).
 		ReplaceAllString(i.Response.Body, `"secret_key":"11111111-1111-1111-1111-111111111111"`)
 
-	// Buildpacks
-	i.URL = regexp.MustCompile(`pack\.local%2Fbuilder%2F[0-9a-f]{20}`).
-		ReplaceAllString(i.URL, "pack.local%2Fbuilder%2F11111111111111111111")
-	i.URL = regexp.MustCompile(`pack\.local/builder/[0-9a-f]{20}`).
-		ReplaceAllString(i.URL, "pack.local/builder/11111111111111111111")
-
-	i.Request.Body = regexp.MustCompile(`pack\.local/builder/[0-9a-f]{20}`).
-		ReplaceAllString(i.Response.Body, "pack.local/builder/11111111111111111111")
-	i.Response.Body = regexp.MustCompile(`pack\.local/builder/[0-9a-f]{20}`).
-		ReplaceAllString(i.Response.Body, "pack.local/builder/11111111111111111111")
-
 	return nil
 }
 
-const (
-	windowDockerEngine = "//./pipe/docker_engine"
-	unixDockerEngine   = "/var/run/docker.sock"
-)
-
-func applyQueryMatcherIgnore(r *http.Request, i *cassette.Request) bool {
-	queryMatcherIgnore := []string(nil)
-
-	if strings.Contains(r.URL.RawQuery, "project_id=11111111-1111-1111-1111-111111111111") {
-		match, err := regexp.MatchString("project_id=[0-9a-f-]{36}", i.URL)
-		if err != nil || !match {
-			return false
-		}
-
-		queryMatcherIgnore = append(queryMatcherIgnore, "project_id")
-	}
-
-	if strings.Contains(r.URL.RawQuery, "end=") {
-		if !strings.Contains(i.URL, "end=") {
-			return false
-		}
-
-		queryMatcherIgnore = append(queryMatcherIgnore, "end")
-	}
-
-	if strings.Contains(r.URL.RawQuery, "start=") {
-		if !strings.Contains(i.URL, "start=") {
-			return false
-		}
-
-		queryMatcherIgnore = append(queryMatcherIgnore, "start")
-	}
-
-	// Return early if no further operation is needed (nothing to be ignored from the query)
-	if len(queryMatcherIgnore) == 0 {
-		return true
-	}
-
-	cassetteURL, _ := url.Parse(i.URL)
-	requestURL := r.URL
-
-	requestURLValues := requestURL.Query()
-	cassetteURLValues := cassetteURL.Query()
-
-	for _, query := range queryMatcherIgnore {
-		requestURLValues.Del(query)
-		cassetteURLValues.Del(query)
-	}
-
-	r.URL.RawQuery = requestURLValues.Encode()
-	cassetteURL.RawQuery = cassetteURLValues.Encode()
-	i.URL = cassetteURL.String()
-
-	return true
-}
-
 func cassetteMatcher(r *http.Request, i cassette.Request) bool {
-	// Docker
-	if r.URL.Host == windowDockerEngine || r.URL.Host == "npipe://"+windowDockerEngine {
-		r.URL.Host = unixDockerEngine
-	}
-
-	// Buildpacks
-	r.URL.RawQuery = regexp.MustCompile(`pack\.local%2Fbuilder%2F[0-9a-f]{20}`).
-		ReplaceAllString(r.URL.RawQuery, "pack.local%2Fbuilder%2F11111111111111111111")
-	r.URL.Path = regexp.MustCompile(`pack\.local/builder/[0-9a-f]{20}`).
-		ReplaceAllString(r.URL.Path, "pack.local/builder/11111111111111111111")
-
 	// Read body
 	if r.Body != nil && r.Body != http.NoBody {
 		reqBody, err := io.ReadAll(r.Body)
@@ -134,13 +55,6 @@ func cassetteMatcher(r *http.Request, i cassette.Request) bool {
 	match, err := regexp.MatchString(".s3.[a-z]{2}-[a-z]{3}.scw.cloud", r.URL.Host)
 	if err == nil && match {
 		return customS3Matcher(r, i)
-	}
-
-	// Temporary QueryMatcherIgnore for container tests (should be removed when #5580 is merged and the new vcr is used)
-	// The cassette stores real IDs and data, but the fake client used to replay interactions only has placeholder values.
-	// We need to ignore those fields in the request URL (after verifying that they are also present in the cassette).
-	if !applyQueryMatcherIgnore(r, &i) {
-		return false
 	}
 
 	return cassette.DefaultMatcher(r, i)
